709a9581bf817e78e672d4d819ee36918fae01c9
thuonghoai <thuong.buihoai@asnet.com.vn>
2022-04-05 17:39:23 +0700

    Example with storybook

--- Learning_React/my-react-app/src/stories/Button.tsx
+++ Learning_React/my-react-app/src/stories/Button.tsx
@@ -1,46 +1,28 @@
-import React from 'react';
-import './button.css';
+import React from "react";
+import "./button.css";
 
 interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary?: boolean;
-  /**
-   * What background color to use
-   */
+  variant?: boolean;
+  children: string,
+  label: string,
   backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
-  size?: 'small' | 'medium' | 'large';
-  /**
-   * Button contents
-   */
-  label: string;
-  /**
-   * Optional click handler
-   */
-  onClick?: () => void;
+  onClick: () => void;
 }
 
 /**
  * Primary UI component for user interaction
  */
 export const Button = ({
-  primary = false,
-  size = 'medium',
-  backgroundColor,
+  variant = true,
   label,
-  ...props
+  backgroundColor,
 }: ButtonProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+  const mode = variant ? "storybook-button--primary" : "storybook-button--secondary";
   return (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      className={["storybook-button", `storybook-button`, mode].join(" ")}
       style={{ backgroundColor }}
-      {...props}
     >
       {label}
     </button>
