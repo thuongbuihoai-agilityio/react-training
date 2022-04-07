a54b55e0e007cb29a98fc08ecb18a2f152ec54ab
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-04-07 11:59:02 +0700

    Example with storybook

--- Learning_React/my-react-app/src/stories/Logo.stories.tsx
+++ Learning_React/my-react-app/src/stories/Logo.stories.tsx
@@ -1,17 +1,16 @@
 import React from "react";
-import headerLogo from "../assets/images/logo.svg"
-import Header from "../components/header/Header";
-import MENU_LIST from "../constants/menu";
+import headerLogo from "../assets/images/logo.svg";
+import Logo from "../components/logo/Logo";
 
 export default {
   /* 👇 The title prop is optional.
   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
   * to learn how to generate automatic titles
   */
-  title: "Component/Header",
-  component: Header,
+  title: "Component/Logo",
+  component: Logo,
 };
 
 export function Default() {
-  return <Header logo={headerLogo} menuList={MENU_LIST} />;
+  return <Logo logo={headerLogo} />;
 }
