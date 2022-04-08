ec848336bfe9d1ea9eec79fd2459efff51d35283
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-04-08 09:58:28 +0700

    Create header section

---
+++ Practice-1/practice_1/src/stories/Header.stories.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Header from "../components/header/Header";
+import MENU_LIST from "../constants/menu";
+import { MAIN_TEXT } from "./Description.stories";
+
+export default {
+  /* 👇 The title prop is optional.
+  * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+  * to learn how to generate automatic titles
+  */
+  title: "Component/Header",
+  component: Header,
+};
+
+export function Default() {
+  return <Header 
+    children1 = "HOFMANN"
+    children2 = "What Makes A Hotel Boutique"
+    children3 = "EXPLORE"
+    MAIN_TEXT = {MAIN_TEXT}
+    menuList = {MENU_LIST} />;
+}
