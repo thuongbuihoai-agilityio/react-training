33e07915dca1145aa02ed0a98fae2c5fe033ebeb
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-04-08 09:58:42 +0700

    Create navigation section

---
+++ Practice-1/practice_1/src/stories/Navigation.stories.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import MENU_LIST from "../constants/menu";
+import Navigation from "../components/navigation/Menu";
+
+export default {
+  /* 👇 The title prop is optional.
+  * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+  * to learn how to generate automatic titles
+  */
+  title: "Component/Navigation",
+  component: Navigation,
+};
+
+export function Default() {
+  return <Navigation menuList={MENU_LIST} />;
+}
