c25847ceca3d1f16c8957a1fe8a17eaa5e5205d1
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-04-08 17:13:47 +0700

    Delete Children

--- Practice-1/practice_1/src/components/header/Header.tsx
+++ Practice-1/practice_1/src/components/header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { Children } from "react";
+import React from "react";
 import Navigation from "../navigation/Menu";
 import { MenuType } from "../../types/menu";
 import SectionTitle from "../title/SectionTitle";
