7c781d23dc33720a3ed429e168201e4efc2d20a0
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-04-08 17:16:06 +0700

    Create card component

---
+++ Practice-1/practice_1/src/constants/card.ts
@@ -0,0 +1,28 @@
+import { CardProps } from "../types/card";
+
+const CARD_FREEMIUM: CardProps = {
+  value1: "Basic Collections",
+  value2: "10 Messages",
+  value3: "50 Images per Day"
+}
+
+const CARD_STARTUP: CardProps = {
+  value1: "Multiple Collections",
+  value2: "100 Messages",
+  value3: "100 Images per Day"
+}
+
+const CARD_BUSINESS: CardProps = {
+  value1: "Unlimited Collections",
+  value2: "500 Messages",
+  value3: "300 Images per Day",
+  value4: "Project Reviews"
+}
+
+const CARD_ENTERPRISE: CardProps = {
+  value1: "Unlimited Collections",
+  value2: "Unlimited Messages",
+  value3: "Unlimited Images per Day",
+  value4: "Project Reviews"
+}
+export { CARD_FREEMIUM, CARD_STARTUP, CARD_BUSINESS, CARD_ENTERPRISE}
---
+++ Practice-1/practice_1/src/types/card.ts
@@ -0,0 +1,6 @@
+export interface CardProps {
+  value1: string;
+  value2: string;
+  value3: string;
+  value4?: string;
+}
