8f7d381e5cb229cecef10245c5b914a98a1d3405
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-04-11 21:36:09 +0700

    Create card type

--- Practice-1/practice_1/src/types/card.ts
+++ Practice-1/practice_1/src/types/card.ts
@@ -1,6 +1,12 @@
 export interface CardProps {
+  key?: string;
+  className?: string;
   value1: string;
   value2: string;
   value3: string;
   value4?: string;
+  title: string;
+  unit: string;
+  price: number;
+  label: string;
 }
