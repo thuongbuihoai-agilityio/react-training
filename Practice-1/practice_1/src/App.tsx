19668a1e77b31988d1814b679b1d5509da814b3c
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-04-11 21:38:19 +0700

    Init app

--- Practice-1/practice_1/src/App.tsx
+++ Practice-1/practice_1/src/App.tsx
@@ -1,8 +1,22 @@
+import Header from "./components/header/Header";
+import Plan from "./components/section/plan/Plan";
+import { CARD_LIST } from "./constants/card";
+import MENU_LIST from "./constants/menu";
+import { MAIN_TEXT } from "./stories/Description.stories";
+import menuBar from "./assets/images/icons/menuBar.svg"
 
 
 function App(): JSX.Element {
   return (
-    
+    <>
+    <Header
+        children1="HOFMANN"
+        children2="What Makes A Hotel Boutique"
+        children3="EXPLORE"
+        MAIN_TEXT={MAIN_TEXT}
+        menuList={MENU_LIST}
+        menuBar={menuBar} />
+    <Plan planTitle={"Subscribing Plans"} cardList={CARD_LIST} /></>
   )
 }
 
