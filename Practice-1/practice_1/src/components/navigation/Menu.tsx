d01e42e886203c02331268a884c9284d8bd4a67a
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-04-11 21:34:55 +0700

    Create navigation component

--- Practice-1/practice_1/src/components/navigation/Menu.tsx
+++ Practice-1/practice_1/src/components/navigation/Menu.tsx
@@ -1,23 +1,25 @@
 import React from "react";
 import { MenuType } from "../../types/menu";
 import Button from "../button/Button";
+import Bar from "../navBar/Bar";
 import "./menu.css"
 
 interface NavigationProps {
   menuList: MenuType[];
+  menuBar: string;
 }
 
-export default function Navigation({ menuList }: NavigationProps): JSX.Element {
+export default function Navigation({ menuList, menuBar }: NavigationProps): JSX.Element {
   function renderMenuList(list: MenuType[]) {
     return list.map((item) => <li className="nav__menu-item" key={item.key}>{item.label}</li>);
   }
 
   return (
     <nav className="nav">
-      <i className="fa fa-bars nav__icon" />
+    <Bar logo={menuBar}></Bar>
       <div className="nav__menu">
-      <ul className="nav__list">{renderMenuList(menuList)}</ul>
-      <Button type="primary">TAKE A TOUR</Button>
+        <ul className="nav__list">{renderMenuList(menuList)}</ul>
+        <Button type="light">TAKE A TOUR</Button>
       </div>
     </nav>
   );
