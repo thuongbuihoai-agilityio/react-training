ddfa5fbfb1897b3a2e0d19ddb9b3bc4475a16fbc
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-04-12 16:06:56 +0700

    Create feature component

---
+++ Practice-1/practice_1/src/components/section/feature/Feature.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { FeatureListProps } from "../../../types/feature";
+import Icon from "../../icon/Icon";
+import FeatureList from "../../list/FeatureList";
+import SectionTitle from "../../title/SectionTitle";
+import "./feature.css"
+
+export interface FeatureProps {
+  headingTitle: string;
+  text: string;
+  list: FeatureListProps[];
+  imageDesktop: string;
+}
+
+export default function Feature({
+  headingTitle,
+  text,
+  list,
+  imageDesktop
+}: FeatureProps): JSX.Element {
+  return (
+    <section className = "feature__img">
+      <div className="container">
+        <div className = "feature__heading">
+          <SectionTitle className = "feature-heading__title"><span>{headingTitle}</span></SectionTitle>
+          <SectionTitle className = "feature__text"><span>{text}</span></SectionTitle>
+        </div>
+        <div className="feature">
+        <div className = "feature__list">
+            <FeatureList featureList={list}></FeatureList>
+          </div>
+          <figure className = "feature__img--desktop">
+            <Icon icon={imageDesktop} alt = "images computer" className = "desktop" />
+          </figure>
+        </div>
+      </div>
+    </section>
+  );
+}
