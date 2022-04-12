5e90b2f6f8815162ac8263418c2e407b1c7b574a
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-04-12 16:12:24 +0700

    Add description for feature section

--- Practice-1/practice_1/src/stories/Description.stories.tsx
+++ Practice-1/practice_1/src/stories/Description.stories.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Description } from "../components/description/Description";
+import { MAIN_TEXT } from "../constants/feature";
+import { FEATURE_RESPONSIVE, FEATURE_DIAMOND, FEATURE_LAYERS } from "../constants/feature";
 
 export default {
   /* 👇 The title prop is optional.
@@ -9,9 +11,27 @@ export default {
   title: "Component/Description",
   component: Description,
 };
-export const MAIN_TEXT = "As conscious traveling Paupers we must always be concerned about our dear Mother Earth. If you think about it, you travel across her face, and She is the host to your journey; without Her we could not find the unfolding adventures that attract and feed our souls."
+
 export function MainText() {
   return (
-    <Description className="description">{MAIN_TEXT}</Description>
+    <Description className="main">{MAIN_TEXT}</Description>
+  );
+}
+
+export function Responsive() {
+  return (
+    <Description className="responsive">{FEATURE_RESPONSIVE}</Description>
+  );
+}
+
+export function Diamond() {
+  return (
+    <Description className="diamond">{FEATURE_DIAMOND}</Description>
+  );
+}
+
+export function Layers() {
+  return (
+    <Description className="layers">{FEATURE_LAYERS}</Description>
   );
 }
