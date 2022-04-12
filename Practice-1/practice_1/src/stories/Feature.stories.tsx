041905fefe091ae9b02d74a734e1d81a0c5c9da4
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-04-12 16:14:07 +0700

    Create feature component

---
+++ Practice-1/practice_1/src/stories/Feature.stories.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Feature from "../components/section/feature/Feature";
+import { FEATURE } from "../constants/feature";
+import imageDesktop from "../assets/images/computer.png";
+
+export default {
+  /* 👇 The title prop is optional.
+  * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+  * to learn how to generate automatic titles
+  */
+  title: "Component/Feature",
+  component: Feature,
+};
+
+export function Default() {
+  return <Feature
+    headingTitle = "Feature"
+    text = "We're taking it to the next level"
+    list={FEATURE}
+    imageDesktop = {imageDesktop} 
+  />
+}
