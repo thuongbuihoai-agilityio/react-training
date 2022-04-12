89818f728779f925a7c54618beb00ad8ef448baa
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-04-12 16:13:46 +0700

    Create feature list component

---
+++ Practice-1/practice_1/src/stories/FeatureList.stories.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import FeatureList from "../components/list/FeatureList";
+import { FEATURE } from "../constants/feature";
+
+export default {
+  /* 👇 The title prop is optional.
+  * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+  * to learn how to generate automatic titles
+  */
+  title: "Component/FeatureList",
+  component: FeatureList,
+};
+
+export function FeatureLists() {
+  return <FeatureList featureList={FEATURE} ></FeatureList>
+}
