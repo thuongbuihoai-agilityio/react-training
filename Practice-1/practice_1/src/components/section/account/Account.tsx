9b8721fab5101f64e03657a97217cc69f6ae78af
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-04-13 17:07:08 +0700

    Create account component

---
+++ Practice-1/practice_1/src/components/section/account/Account.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { InputProps } from "../../../types/input";
+import Form from "../../form/Form";
+import SectionTitle from "../../title/SectionTitle";
+import "./account.css"
+
+export interface AccountProps {
+  list: InputProps[];
+}
+
+export default function Account({
+  list
+}: AccountProps): JSX.Element {
+  return (
+    <section className = "account">
+       <div className = "container">
+       <SectionTitle className="account__heading"><p>Create An Account</p></SectionTitle>
+       <Form inputList={list}></Form>
+       </div>
+      </section>
+  );
+}
