85e46bd0f43a766e233410f6f2e71db93b63364b
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-04-13 14:36:04 +0700

    Change folder location

---
+++ Practice-1/practice_1/src/components/list/cardList/CardList.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { CardProps } from "../../../types/card";
+import Card from "../../cards/card/Card";
+import "./cardList.css"
+
+interface RenderCard {
+  cardList: CardProps[];
+}
+export default function CardList({ cardList }: RenderCard): JSX.Element {
+  function renderCardList(list: CardProps[]) {
+    return list.map((item) => (
+      <Card
+        key={item.key}
+        className={`card__info card__info--${item.className}`}
+        value1={item.value1}
+        value2={item.value2}
+        value3={item.value3}
+        value4={item.value4}
+        title={item.title}
+        unit={item.unit}
+        price={item.price}
+        label={item.label}
+      />
+    ));
+  }
+  return (
+    <div className="container">
+      <div className="card__list">{renderCardList(cardList)}</div>
+    </div>
+  );
+}
