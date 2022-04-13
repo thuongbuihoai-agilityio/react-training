998ef3c32f61fcf1d8d7c3f40574c4d7d741a45f
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-04-13 14:40:48 +0700

    Create component circle

---
+++ Practice-1/practice_1/src/stories/Circle.stories.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Circle } from "../components/circles/circle/Circle";
+export default {
+  /* 👇 The title prop is optional.
+  * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+  * to learn how to generate automatic titles
+  */
+  title: "Component/Circle",
+  component: Circle,
+};
+
+export function Circle_74_98() {
+  return <Circle className="74-98" />;
+}
+
+export function Circle_39_75() {
+  return <Circle className="39-75" />;
+}
+
+export function Circle_81_15() {
+  return <Circle className="81-15" />;
+}
