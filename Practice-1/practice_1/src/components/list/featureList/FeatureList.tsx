8067bd90e7fce43e69933fff67673b6015c67b44
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-04-13 14:36:33 +0700

    Change folder location

---
+++ Practice-1/practice_1/src/components/list/featureList/FeatureList.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { FeatureListProps } from "../../../types/feature";
+import FeatureItem from "../../featureItem/FeatureItem";
+
+interface RenderFeature {
+  featureList: FeatureListProps[];
+}
+
+export default function FeatureList({ featureList }: RenderFeature): JSX.Element {
+  function renderFeatureList(list: FeatureListProps[]) {
+    return list.map((item) => (
+      <FeatureItem
+        className = {item.className}
+        title = {item.title}
+        content = {item.content}
+        icon = {item.icon}
+        alt = {item.alt} 
+      />
+    ));
+  }
+  return (
+    <div>{renderFeatureList(featureList)}</div>
+  );
+}
