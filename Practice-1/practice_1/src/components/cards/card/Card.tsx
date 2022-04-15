8e618c13d0abe2615b804fe1f99645ddef9ff658
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-04-15 09:12:07 +0700

    Change name

--- Practice-1/practice_1/src/components/cards/card/Card.tsx
+++ Practice-1/practice_1/src/components/cards/card/Card.tsx
@@ -7,21 +7,13 @@ import "./card.css"
 import { CardProps } from "../../../types/card";
 
 export default function Card({
-  className,
-  title,
-  unit,
-  price,
-  value1,
-  value2,
-  value3,
-  value4,
-  label,
+  className, title, unit, price, value1, value2, value3, value4, label
 }: CardProps): JSX.Element {
   return (
     <div className="card">
       <div className={`card__info card__info--${className}`}>
         <CardTitle className="name"><span>{title}</span></CardTitle>
-        <CardPrice children1={unit} children2={price} ></CardPrice>
+        <CardPrice unit={unit} price={price} ></CardPrice>
       </div>
       <div className="card__description">
         <CardText value1={value1} value2={value2} value3={value3} value4={value4}></CardText>
