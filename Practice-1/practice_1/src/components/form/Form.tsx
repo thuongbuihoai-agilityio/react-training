b9d15908dac171f37e4645db1954a5c865c837a3
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-04-18 16:36:46 +0700

    Refactor code, review props children

--- Practice-1/practice_1/src/components/form/Form.tsx
+++ Practice-1/practice_1/src/components/form/Form.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FormProps } from "../../types/form";
 import { InputProps } from "../../types/input";
 import Button from "../button/Button";
 import { Description } from "../description/Description";
@@ -6,18 +7,16 @@ import { Input } from "../input/Input";
 import { Link } from "../link/Link";
 import "./form.css"
 
-interface FormProps {
-  inputList: InputProps[];
-}
 export default function Form({ inputList }: FormProps): JSX.Element {
   const renderInputList = (list: InputProps[]) => list.map((item) => (
     <Input className="info" type={item.type} placeholder={item.placeholder} />
   ));
+
   return (
     <form className="form">
       <Description className="signup"><p>Sign up</p></Description>
       {renderInputList(inputList)}
-      <Link value={"Already Have an account?"}></Link>
+      <Link title="Already Have an account?" href="javascript:void(0)" />
       <Button type="submit" className="signUp">Sign Up</Button>
     </form>
   );
