3dc309530eceaa2534dad6fe86c690aaed8e0112
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-04-18 16:39:42 +0700

    Change import link and review format style

--- Practice-1/practice_1/src/components/title/CardTitle.tsx
+++ Practice-1/practice_1/src/components/title/CardTitle.tsx
@@ -1,18 +1,14 @@
 import React from "react";
+import { CardTitleProps } from "../../types/cardTitle";
 import "./cardTitle.css";
-
-interface CardTitleProps {
-  className?: string;
-  children: JSX.Element;
-}
 
 export default function CardTitle({
   className,
   children,
 }: CardTitleProps): JSX.Element {
   return (
-      <p className={`card__${className}`}>
-        {children}
-      </p>
-    )
-  }
+    <p className={`card__${className}`}>
+      {children}
+    </p>
+  )
+}
--- Practice-1/practice_1/src/components/title/SectionTitle.tsx
+++ Practice-1/practice_1/src/components/title/SectionTitle.tsx
@@ -1,18 +1,14 @@
 import React from "react";
+import { SectionTitleProps } from "../../types/title";
 import "./sectionTitle.css";
-
-interface SectionTitleProps {
-  className?: string;
-  children: JSX.Element;
-}
 
 export default function SectionTitle({
   className,
   children,
 }: SectionTitleProps): JSX.Element {
   return (
-      <h2 className={`section-title section-title--${className}`}>
-        {children}
-      </h2>
-    )
-  }
+    <h2 className={`section-title section-title--${className}`}>
+      {children}
+    </h2>
+  )
+}
