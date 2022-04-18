5380d4cf2d2ea736ebc4f8fb6ee1316d5387ec5d
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-04-18 16:33:12 +0700

    Refactor code and update indent line

--- Practice-1/practice_1/src/components/circleItem/CircleItem.tsx
+++ Practice-1/practice_1/src/components/circleItem/CircleItem.tsx
@@ -1,18 +1,22 @@
 import React from "react";
-import { CircleListProps } from "../../types/circle";
+import { CircleListProps } from "../../types/circleList";
 import { CirclePercent } from "../circles/circlePercent/CirclePercent";
 import { Description } from "../description/Description";
 import SectionTitle from "../title/SectionTitle";
 import "./circleItem.css"
 
 export default function CircleItem({
-  number1, number2, title, content, className
-}: CircleListProps): JSX.Element {
+  number1,
+  number2,
+  title,
+  content,
+  className
+}: CircleListProps) {
   return (
     <div className = "circle__info">
-    <CirclePercent className = {`${className}`} number1={number1} number2={number2} />
-    <SectionTitle className="circle__title"><p>{title}</p></SectionTitle>
-    <Description className="content">{content}</Description>
+      <CirclePercent className = {`${className}`} number1={number1} number2={number2} />
+      <SectionTitle className="circle__title">{title}</SectionTitle>
+      <Description className="content">{content}</Description>
   </div>
   );
 }
