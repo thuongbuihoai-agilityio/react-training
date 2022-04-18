9e18ee480b7b8ac03d00ae07cc1b927674072e92
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-04-18 16:35:53 +0700

    Refactor code, review indent line and change import link

--- Practice-1/practice_1/src/components/featureItem/FeatureItem.tsx
+++ Practice-1/practice_1/src/components/featureItem/FeatureItem.tsx
@@ -5,13 +5,17 @@ import SectionTitle from "../title/SectionTitle";
 import "./featureItem.css"
 
 export default function FeatureItem({
-  className, icon, alt, title, content
+  className,
+  icon,
+  alt,
+  title,
+  content
 }: FeatureListProps): JSX.Element {
   return (
     <div className="feature__item">
       <img className={`icon--${className}`} src={icon} alt={alt} />
       <div>
-        <SectionTitle className="feature__title"><span>{title}</span></SectionTitle>
+        <SectionTitle className="feature__title">{title}</SectionTitle>
         <Description className="responsive">{content}</Description>
       </div>
     </div>
