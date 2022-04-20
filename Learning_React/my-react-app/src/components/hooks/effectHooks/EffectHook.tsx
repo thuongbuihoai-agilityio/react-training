8a7a5102385ba49ecbd824652c9ab302cfa68251
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-04-20 17:01:20 +0700

    Example react hooks

--- Learning_React/my-react-app/src/components/hooks/effectHooks/EffectHook.tsx
+++ Learning_React/my-react-app/src/components/hooks/effectHooks/EffectHook.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useLayoutEffect, useState } from "react";
 
 export function EffectCallback() {
   // useEffect (callback)
@@ -22,29 +22,93 @@ export function EffectCallback() {
   );
 }
 
-export function EffectArray() {
+export function Timer() {
   // userEffect (callback, [])
-    // Call the callback only once after the component is mounted
-  const [posts, setPosts] = useState([])
+  // Call the callback only once after the component is mounted
+
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-    .then(res => res.json())
-    .then(posts => {
-      setPosts(posts);
-    })
+    let timer = setTimeout(() => {
+    setCount((count) => count + 1);
+  }, 1000);
+
+  return () => clearTimeout(timer)
+  }, []);
+
+  return <h1>I've rendered {count} times!</h1>;
+}
+
+// ClearUp function is always called before component mounted
+export function ClearUp() {
+  const [countdown, setCountdown] = useState(180)
+
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      setCountdown(countdown - 1)
+    }, 1000)
+
+    return () => clearInterval(timerId)
   }, [])
-  console.log(posts);
-  
 
   return (
     <div>
-      <h2>useEffect (callback, [])</h2>
-        <ul>
-          {posts.map(post => (
-            <li key={post.id}>{post.title}</li>
-          ))}
-        </ul>
+      <h1>Clear up</h1>
+      <h3>{countdown}</h3>
     </div>
-  );
+  )
+}
+
+// Preview avatar
+export function PreviewAvatar() {
+  const [avatar, setAvatar] = useState()
+
+  useEffect(() => {
+    return () => {
+      avatar && URL.revokeObjectURL(avatar.preview)
+    }
+  }, [avatar])
+
+  const handlePreviewAvatar = (event) => {
+    // get file from event
+    // file is object
+    const file = event.target.files[0]
+    file.preview = URL.createObjectURL(file)
+
+    setAvatar(file)
+  }
+
+  return (
+    <div>
+      <h1>Preview Avatar</h1>
+      <input type="file"
+        onChange={handlePreviewAvatar}
+      />
+      {avatar && (
+        <img src={avatar.preview} alt="images" />
+      )}
+    </div>
+  )
+}
+
+// useLayoutEffect
+export function UseLayoutEffect() {
+  const [count, setCount] = useState(180)
+
+  useLayoutEffect(() => {
+   if (count > 3)
+    setCount(0)
+  }, [count])
+
+  const handleRun = () => {
+    setCount(count + 1)
+  }
+
+  return (
+    <div>
+      <h1>Example useLayoutEffect</h1>
+      <h3>{count}</h3>
+      <button onClick={handleRun}>Run</button>
+    </div>
+  )
 }
--- Learning_React/my-react-app/src/components/hooks/stateHooks/StateHook.tsx
+++ Learning_React/my-react-app/src/components/hooks/stateHooks/StateHook.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useReducer, useState } from "react";
 
 export default function Example() {
   // Declare a new state variable, which we'll call "count"
@@ -16,4 +16,34 @@ export default function Example() {
       </button>
     </div>
   );
+}
+
+// useReducer
+const initState = 0
+
+const UP_ACTION = "up"
+const DOWN_ACTION = "down"
+
+const reducer = (state: number, action: string) => {
+  switch(action) {
+    case UP_ACTION:
+      return state + 1
+    case DOWN_ACTION:
+      return state - 1
+    default: throw new Error("Invalid action")
+  }
+}
+
+export function Reducer() {
+  // dispatch is used to trigger the action is up or down
+  const [count, dispatch] = useReducer( reducer, initState)
+
+  return (
+    <div>
+      <h1>Example useReducer</h1>
+      <h3>{count}</h3>
+      <button onClick={() => dispatch(DOWN_ACTION)}>Down</button>
+      <button onClick={() => dispatch(UP_ACTION)}>Up</button>
+    </div>
+  )
 }
