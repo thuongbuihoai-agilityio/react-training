87de20708c731f8f62fce95c399d63ef40a888e0
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-04-22 15:39:32 +0700

    Create header constants

---
+++ practice-2/practice_2/src/constants/header.ts
@@ -0,0 +1,17 @@
+import logo from "../assets/images/logo.png";
+import avatar from "../assets/images/appleIceCream.jpg"
+
+const logos = {
+  src: logo,
+  alt: "Just Shoppe",
+};
+
+const users = {
+  username: "Hoai Thuong",
+  avatar: {
+    src: avatar,
+    alt: "user avatar",
+  },
+};
+
+export { logos, users }
