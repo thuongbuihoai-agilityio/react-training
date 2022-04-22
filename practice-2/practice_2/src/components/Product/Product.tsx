91705b5d350600da82c85951e0b5a037b6716c0c
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-04-22 15:38:41 +0700

    Create component product

---
+++ practice-2/practice_2/src/components/Product/Product.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { ProductProps } from "../../types/product";
+import "./product.css"
+
+export default function Products({ image }: ProductProps) {
+
+  return (
+    <div className="product">
+      <img
+      className="product__image"
+      src={image.src}
+      alt={image.alt}
+    />
+    <p className="product__view">Quick view</p>
+    </div>
+  );
+}
