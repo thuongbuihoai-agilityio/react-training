3b8e5e1c5e6d3021203538ba030e2781f43ee4a8
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-04-25 17:50:52 +0700

    Add button, filter component

---
+++ practice-2/practice_2/src/stories/Button.stories.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Button from "../components/Button/Button";
+
+export default {
+  /* 👇 The title prop is optional.
+  * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+  * to learn how to generate automatic titles
+  */
+  title: "Component/Button",
+  component: Button,
+};
+
+export function Default() {
+  return <Button children="Filter" />;
+}
