0d6f5012f3bc0fcaee8c20dfcede857e0ce28716
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-04-26 17:55:36 +0700

    Create card component

--- practice-2/practice_2/src/components/Card/CardItem/CardItem.tsx
+++ practice-2/practice_2/src/components/Card/CardItem/CardItem.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { cardImage } from "../../../constants/card";
 import Price from "../../Price/Price";
-import Products from "../../Product/Product";
+import Products from "../../Product/ProductItem/Product";
 import Title from "../../Title/Title";
 import "../card.css"
 
@@ -11,7 +11,7 @@ export default function CardItem() {
     <div className="card">
       <Products image={cardImage} />
       <div className="card__content">
-        <Title className={""} children="Banh kem" />
+        <Title className={""} children="Build Your Pizza" />
         <Price children="50000" />
       </div>
     </div>
