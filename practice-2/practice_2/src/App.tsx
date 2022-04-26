715d35cda61e7ab23561a9047d4b133d81602c9a
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-04-26 18:00:10 +0700

    Create type and add style css

--- practice-2/practice_2/src/App.tsx
+++ practice-2/practice_2/src/App.tsx
@@ -1,12 +1,16 @@
 import CardItem from "./components/Card/CardItem/CardItem"
+import Filter from "./components/Filter/Filter/Filter"
 import Header from "./components/Header/Header"
+import Navigation from "./components/Navigation/Navigation"
 import { logos } from "./constants/header"
 
 function App() {
 
   return (
     <>
+      <Navigation />
       <Header url={logos.src} />
+      <Filter />
       <CardItem />
     </>
   )
