54e61778ee2fd8c955fdecfaf0be152685d0c3f9
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-04-26 17:56:20 +0700

    Create filter component

---
+++ practice-2/practice_2/src/components/Filter/Filter/Filter.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { DEFAULT_CATEGORY, DEFAULT_FILTERER } from "../../../constants/categories";
+import Button from "../../Button/Button";
+import Category from "../../Categories/Categories";
+import FilterBy from "../FilterBy/FilterBy";
+import "./filter.css"
+
+export default function Filter({}) {
+  return (
+    <div className="filter">
+      <Button className="filter" children="Filter" />
+      <input type="checkbox" hidden className="filter__input" id="filter__check" />
+      <label htmlFor="filter__check" className="filter__overlay"></label>
+      <div className="filters">
+        <Category categoriesList={DEFAULT_CATEGORY} />
+        <FilterBy filterBy={DEFAULT_FILTERER} />
+      </div>
+    </div>
+  )
+}
