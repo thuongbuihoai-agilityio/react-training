7883ffc9f789839a8e244cb267f26e9ac19ee83f
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-04-27 17:37:56 +0700

    Init app

--- practice-2/practice_2/src/App.tsx
+++ practice-2/practice_2/src/App.tsx
@@ -1,17 +1,25 @@
-import CardItem from "./components/Card/CardItem/CardItem"
+import { useState } from "react"
+import CardItem from "./pages/CardItem/CardItem"
+import { Routes, Route } from "react-router-dom"
 import Filter from "./components/Filter/Filter/Filter"
 import Header from "./components/Header/Header"
-import Navigation from "./components/Navigation/Navigation"
+import Navigation from "./components/common/Navigation/Navigation"
+import ProductDetails from "./pages/ProductDetails/ProductDetails"
+import { cardImage } from "./constants/card"
 import { logos } from "./constants/header"
 
 function App() {
-
   return (
     <>
       <Navigation />
       <Header url={logos.src} />
       <Filter />
-      <CardItem />
+      <Routes>
+        <Route path="/" element={<CardItem />}/>
+      </Routes>
+      <Routes>
+        <Route path="/detail" element={<ProductDetails productId="1" image={cardImage} />}/>
+      </Routes>
     </>
   )
 }
--- practice-2/practice_2/src/types/commonType/button.ts
+++ practice-2/practice_2/src/types/commonType/button.ts
@@ -1,4 +1,4 @@
 export interface ButtonProps {
   className?: string
-  children: string;
+  value: string;
 }
