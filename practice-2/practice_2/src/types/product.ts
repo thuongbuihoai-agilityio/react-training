631fb745b0067bb87e1e50698af3736169002e7a
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-04-28 17:35:44 +0700

    Init app

--- practice-2/practice_2/src/types/product.ts
+++ practice-2/practice_2/src/types/product.ts
@@ -2,10 +2,14 @@ import { ImagesProps } from "./logo";
 
 export interface ProductProps {
   id: string,
+  name: string,
   image: ImagesProps;
+  product: {}
 }
 
 export interface ProductDetailProps {
+  id: string,
+  params: {},
   image: ImagesProps;
   productId?: string;
 }
