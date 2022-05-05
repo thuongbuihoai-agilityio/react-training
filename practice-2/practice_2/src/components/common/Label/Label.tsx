8d110fdc87beaf6fb9609f8bc8da20e48923531b
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-05-05 16:59:27 +0700

    Apple syntax ES6

--- practice-2/practice_2/src/components/common/Label/Label.tsx
+++ practice-2/practice_2/src/components/common/Label/Label.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { LabelProps } from "../../../types/commonType/label";
 import "./label.css"
 
-export default function Label({ value, className }: LabelProps) {
+const Label: React.FC<LabelProps> = ({ value, className }) => {
   return (
     <label htmlFor="filter__check"
       className={`label-${className}`}>
@@ -10,3 +10,5 @@ export default function Label({ value, className }: LabelProps) {
     </label>
   );
 }
+
+export default Label;
