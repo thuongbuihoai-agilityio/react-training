d1d271fdf38195be72d9b75baf8c4100a5b5bac9
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-05-05 16:58:54 +0700

    Handle filter by category

--- practice-2/practice_2/src/components/Categories/Categories.tsx
+++ practice-2/practice_2/src/components/Categories/Categories.tsx
@@ -1,16 +1,28 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useContext } from "react";
+import { ProductListContext } from "../../App";
 import fetchCategory from "../../hooks/fetchCategory";
-import { CategoriesProps } from "../../types/categories";
+import { CategoriesProps, CategoryProps } from "../../types/categories";
 import "./categories.css"
 
-export default function Category() {
-  const categories = fetchCategory();
+const Category: React.FC<CategoryProps> = ({ setFilterInput }) => {
+  const setIsReset = useContext(ProductListContext) as Function
+  const handleSearch = (e: React.MouseEvent<HTMLElement>) => {
+    const categoryId = {categoryId : e.currentTarget.dataset.index}
+    setFilterInput(categoryId);
+    setIsReset(true)
+  }
 
+  const handleDefaultCategory = () => {
+    const categoryId = ""
+    setFilterInput(categoryId);
+    setIsReset(true)
+  }
+
+  const categories = fetchCategory();
   function renderCategoryList(categories: []) {
     return categories.map((category: CategoriesProps, index: number) => 
-      <li key={index} className="categories__item">
-        <Link to="/" state={category} key={category.id}>{category.name}</Link>
+      <li  key={index} className="categories__item">
+        <a data-index={category.id} onClick={handleSearch} key={category.id}>{category.name}</a>
       </li>
     );
   }
@@ -21,8 +33,11 @@ export default function Category() {
         <p className="categories__title">PRODUCT CATEGORIES</p>
         <ul className="categories__list">
           {renderCategoryList(categories as [])}
+          <li className="categories__item" onClick={handleDefaultCategory}>Default</li>
         </ul>
       </div>
     </>
   )
 }
+
+export default Category;
