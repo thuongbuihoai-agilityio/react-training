9c2c0465d3ba183b2302c46e926b85b4eedf2af6
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-05-05 17:00:12 +0700

    Handle filter

--- practice-2/practice_2/src/components/Filter/Filter/Filter.tsx
+++ practice-2/practice_2/src/components/Filter/Filter/Filter.tsx
@@ -1,20 +1,23 @@
 import React from "react";
-import { DEFAULT_CATEGORY, DEFAULT_FILTERER } from "../../../constants/categories";
+import { DEFAULT_FILTERER } from "../../../constants/categories";
 import Label from "../../common/Label/Label";
 import Category from "../../Categories/Categories";
 import FilterBy from "../FilterBy/FilterBy";
 import "./filter.css"
+import { FilterByCategory } from "../../../types/filter";
 
-export default function Filter({}) {
+const Filter: React.FC<FilterByCategory> = ({ setFilterInput }) => {
   return (
     <div className="filter">
       <Label className="filter" value="Filter" />
       <input type="checkbox" hidden className="filter__input" id="filter__check" />
       <label htmlFor="filter__check" className="filter__overlay"></label>
       <div className="filters">
-        <Category categoriesList={DEFAULT_CATEGORY} />
+        <Category setFilterInput={setFilterInput}/>
         <FilterBy filterBy={DEFAULT_FILTERER} />
       </div>
     </div>
   )
 }
+
+export default Filter;
