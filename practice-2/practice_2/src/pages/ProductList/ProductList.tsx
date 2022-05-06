09ae2ce4d2f93864b1be5890824748d0720d526a
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-05-06 17:19:10 +0700

    Handle edit task

--- practice-2/practice_2/src/pages/ProductList/ProductList.tsx
+++ practice-2/practice_2/src/pages/ProductList/ProductList.tsx
@@ -11,11 +11,9 @@ const ProductList: React.FC<ProductListProps> = ({products}) => {
     <div className="product__list">
       {products.map((product: ProductType, index: number) =>
         <div className="product__item" key={index}>
-          <ProductItem
-            product={product}
-          />
+          <ProductItem product={product} />
           <div className="product__content">
-            <Title className={""} value={product.name} />
+            <Title value={product.name} />
             <Price value={product.price.toString()} />
           </div>
         </div>
