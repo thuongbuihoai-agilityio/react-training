71a54650a1f6a0c9bdbfb306d30bcd5b6f59244d
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-05-09 17:36:54 +0700

    Change value to text

---
+++ practice-2/practice_2/src/components/Button/Button.tsx
@@ -0,0 +1,14 @@
+import React, { memo } from "react";
+import { ButtonProps } from "../../types/commonType/button";
+import "./button.css";
+
+const Button: React.FC<ButtonProps> = ({ text, className }) => {
+  return (
+    <button
+      className={`btn btn__${className}`}>
+      {text}
+    </button>
+  );
+}
+
+export default memo(Button);
