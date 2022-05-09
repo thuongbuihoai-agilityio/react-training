086f1be43ca0ef515134ea1491e60a6fbb6f2d2d
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-05-09 17:38:53 +0700

    Change file name and delete isReset

---
+++ practice-2/practice_2/src/components/InputSearch/InputSearch.tsx
@@ -0,0 +1,26 @@
+import React, { memo, useContext } from "react";
+import { ProductListContext } from "../../App";
+import { SearchProps } from "../../types/search";
+import "./inputSearch.css";
+
+const InputSearch: React.FC<SearchProps> = ({ setFilterInput }) => {  
+  const setIsReset = useContext(ProductListContext) as Function;
+  const handleSearch = (e: { target: { value: string } }) => {
+    const nameLike = {name_like:e.target.value};
+    setFilterInput(nameLike);
+    setIsReset(true);
+  }
+
+  return (
+    <div className="search">
+      <input
+        onChange={handleSearch}
+        type="text"
+        className="search__text"
+        placeholder="Search item"
+      />
+    </div>
+  );
+}
+
+export default memo(InputSearch);
