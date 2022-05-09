a4f55abab95b6382b60fb9d78448189963dc2a15
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-05-09 17:40:11 +0700

    Create component navigation

---
+++ practice-2/practice_2/src/components/Navigation/Navigation.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { SearchByName } from "../../types/search";
+import InputSearch from "../InputSearch/InputSearch";
+import "./navigation.css";
+
+const Navigation: React.FC<SearchByName> = ({ setFilterInput }) => {
+  return (
+    <nav className="nav">
+      <ul className="nav__list">
+        <li className="nav__item">
+          <a href="" className="nav__link"> <i className="fa fa-clock"></i> Mon - Sat: 8AM - 5PM</a>
+        </li>
+        <li className="nav__item">
+          <a href="" className="nav__link"> <i className="fa fa-phone"></i> 123 456 7890</a>
+        </li>
+      </ul>
+      <InputSearch setFilterInput={setFilterInput} />
+    </nav>
+  );
+}
+
+export default Navigation;
