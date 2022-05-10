69f32995765ad12a19bac843175f0ed85fb29f5f
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-05-10 17:48:36 +0700

    Init app

--- practice-2/practice_2/src/App.tsx
+++ practice-2/practice_2/src/App.tsx
@@ -1,29 +1,15 @@
-import { createContext, useState } from "react";
-import { Routes, Route } from "react-router-dom";
-import SelectByCategory from "./components/Selects/SelectByCategory/SelectByCategory";
-import Header from "./components/Header/Header";
-import url from "./assets/images/appleIceCream.jpg";
-import Navigation from "./components/Navigation/Navigation";
-import ProductDetails from "./pages/ProductDetails/ProductDetails";
-import { USERS } from "./constants/header";
-import ProductList from "./pages/ProductList/ProductList";
-import Footer from "./components/Footer/Footer";
-import useProduct from "./hooks/useProduct";
+import Header from "@/components/Header/Header";
+import url from "@/assets/images/appleIceCream.jpg";
+import { USERS } from "@/constants/header";
+import Footer from "@/components/Footer/Footer";
+import MainContext from "@/context/MainContext";
 
-export const ProductListContext = createContext({});
 const App: React.FC = () => {
-  const [filterInput, setFilterInput] = useState("");
-  const products = useProduct(filterInput);
 
   return (
     <>
-      <Navigation setFilterInput={setFilterInput} />
       <Header username={USERS.username} image={url} />
-      <SelectByCategory setFilterInput={setFilterInput} />
-      <Routes>
-        <Route path="/" element={<ProductList products={products} />}/>
-        <Route path="/detail" element={<ProductDetails />}/>
-      </Routes>
+      <MainContext />
       <Footer />
     </>
   );
