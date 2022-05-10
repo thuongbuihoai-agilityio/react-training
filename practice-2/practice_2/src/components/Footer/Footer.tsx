4edafde1604fda411fce77c45d58825d91b68ac4
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-05-10 17:45:59 +0700

    Review code style

--- practice-2/practice_2/src/components/Footer/Footer.tsx
+++ practice-2/practice_2/src/components/Footer/Footer.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./footer.css";
 
 const Footer: React.FC = () => {
+
   return (
     <footer className="footer">
       <p className="footer__contact">Copyright © 2022 — Powered by JustShoppe</p>
