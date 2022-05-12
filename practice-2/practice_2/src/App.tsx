04f61bebc9143cb1d1a26e9ef2392cb7caae1602
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-05-12 16:33:57 +0700

    Refactor: Code flow reorganization

--- practice-2/practice_2/src/App.tsx
+++ practice-2/practice_2/src/App.tsx
@@ -2,14 +2,25 @@ import Header from "@/components/Header/Header";
 import url from "@/assets/images/appleIceCream.jpg";
 import { USERS } from "@/constants/header";
 import Footer from "@/components/Footer/Footer";
-import MainContext from "@/context/MainContext";
+import Navigation from "./components/Navigation/Navigation";
+import SelectByCategory from "./components/Selects/SelectByCategory/SelectByCategory";
+import { Route, Routes } from "react-router-dom";
+import ProductList from "./pages/ProductList/ProductList";
+import ProductDetails from "./pages/ProductDetails/ProductDetails";
+import MainContext from "./context/LoadingContext";
 
 const App: React.FC = () => {
-
   return (
     <>
       <Header username={USERS.username} image={url} />
-      <MainContext />
+      <MainContext>
+        <Navigation />
+        <SelectByCategory />
+        <Routes>
+          <Route path="/" element={<ProductList />} />
+          <Route path=":productId" element={<ProductDetails />} />
+        </Routes>
+      </MainContext>
       <Footer />
     </>
   );
