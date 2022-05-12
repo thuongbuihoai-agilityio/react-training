51548ecbd2dd1af5dc1cbfd3eaa1a2f374dcf36e
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-05-12 16:50:17 +0700

    Refactor: Use base64 update image

--- practice-2/practice_2/src/components/Modal/ModelUpdate/ModalUpdate.tsx
+++ practice-2/practice_2/src/components/Modal/ModelUpdate/ModalUpdate.tsx
@@ -1,40 +1,37 @@
 import axios from "axios";
 import React, { useState } from "react";
-import { useLocation } from "react-router-dom";
 import useCategory from "@/hooks/useCategory";
 import { CategoriesProps } from "@/types/categories";
-import { ProductUpdateProps, ProductProps } from "@/types/product";
 import { SUCCESS_MSG } from "@/constants/message";
 import { ModalUpdateProps } from "types/modal";
 import { PRODUCT_CRUD } from "@/constants/url";
 import "./modalUpdate.css";
+import getBase64 from "@/helpers/getBase64";
 
-const ModalUpdate: React.FC<ModalUpdateProps> = ({ id, hideModalUpdate, onChangeProductDetail }) => {
-  const location = useLocation();
-  const product: ProductProps = (location.state as ProductUpdateProps).product;
-  const [productEdit, setProductEdit] = useState<ProductProps>(product);
+const ModalUpdate: React.FC<ModalUpdateProps> = ({ product, hideModalUpdate, onChangeProductDetail }) => {
+  const [productEdit, setProductEdit] = useState(product);
 
   const categories = useCategory();
   const [selectedFile, setSelectedFile] = useState([]);
 
   const handleUpdateProduct = async (id: string) => {
     for (let i = 0; i < selectedFile.length; i++) {
-      productEdit.images.push("src/assets/images/" + selectedFile[i].name);
+      productEdit.images.push(selectedFile[i]);
     }
-      await axios ({
-        method: "put",
-        url: PRODUCT_CRUD + id,
-        data:{...productEdit},
-      })
-      .then(function (response) {
-        onChangeProductDetail(response.data);
-        alert(SUCCESS_MSG.MESSAGE_UPDATE_PRODUCT);
-        hideModalUpdate(false);
-      })
-      .catch(function (error) {
-        alert(error);
-      });
-    }
+    await axios ({
+      method: "put",
+      url: PRODUCT_CRUD + id,
+      data:{...productEdit},
+    })
+    .then(function (response) {
+      onChangeProductDetail(response.data);
+      alert(SUCCESS_MSG.MESSAGE_UPDATE_PRODUCT);
+      hideModalUpdate(false);
+    })
+    .catch(function (error) {
+      alert(error);
+    });
+  }
 
   const handleChange = (event: { target: { value: {}; name: string; } }) => {
     const value = event.target.value;
@@ -42,13 +39,21 @@ const ModalUpdate: React.FC<ModalUpdateProps> = ({ id, hideModalUpdate, onChange
     setProductEdit({...productEdit, [key]: value});
   }
 
-  const imageChange = (event: React.ChangeEvent) => {
-    const target= event.target as HTMLInputElement;
-    if(target.files) {
-      for (let i = 0; i < target.files.length; i++) {
-        setSelectedFile(selectedFile => [...selectedFile, target.files[i]]);
+  const imageChange = async (event: { target: { files: any; }; }) => {
+    const files = event.target.files;
+    if(files) {
+      for (let i = 0; i < files.length; i++) {
+        const imageSrc = await getBase64(files[i]);
+        setSelectedFile(selectedFile=>[...selectedFile, imageSrc])
       }
+      console.log("selected file", selectedFile);
     }
+  }
+
+  const handleDeleteImage = (event: React.ChangeEvent) => {
+    const target = event.target as Element;
+    const indexOfArr = productEdit.images.findIndex(item=> item == (target as HTMLImageElement).dataset.id);
+    productEdit.images.splice(indexOfArr, 1);
   }
 
   return (
@@ -88,14 +93,14 @@ const ModalUpdate: React.FC<ModalUpdateProps> = ({ id, hideModalUpdate, onChange
             <div className="form-control">
               <div className="form__img--list">
                 {product.images.map((img: string, index: number) =>
-                  <img key={index} className="form__img" src={img} />
+                  <img key={index} data-id={img} className="form__img" onClick={handleDeleteImage} src={img} />
                 )}
-                <input type="file" id="file" multiple name="images" onChange={imageChange} className="form__input--img"/>
+                <input type="file" id="file" onChange={imageChange} multiple name="images" className="form__input--img"/>
               </div>
             </div>
           </div>
           <div className="modal-footer">
-            <button className="btn btn__submit" onClick={() => handleUpdateProduct(id)}>Submit</button>
+            <button className="btn btn__submit" onClick={() => handleUpdateProduct(product.id)}>Submit</button>
             <button className="btn btn__cancel" onClick={() => hideModalUpdate(false)}>Cancel</button>
           </div>
         </div>
