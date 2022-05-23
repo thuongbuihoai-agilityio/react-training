89261f344ce4848a0ef0119386b277052db03cf5
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-05-23 17:18:55 +0700

    Example: Init app and use SWRConfig

--- Learning_React/my-react-app/src/Example-SWR/TodoApp.tsx
+++ Learning_React/my-react-app/src/Example-SWR/TodoApp.tsx
@@ -1,15 +1,27 @@
-import { Route, Routes, useRoutes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import { SWRConfig } from "swr";
 import About from "./components/About/About";
 import Contact from "./components/Contact/Contact";
 import Home from "./components/Home/Home";
+import TodoDetail from "./Todos/TodoDetail/TodoDetail";
+import TodoListSWR from "./Todos/TodoList/TodoList";
+
+const swrConfig = {
+  revalidateOnFocus: false,
+  shouldRetryOnError: true
+};
 
 const TodoAppSWR: React.FC = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="about" element={<About />} />
-      <Route path="contact" element={<Contact />} />
-    </Routes>
+    <SWRConfig value={swrConfig}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="todos" element={<TodoListSWR />}/>
+        <Route path={`/todo/:id`} element={<TodoDetail />} />
+      </Routes>
+    </SWRConfig>
   )
 }
 
