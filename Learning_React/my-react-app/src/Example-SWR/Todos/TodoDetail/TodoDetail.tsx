322d91d61f622ea75b932301e164dc24655c1558
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-05-23 17:16:42 +0700

    Example: Create component todo detail and apply useParams

---
+++ Learning_React/my-react-app/src/Example-SWR/Todos/TodoDetail/TodoDetail.tsx
@@ -0,0 +1,51 @@
+import axios from "axios";
+import { useCallback, useEffect, useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import { TASK_URL } from "../../constants/url";
+import { TodoProps } from "../../types/todo";
+import ModalUpdate from "../Modal/ModalUpdate";
+
+const TodoDetail: React.FC = () => {
+  const location = useLocation();
+  const { id } = useParams();
+  const { todo } = location.state as TodoProps;
+
+  const [todoDetailNew, setTodoDetailNew] = useState(todo);
+  const [openModalUpdate, setOpenModalUpdate] = useState(false);
+  const [isReload, setIsReLoad] = useState(false);
+
+  async function fetchMyAPI() {
+    const result = await axios
+    .get(`${TASK_URL}/${id}`)
+    .then((res) => {
+      setTodoDetailNew(res.data);
+    })
+    .catch(function (error) {
+      alert(error);
+    });
+  }
+
+  useEffect(() => {
+    if(todo.id) {
+      fetchMyAPI();
+    }
+  }, [isReload]);
+
+  const handleOpenModalUpdate = useCallback(() => {
+    setOpenModalUpdate(true)
+  }, []);
+
+  return (
+    <div className="todo__item">
+      <ul className="detail__list">
+        <li className="detail__item">
+          {todoDetailNew.title}
+        </li>
+        <button onClick={handleOpenModalUpdate} className="btn btn__update">Update</button>
+      </ul>
+      {openModalUpdate && <ModalUpdate isReload={isReload} setIsReLoad={setIsReLoad} todo={todoDetailNew} hideModalUpdate={setOpenModalUpdate} />}
+    </div>
+  )
+}
+
+export default TodoDetail;
