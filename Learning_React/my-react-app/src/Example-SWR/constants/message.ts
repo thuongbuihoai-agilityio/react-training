b796f39c9ef99fa0d58eca58202083e0a4ee59bc
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-05-23 17:10:13 +0700

    Example: Create message

---
+++ Learning_React/my-react-app/src/Example-SWR/constants/message.ts
@@ -0,0 +1,7 @@
+const SUCCESS_MSG = {
+  MESSAGE_ADD_TODO: "Todo add successful",
+  MESSAGE_UPDATE_TODO: "Todo update successful",
+  MESSAGE_DELETE_TODO: "Todo delete successful",
+};
+
+export { SUCCESS_MSG };
