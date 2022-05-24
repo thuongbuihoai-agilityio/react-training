035619d4fd95a884256d68eb390fbcb4c3a6aa8f
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-05-24 17:19:18 +0700

    Example: Writing unit test for home component

--- Learning_React/my-react-app/src/Example-SWR/components/Home/Home.tsx
+++ Learning_React/my-react-app/src/Example-SWR/components/Home/Home.tsx
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom";
 
 const Home: React.FC = () => {
   return (
-    <div className="todo">
+    <div className="todo" data-testid="home">
       <nav className="nav">
-        <Link className="nav__item" to="about">About</Link>
-        <Link className="nav__item" to="contact">Contact</Link>
-        <Link className="nav__item" to="todos">Todo List</Link>
+        <Link className="nav__item" to="/about" data-testid="about">About</Link>
+        <Link className="nav__item" to="/contact">Contact</Link>
+        <Link className="nav__item" to="/todos">Todo List</Link>
       </nav>
     </div>
   );
---
+++ Learning_React/my-react-app/src/Example-SWR/components/Home/__test__/Home.test.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import {render} from "@testing-library/react";
+import "@testing-library/jest-dom";
+import {createMemoryHistory} from "history";
+import {Router} from "react-router-dom";
+import Home from "../Home";
+
+describe("Home component", () => {
+  test("rendering/navigating", async () => {
+    const history = createMemoryHistory();
+    const {getByTestId} = render(
+      <Router location={history.location} navigator={history}>
+        <Home />
+      </Router>
+    )
+    expect(getByTestId("home")).toBeInTheDocument();
+  })
+})
