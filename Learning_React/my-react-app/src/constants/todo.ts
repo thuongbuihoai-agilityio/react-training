50d8cbbd08b55bb8d0fcc2ca9db3bb87edac6a5c
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-05-25 17:29:32 +0700

    Example: Create todo constants

---
+++ Learning_React/my-react-app/src/constants/todo.ts
@@ -0,0 +1,13 @@
+import { TodoProps } from "../Example-SWR/types/todo";
+
+const TODO_CONSTANTS: TodoProps = {
+  todo: {
+    id: "a56403e9-8163-451c-a17b-6bd2fa3f389a",
+    title: "Example create Todo App (20220522)",
+  },
+  deleteTodo: () => {}
+};
+
+export {
+  TODO_CONSTANTS
+}
