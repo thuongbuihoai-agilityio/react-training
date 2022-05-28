700d52a3c03c2c5f35f668f839033a6d2ee3115d
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-05-28 22:51:11 +0700

    Create storybook: Logo

---
+++ practice-3/src/stories/Logo.stories.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Logo from "../components/Logo/Logo";
+import url from "../assets/images/logos/logos.png"
+
+export default {
+  /* 👇 The title prop is optional.
+  * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+  * to learn how to generate automatic titles
+  */
+  title: "Component/Logo",
+  component: Logo,
+};
+
+const Default: React.FC = () => {
+  return <Logo src={url} />;
+}
+
+export { Default };
