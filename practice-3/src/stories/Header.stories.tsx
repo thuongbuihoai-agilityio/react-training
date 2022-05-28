b300d53872c9bfe529758bbc143f0c8b1d094e51
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-05-28 22:50:50 +0700

    Create storybook: Header

---
+++ practice-3/src/stories/Header.stories.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "../components/Header/Header";
+
+export default {
+  /* 👇 The title prop is optional.
+  * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+  * to learn how to generate automatic titles
+  */
+  title: "Component/Header",
+  component: Header,
+};
+
+const Default: React.FC = () => {
+  return <Header />;
+}
+
+export { Default };
