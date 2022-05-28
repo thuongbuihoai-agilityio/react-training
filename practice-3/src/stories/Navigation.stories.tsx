9caf0c11a6cb2af4d9346eb6e75a23b43999cb71
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-05-28 22:51:33 +0700

    Create storybook: Navigation

---
+++ practice-3/src/stories/Navigation.stories.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Navigation from "../components/Navigation/Navigation";
+
+export default {
+  /* 👇 The title prop is optional.
+  * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+  * to learn how to generate automatic titles
+  */
+  title: "Component/Navigation",
+  component: Navigation,
+};
+
+const Default: React.FC = () => {
+  return <Navigation />;
+}
+
+export { Default };
