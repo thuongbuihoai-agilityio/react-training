1641b89590681850027644a54619b25206994591
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-05-28 20:15:14 +0700

    Implement filter prododuct by category and search input

--- practice-3/src/components/Input/InputSearch/InputSearch.tsx
+++ practice-3/src/components/Input/InputSearch/InputSearch.tsx
@@ -1,17 +1,18 @@
+import { FilterContext } from "../../../context/FilterContext";
 import React, { memo, useContext } from "react";
 import "./inputSearch.css";
 
 const InputSearch: React.FC = () => {  
-  // const {setIsReload, setFilterInput} = useContext(LoadingContext);
-  // const handleSearch = (e: { target: { value: string } }) => {
-  //   const nameLike = {name_like:e.target.value};
-  //   setFilterInput(nameLike);
-  //   setIsReload(true);
-  // }
+  const {setFilterInput} = useContext(FilterContext);
+  const handleSearch = (e: { target: { value: string } }) => {
+    const nameLike = {name_like:e.target.value};
+    setFilterInput(nameLike);
+  }
 
   return (
     <div className="search">
       <input
+        onChange={handleSearch}
         type="text"
         className="search__text"
         placeholder="Search item"
