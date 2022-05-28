22fcac59bbffb0b7d36745b72d987de8282de6b6
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-05-28 09:09:43 +0700

    Create menu constants

---
+++ practice-3/src/constants/menu.ts
@@ -0,0 +1,30 @@
+import { MenuType } from "../types/menu";
+
+const MENU_LIST: MenuType[] = [
+  {
+    key: "home",
+    label: "Home",
+  },
+  {
+    key: "about",
+    label: "About",
+  },
+  {
+    key: "shop",
+    label: "Shop",
+  },
+  {
+    key: "blog",
+    label: "Blog",
+  },
+  {
+    key: "faq",
+    label: "FAQ",
+  },
+  {
+    key: "contact",
+    label: "Contact"
+  }
+];
+
+export default MENU_LIST;
