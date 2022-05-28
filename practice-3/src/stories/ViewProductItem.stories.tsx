e8628834b0c72dd03ce6703b869fc2fe5be3f4c2
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-05-28 22:52:10 +0700

    Create storybook: View product item

---
+++ practice-3/src/stories/ViewProductItem.stories.tsx
@@ -0,0 +1,25 @@
+import PRODUCT_CONTENT from "../constants/product";
+import React from "react";
+import ViewProductItem from "../components/ViewsProducts/ViewProductItem/ViewProductsItem";
+import { Route, Routes } from "react-router-dom";
+
+export default {
+  /* 👇 The title prop is optional.
+  * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+  * to learn how to generate automatic titles
+  */
+  title: "Component/ViewProductItem",
+  component: ViewProductItem,
+};
+
+const Default: React.FC = () => {
+  return (
+    <Routes>
+      <Route element={<ViewProductItem product={PRODUCT_CONTENT} deleteProduct={function (id: string): void {
+      throw new Error("Function not implemented.");
+    } } />} />
+    </Routes>
+  );
+}
+
+export { Default };
