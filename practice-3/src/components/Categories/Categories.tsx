f78feea2665f993437758a356ad8475c7a0b5dcb
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-05-28 22:52:25 +0700

    Create storybook

--- practice-3/src/components/Categories/Categories.tsx
+++ practice-3/src/components/Categories/Categories.tsx
@@ -1,27 +1,29 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import useCategories from "../../hooks/useCategories";
 import { CategoryProps } from "../../types/category";
-import "./categories.css";
 import { FilterContext } from "../../context/FilterContext";
+import "./categories.css";
 
 const Category: React.FC = () => {
+  const [activeId, setActiveId] = useState("");
   const {setFilterInput} = useContext(FilterContext);
-  const handleSearch = (e: React.MouseEvent<HTMLElement>) => {
+  const handleSearch = ( id: string) => (e: React.MouseEvent<HTMLElement>) => {
     const categoryId = {categoryId : e.currentTarget.dataset.index};
-    console.log("category", categoryId);
-    
     setFilterInput(categoryId);
+    setActiveId(id);
   }
 
   const handleDefaultCategory = () => {
     const categoryId = "";
     setFilterInput(categoryId);
+    setActiveId("");
   }
 
   const {categories} = useCategories();
   function renderCategoryList(categories: []) {
     return categories?.map((category: CategoryProps) =>
-      <li data-index={category.id} onClick={handleSearch} key={category.id} className="categories__item">
+      <li data-index={category.id} onClick={handleSearch(category.id)} key={category.id}
+        className={`categories__item ${activeId === category.id ? "active" : "inactive"}`}>
         {category.name}
       </li>
     );
@@ -32,8 +34,9 @@ const Category: React.FC = () => {
       <div className="categories">
         <p className="categories__title">What are you looking for here?</p>
           <ul className="categories__list">
-            <li onClick={handleDefaultCategory} className="categories__item">All</li>
-            {renderCategoryList(categories as [])}
+            <li onClick={handleDefaultCategory}
+              className={`categories__item ${activeId === "" ? "active" : "inactive"}`}>All</li>
+              {renderCategoryList(categories as [])}
           </ul>
       </div>
     </>
---
+++ practice-3/src/constants/product.ts
@@ -0,0 +1,17 @@
+import { Product } from "types/product";
+
+const PRODUCT_CONTENT: Product = {
+  id: "16519992102442282747",
+  categoryId: "1651924356334",
+  name: "Cheese cake",
+  price: 20,
+  quantity: 5,
+  description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.",
+  images: [
+    "/src/assets/images/products/cheese-cake.jpg",
+    "/src/assets/images/products/cheese-cake.jpg",
+    "/src/assets/images/products/cheese-cake.jpg"
+  ]
+}
+
+export default PRODUCT_CONTENT;
