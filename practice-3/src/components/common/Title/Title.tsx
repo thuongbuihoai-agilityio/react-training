8bbb8648f8e8b428ca9f42a93702193707a5639d
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-05-30 10:17:28 +0700

    Refactor: apply alias path react

--- practice-3/src/components/common/Title/Title.tsx
+++ practice-3/src/components/common/Title/Title.tsx
@@ -1,8 +1,9 @@
-import { TitleProps } from "@/types/title";
 import React, { memo } from "react";
+import { TitleProps } from "@/types/title";
 import "./title.css";
 
 const Title: React.FC<TitleProps> = ({ text, className }) => {
+
   return (
     <h2 className={className}>
       {text}
