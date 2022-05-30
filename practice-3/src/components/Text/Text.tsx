1c7e14d98535fd03ac81dc0aaf0ea8e948be7615
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-05-30 10:19:22 +0700

    Refactor: apply alias path react and delete line

--- practice-3/src/components/Text/Text.tsx
+++ practice-3/src/components/Text/Text.tsx
@@ -1,8 +1,9 @@
-import { TitleProps } from "@/types/title";
 import React, { memo } from "react";
+import { TitleProps } from "@/types/title";
 import "./text.css";
 
 const Text: React.FC<TitleProps> = ({ text, className }) => {
+
   return (
     <p className={className}>
       {text}
