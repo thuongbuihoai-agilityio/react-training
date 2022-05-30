dc00b313f08b6afba4a4d5c62c03adfb7e281dbb
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-05-30 10:15:01 +0700

    Refactor: apply alias path react and endline

--- practice-3/src/hooks/useCategories.ts
+++ practice-3/src/hooks/useCategories.ts
@@ -1,7 +1,6 @@
-import { CATEGORIES_URL } from "../constants/url";
-import { get } from "../helpers/fetchApi";
 import useSWR from "swr";
-
+import { CATEGORIES_URL } from "@/constants/url";
+import { get } from "@/helpers/fetchApi";
 
 export default function useCategories() {
   const { data, error } = useSWR(CATEGORIES_URL, get, { refreshInterval: 1000 });
