7855a58e25b4481bbdb1bf97475683822203c4d2
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-06-03 09:40:40 +0700

    Writing unit test: Component modal delete

--- practice-3/src/components/Modal/ModalDelete/ModalDelete.tsx
+++ practice-3/src/components/Modal/ModalDelete/ModalDelete.tsx
@@ -1,19 +1,18 @@
 import React from "react";
-import Button from "@/components/common/Button/Button";
 import { ModalDeleteProps } from "@/types/modal";
 import "./modalDelete.css";
 
 const ModalDelete: React.FC<ModalDeleteProps> = ({ id, hideModalDelete, deleteProduct }) => {
   return (
-    <div className="modal-delete" id="showModal">
+    <div data-testid="modal-delete" className="modal-delete" id="showModal">
       <div className="modal-dialog">
         <div className="modal-content">
           <div className="modal-body">
             <span className="modal-title">Are you sure to delete this product?</span>
           </div>
           <div className="modal-footer">
-            <Button className="btn btn__no" onClick={() => hideModalDelete(false)} text="No" />
-            <Button className="btn btn__yes" onClick={() => deleteProduct(id)} text="Yes" />
+            <button data-testid="btn-no" className="btn btn__no" onClick={() => hideModalDelete(false)}>No</button>
+            <button data-testid="btn-yes" className="btn btn__yes" onClick={() => deleteProduct(id)}>Yes</button>
           </div>
         </div>
       </div>
