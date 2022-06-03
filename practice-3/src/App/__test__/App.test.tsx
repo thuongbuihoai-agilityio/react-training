b3138a4b42cb26baf4fe6f8ce8e4009baff1b63f
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-06-03 09:39:35 +0700

    Writing unit test: App

--- practice-3/src/App/__test__/App.test.tsx
+++ practice-3/src/App/__test__/App.test.tsx
@@ -1,10 +1,32 @@
+import { useState } from "react";
 import {render, screen} from "@testing-library/react";
 import {createMemoryHistory} from "history";
 import { Router } from "react-router-dom";
 import "@testing-library/jest-dom";
+import { PRODUCT_MOCKING_LIST } from "@/constants/product";
+import { CATEGORY_MOCKING_LIST } from "@/constants/categories";
 import App from "../App";
 
+const useProductsMock = { products: PRODUCT_MOCKING_LIST };
+  jest.mock("../../hooks/useProducts.ts", () => ({
+    default: jest.fn(() => useProductsMock),
+}));
+
+const useCategoriesMock = { categories: CATEGORY_MOCKING_LIST };
+  jest.mock("../../hooks/useCategories.ts", () => ({
+    default: jest.fn(() => useCategoriesMock),
+}));
+
+jest.mock("react", () => ({
+  ...jest.requireActual("react"),
+  useState: jest.fn(),
+}));
+
 describe("App component", () => {
+  beforeEach(()=>{
+    (useState as jest.Mock).mockImplementation(jest.requireActual("react").useState);
+  })
+
   test("rendering a view product list component that uses useLocation", () => {
     const history = createMemoryHistory();
     const route = "/";
