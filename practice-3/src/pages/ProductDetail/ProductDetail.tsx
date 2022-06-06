3a434dc2b6d1efbe9e6ffb91358023bf9f1106dc
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-06-06 17:24:29 +0700

    Review use swr, context, update state

--- practice-3/src/pages/ProductDetail/ProductDetail.tsx
+++ practice-3/src/pages/ProductDetail/ProductDetail.tsx
@@ -1,59 +1,84 @@
-import React, { useCallback, useState } from "react";
-import { useLocation } from "react-router-dom";
+import React, { useCallback, useContext, useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import Price from "@/components/Price/Price";
 import Button from "@/components/common/Button/Button";
 import Title from "@/components/common/Title/Title";
 import Text from "@/components/Text/Text";
-import { Product, ProductUpdateProps } from "@/types/product";
 import ModalUpdate from "@/components/Modal/ModalUpdate/ModalUpdate";
 import "./productDetail.css";
+import axios from "axios";
+import { PRODUCT_CRUD } from "@/constants/url";
+import { ProductContext } from "@/context/ProductContext";
 
 const ProductDetails: React.FC = () => {
-  const location = useLocation();
-  const { product } = location.state as ProductUpdateProps;
-
+  const { data } = useContext(ProductContext);
+  const { id } = useParams();
+  const [isReload, setIsReLoad] = useState(false);
+  const dataEl = data.find((item: { id: string }) => item.id === id);
+  const [productDetailNew, setProductDetailNew] = useState(dataEl);
   const [openModalUpdate, setOpenModalUpdate] = useState(false);
 
-  const handleOpenModalUpdate = useCallback(() => {
-    setOpenModalUpdate(true);
-  }, []);
+  const toggleModalUpdate = useCallback(() => {
+    setOpenModalUpdate(!openModalUpdate);
+  }, [openModalUpdate]);
 
-  const handleChangeProductDetail = (newProduct: {}) => {
-    (location.state as Product).product = newProduct;
+  async function fetchMyAPI() {
+    const result = await axios
+    .get(`${PRODUCT_CRUD}/${id}`)
+    .then((res) => {
+      setProductDetailNew(res.data);
+    })
+    .catch(function (error) {
+      alert(error);
+    });
   }
+
+  useEffect(() => {
+    if(id) {
+      fetchMyAPI();
+    }
+  }, [isReload]);
 
   return (
     <>
       <div data-testid="product-detail-page" className="productDetails">
         <div className="productDetails__img--left">
-          <img
-            className="product__image"
-            src={product.images[0]}
-          />
+          <img className="product__image" src={productDetailNew?.images[0]} />
         </div>
         <div className="productDetails__img--right">
-          {product.images.map((img: string, index: number) =>
-            <img
-              key={index}
-              src={img}
-              alt="This is image"
-            />
-            )
-          }
+          {productDetailNew?.images.map((img: string, index: number) => (
+            <img key={index} src={img} alt="This is image" />
+          ))}
         </div>
         <div className="productDetails__info">
           <div className="productDetail__update">
-            <Title className="productDetail__title" text={product.name} />
-            <Button onClick={handleOpenModalUpdate} className="btn btn__update" text={<i className="fa fa-pen"></i>} />
+            <Title className="productDetail__title" text={productDetailNew.name} />
+            <Button
+              onClick={toggleModalUpdate}
+              className="btn btn__update"
+              text={<i className="fa fa-pen"></i>}
+            />
           </div>
-            <Price className="productDetail__price" value={product.price} />
-          <input className="productDetails__input" min={0} type="number" value={product.quantity} />
-          <Text text={product.description} />
+          <Price className="productDetail__price" value={productDetailNew.price} />
+          <input
+            className="productDetails__input"
+            min={0}
+            type="number"
+            value={productDetailNew.quantity}
+          />
+          <Text text={productDetailNew.description} />
         </div>
       </div>
-      {openModalUpdate && <ModalUpdate product={product} onChangeProductDetail={handleChangeProductDetail} hideModalUpdate={setOpenModalUpdate} />}
+      {openModalUpdate && (
+        <ModalUpdate
+          product={productDetailNew}
+          isReload={isReload}
+          setIsReLoad={setIsReLoad}
+          hideModalUpdate={toggleModalUpdate}
+        />
+      )}
     </>
   );
-}
+};
 
 export default ProductDetails;
