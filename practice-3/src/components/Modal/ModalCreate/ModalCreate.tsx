9f68d2a72237ac154afba8c44aed314fd694afed
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-06-07 17:05:12 +0700

    Refactor: Format code and add testing for button submit

--- practice-3/src/components/Modal/ModalCreate/ModalCreate.tsx
+++ practice-3/src/components/Modal/ModalCreate/ModalCreate.tsx
@@ -8,25 +8,30 @@ import { validate } from "@/helpers/validate";
 import { setFieldsValue } from "@/helpers/index";
 import { Product } from "@/types/product";
 import { FormProps } from "@/types/form";
-import "./modalCreate.css";
 import useSWR, { Key } from "swr";
 import { CATEGORIES_URL } from "@/constants/url";
 import { get } from "@/helpers/fetchApi";
+import "./modalCreate.css";
 
-const ModalCreate: React.FC<ModalCreateProps> = ({ createProduct, hideModalCreate }) => {
+const ModalCreate: React.FC<ModalCreateProps> = ({
+  createProduct,
+  hideModalCreate,
+}) => {
   const [newProduct, setNewProduct] = useState([]);
   const key: Key = CATEGORIES_URL;
-  const fetcher = () => get<Product[]>(CATEGORIES_URL)
+  const fetcher = () => get<Product[]>(CATEGORIES_URL);
   const { data } = useSWR(key, fetcher);
   const [selectedFile, setSelectedFile] = useState([]);
   const [formValues, setFormValues] = useState<FormProps>(FORM_VALUES);
 
-  const handleClearModal = () => {
-    (Object.keys(formValues) as (keyof typeof formValues)[]).map(fieldName => {
-      formValues[fieldName].error = "";
-    })
-    hideModalCreate(false);
-  }
+  const handleClearValidate = () => {
+    (Object.keys(formValues) as (keyof typeof formValues)[]).map(
+      (fieldName) => {
+        formValues[fieldName].error = "";
+      }
+    );
+    hideModalCreate();
+  };
 
   const handleCreateProduct = () => {
     const images: string[] = [];
@@ -34,101 +39,176 @@ const ModalCreate: React.FC<ModalCreateProps> = ({ createProduct, hideModalCreat
       images.push(selectedFile[i]);
     }
     setFormValues(validate(formValues));
-    const temp = (Object.keys(formValues) as (keyof typeof formValues)[]).map(fieldName => {
-      if(formValues[fieldName].error){
-        return false;
+    const temp = (Object.keys(formValues) as (keyof typeof formValues)[]).map(
+      (fieldName) => {
+        if (formValues[fieldName].error) {
+          return false;
+        }
       }
-    })
+    );
 
-    if(!temp.includes(false)){
+    if (!temp.includes(false)) {
       createProduct({ images, ...newProduct } as unknown as Product);
-      hideModalCreate(false);
+      hideModalCreate();
     }
-  }
+  };
 
-  const handleChange = (event: { target: { value: string; name: string; } }) => {
+  const handleChange = (event: { target: { value: string; name: string } }) => {
     const value = event.target.value;
     const fieldName = event.target.name;
-    setNewProduct({...newProduct, [fieldName]: value});
+    setNewProduct({ ...newProduct, [fieldName]: value });
     setFormValues(setFieldsValue(formValues, value, fieldName) as any);
-  }
+  };
 
-  const imageChange = async (event: { target: { files: any; }; }) => {
+  const imageChange = async (event: { target: { files: any } }) => {
     const files = event.target.files;
-    if(files) {
+    if (files) {
       for (let i = 0; i < files.length; i++) {
         const imageSrc = await getBase64(files[i]);
-        setSelectedFile(selectedFile => [...selectedFile, imageSrc] as any)
+        setSelectedFile((selectedFile) => [...selectedFile, imageSrc] as any);
       }
     }
-  }
+  };
 
-  const handleDeleteImage = (event: { target: Element| any}) => {
+  const handleDeleteImage = (event: { target: Element | any }) => {
     const target = event.target as Element;
-    const indexOfArr = selectedFile.findIndex((item: string) =>
-      item == (target as HTMLInputElement).dataset.id);
-      selectedFile.splice(indexOfArr, 1);
-      setSelectedFile([...selectedFile]);
-  }
+    const indexOfArr = selectedFile.findIndex(
+      (item: string) => item == (target as HTMLInputElement).dataset.id
+    );
+    selectedFile.splice(indexOfArr, 1);
+    setSelectedFile([...selectedFile]);
+  };
 
   return (
-    <div data-testid="modal-create" className="modal-create" id="bookDeleteModal">
-    <div className="modal-dialog-modalUpdate">
-      <div className="modal-content">
-        <div className="modal-header">
-          <h4 className="modal-heading" id="productModalLabel">Add New Product Information</h4>
-        </div>
-        <div className="modal-body">
-          <div className="form-control">
-            <label htmlFor="">Product name: </label>
-            <Input className="form__input" type="text" onChange={handleChange} name="name" />
-            <small className="form__error">{formValues.name.error ? formValues.name.error : ""}</small>
+    <div
+      data-testid="modal-create"
+      className="modal-create"
+      id="bookDeleteModal"
+    >
+      <div className="modal-dialog-modalUpdate">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h4 className="modal-heading" id="productModalLabel">
+              Add New Product Information
+            </h4>
           </div>
-          <div className="form-control">
-            <label htmlFor="">Description: </label>
-            <textarea className="form__text" onChange={handleChange} name="description" id="" cols={30} rows={3}></textarea>
-            <small className="form__error">{formValues.description.error ? formValues.description.error : ""}</small>
-          </div>
-          <div className="form-control">
-            <label htmlFor="">Categories: </label>
-            <select className="form__select" name="categoryId" onChange={handleChange}>
-              {data?.map(({id, name}: CategoryProps, index: number) =>
-                <option key={index} value={id}>{name}</option>
-              )}
-            </select>
-            <small className="form__error">{formValues.categoryId.error ? formValues.categoryId.error : ""}</small>
-          </div>
-          <div id="form__number" className="form-control">
+          <div className="modal-body">
             <div className="form-control">
-              <label htmlFor="">Price: </label>
-              <Input className="modal__input" type="number" min={0} name="price" onChange={handleChange} />
-              <small className="form__error">{formValues.price.error ? formValues.price.error : ""}</small>
+              <label htmlFor="">Product name: </label>
+              <Input
+                className="form__input"
+                type="text"
+                name="name"
+                onChange={handleChange}
+              />
+              <small className="form__error">
+                {formValues.name.error ? formValues.name.error : ""}
+              </small>
             </div>
             <div className="form-control">
-              <label htmlFor="">Quantity: </label>
-              <Input className="modal__input" type="number" min={0} name="quantity" onChange={handleChange} />
-              <small className="form__error">{formValues.quantity.error ? formValues.quantity.error : ""}</small>
+              <label htmlFor="">Description: </label>
+              <textarea
+                className="form__text"
+                name="description"
+                id=""
+                cols={30}
+                rows={3}
+                onChange={handleChange}
+              ></textarea>
+              <small className="form__error">
+                {formValues.description.error
+                  ? formValues.description.error
+                  : ""}
+              </small>
+            </div>
+            <div className="form-control">
+              <label htmlFor="">Categories: </label>
+              <select
+                className="form__select"
+                name="categoryId"
+                onChange={handleChange}
+              >
+                {data?.map(({ id, name }: CategoryProps, index: number) => (
+                  <option key={index} value={id}>
+                    {name}
+                  </option>
+                ))}
+              </select>
+              <small className="form__error">
+                {formValues.categoryId.error ? formValues.categoryId.error : ""}
+              </small>
+            </div>
+            <div id="form__number" className="form-control">
+              <div className="form-control">
+                <label htmlFor="">Price: </label>
+                <Input
+                  className="modal__input"
+                  type="number"
+                  min={0}
+                  name="price"
+                  onChange={handleChange}
+                />
+                <small className="form__error">
+                  {formValues.price.error ? formValues.price.error : ""}
+                </small>
+              </div>
+              <div className="form-control">
+                <label htmlFor="">Quantity: </label>
+                <Input
+                  className="modal__input"
+                  type="number"
+                  min={0}
+                  name="quantity"
+                  onChange={handleChange}
+                />
+                <small className="form__error">
+                  {formValues.quantity.error ? formValues.quantity.error : ""}
+                </small>
+              </div>
+            </div>
+            <div className="form-control">
+              <div className="form__img--list">
+                {selectedFile.length > 0 &&
+                  selectedFile.map((src, key: number) => (
+                    <img
+                      className="form__img"
+                      key={key}
+                      data-id={src}
+                      src={src}
+                      onClick={handleDeleteImage}
+                    />
+                  ))}
+                <Input
+                  className="form__input--img"
+                  type="file"
+                  id="file"
+                  multiple
+                  name="images"
+                  onChange={imageChange}
+                />
+              </div>
             </div>
           </div>
-          <div className="form-control">
-            <div className="form__img--list">
-              {
-                (selectedFile.length > 0) && selectedFile.map((src, key: number) =>
-                  <img key={key} data-id={src} onClick={handleDeleteImage} className="form__img" src={src} />
-                )
-              }
-              <Input type="file" id="file" onChange={imageChange} multiple name="images" className="form__input--img"/>
-            </div>
+          <div className="modal-footer-modalUpdate">
+            <button
+              data-testid="hide-modal-btn"
+              className="btn btn__no"
+              onClick={handleClearValidate}
+            >
+              Cancel
+            </button>
+            <button
+              data-testid="submit-btn"
+              className="btn btn__yes"
+              onClick={() => handleCreateProduct()}
+            >
+              Submit
+            </button>
           </div>
-        </div>
-        <div className="modal-footer-modalUpdate">
-          <button data-testid="hide-modal-btn" className="btn btn__no" onClick={handleClearModal}>Cancel</button>
-          <button data-testid="submit-btn" className="btn btn__yes" onClick={() => handleCreateProduct()}>Submit</button>
         </div>
       </div>
     </div>
-  </div>
   );
-}
+};
 
 export default ModalCreate;
--- practice-3/src/components/Modal/ModalCreate/__test__/ModalCreate.test.tsx
+++ practice-3/src/components/Modal/ModalCreate/__test__/ModalCreate.test.tsx
@@ -1,19 +1,11 @@
 import { fireEvent, render } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import ModalCreate from "../ModalCreate";
-import { PRODUCT_MOCKING_LIST } from "@/constants/product";
-import { CATEGORY_MOCKING_LIST } from "@/constants/categories";
 import { useState } from "react";
-
-const useProductsMock = { products: PRODUCT_MOCKING_LIST };
-  jest.mock("../../../../hooks/useProducts.ts", () => ({
-    default: jest.fn(() => useProductsMock),
-}));
-
-const useCategoriesMock = { categories: CATEGORY_MOCKING_LIST };
-  jest.mock("../../../../hooks/useCategories.ts", () => ({
-    default: jest.fn(() => useCategoriesMock),
-}));
+import mockAxios from "@/__mocks__/axios";
+import { CATEGORY_MOCKING_LIST } from "@/constants/categories";
+import { CATEGORIES_URL } from "@/constants/url";
+import { get } from "@/helpers/fetchApi";
 
 jest.mock("react", () => ({
   ...jest.requireActual("react"),
@@ -21,25 +13,44 @@ jest.mock("react", () => ({
 }));
 
 describe("Modal create component", () => {
+  const createProduct = jest.fn();
   const hideModalCreate = jest.fn();
   beforeEach(()=>{
     (useState as jest.Mock).mockImplementation(jest.requireActual("react").useState);
   })
 
+  afterEach(() => {
+    mockAxios.reset();
+  });
+
+  test("get categories item should call", async () => {
+    mockAxios.get.mockResolvedValueOnce({data: CATEGORY_MOCKING_LIST});
+    const result = await get(CATEGORIES_URL);
+    expect(mockAxios.get).toHaveBeenCalledWith(CATEGORIES_URL);
+    expect(result).toEqual(CATEGORY_MOCKING_LIST);
+  });
+
   test("should render modal create component", () => {
-    const { getByTestId } = render(<ModalCreate hideModalCreate={() => {}} />);
+    const { getByTestId } = render(<ModalCreate hideModalCreate={() => {}} createProduct={() => {}} />);
     expect(getByTestId("modal-create")).toBeInTheDocument();
   });
 
   test("should hide modal create when click Cancel", () => {
-    const { getByTestId } = render(<ModalCreate hideModalCreate={hideModalCreate} />);
+    const { getByTestId } = render(<ModalCreate hideModalCreate={hideModalCreate} createProduct={() => {}} />);
     const submitBtn = getByTestId("hide-modal-btn");
     fireEvent.click(submitBtn);
     expect(hideModalCreate).toHaveBeenCalled();
   });
 
+  test("should create product when click Submit", () => {
+    const { getByTestId } = render(<ModalCreate hideModalCreate={() => {}} createProduct={createProduct} />);
+    const submitBtn = getByTestId("submit-btn");
+    fireEvent.click(submitBtn);
+    expect(hideModalCreate).toHaveBeenCalled();
+  });
+
   test("matches snapshot", () => {
-    const { asFragment } = render(<ModalCreate hideModalCreate={() => {}} />);
+    const { asFragment } = render(<ModalCreate hideModalCreate={() => {}} createProduct={() => {}} />);
     expect(asFragment()).toMatchSnapshot();
   });
 });
