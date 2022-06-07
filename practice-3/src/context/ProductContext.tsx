4376ae5fc4d33eb0694d45b6636efc699f567cea
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-06-07 17:08:59 +0700

    Refactor: Implement product context

--- practice-3/src/context/ProductContext.tsx
+++ practice-3/src/context/ProductContext.tsx
@@ -2,10 +2,10 @@ import { createContext, useState } from "react";
 
 export const ProductContext = createContext<any>({});
 const ProductProvider: React.FC<{children: JSX.Element[] | JSX.Element}> = ({ children }) => {
-const [data, setData] = useState([]);
+const [products, setProducts] = useState([]);
 
   return (
-    <ProductContext.Provider value={{ data, setData }}>
+    <ProductContext.Provider value={{ products, setProducts }}>
       {children}
     </ProductContext.Provider>
   )
