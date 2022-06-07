bbf84de44e07a3c54f215829e20f3a0d6aa32259
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-06-07 17:07:12 +0700

    Refactor: Add testing for get data

--- practice-3/src/components/Modal/ModalUpdate/__test__/ModalUpdate.test.tsx
+++ practice-3/src/components/Modal/ModalUpdate/__test__/ModalUpdate.test.tsx
@@ -3,6 +3,10 @@ import { PRODUCT_MOCKING } from "@/constants/product";
 import ModalUpdate from "../ModalUpdate";
 import "@testing-library/jest-dom";
 import { useState } from "react";
+import mockAxios from "@/__mocks__/axios";
+import { CATEGORIES_URL } from "@/constants/url";
+import { CATEGORY_MOCKING_LIST } from "@/constants/categories";
+import { get } from "@/helpers/fetchApi";
 
 jest.mock("react", () => ({
   ...jest.requireActual("react"),
@@ -11,30 +15,38 @@ jest.mock("react", () => ({
 
 describe("Modal create component", () => {
   const hideModalUpdate = jest.fn();
-  const updateProduct = jest.fn();
   beforeEach(()=>{
     (useState as jest.Mock).mockImplementation(jest.requireActual("react").useState);
   })
 
-  test("should render modal delete component", () => {
+  afterEach(() => {
+    mockAxios.reset();
+  });
+
+  test("get categories item should call", async () => {
+    mockAxios.get.mockResolvedValueOnce({data: CATEGORY_MOCKING_LIST});
+    const result = await get(CATEGORIES_URL);
+    expect(mockAxios.get).toHaveBeenCalledWith(CATEGORIES_URL);
+    expect(result).toEqual(CATEGORY_MOCKING_LIST);
+  });
+
+  test("should render modal update component", () => {
     const { getByTestId } = render(<ModalUpdate product={PRODUCT_MOCKING}
-      hideModalUpdate={() => {}} />);
+    hideModalUpdate={() => {}} isReload={true} setIsReLoad={() => {}} />);
     expect(getByTestId("modal-update")).toBeInTheDocument();
   });
 
   test("should hide modal update when click Cancel", () => {
     const { getByTestId } = render(<ModalUpdate product={PRODUCT_MOCKING}
-      hideModalUpdate={hideModalUpdate} />);
+      hideModalUpdate={hideModalUpdate} isReload={true} setIsReLoad={() => {}} />);
     const hideModal = getByTestId("btn-no-modalUpdate");
     fireEvent.click(hideModal);
     expect(hideModalUpdate).toHaveBeenCalled();
   });
 
-  test("should update product when click Submit", () => {
-    const { getByTestId } = render(<ModalUpdate product={PRODUCT_MOCKING}
-      hideModalUpdate={() => {}} />);
-    const hideModal = getByTestId("btn-yes-modalUpdate");
-    fireEvent.click(hideModal);
-    expect(updateProduct).toHaveBeenCalled();
+  test("matches snapshot", () => {
+    const { asFragment } = render(<ModalUpdate product={PRODUCT_MOCKING}
+      hideModalUpdate={() => {}} isReload={true} setIsReLoad={() => {}} />);
+    expect(asFragment()).toMatchSnapshot();
   });
 });
