1a01f208dadf7e410b5bd74df3a766288bfa738d
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-06-07 17:02:46 +0700

    Refactor: Format code and use memo

--- practice-3/src/App/App.tsx
+++ practice-3/src/App/App.tsx
@@ -1,3 +1,4 @@
+import { memo } from "react";
 import { SWRConfig } from "swr";
 import { Route, Routes } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
@@ -7,29 +8,28 @@ import Footer from "@/components/common/Footer/Footer";
 import Header from "@/components/common/Header/Header";
 import Navigation from "@/components/common/Navigation/Navigation";
 import ProductDetails from "@/pages/ProductDetail/ProductDetail";
-import SWR_CONFIG from "../constants/swrConfig";
 import ProductListView from "@/pages/ProductListView/ProductListView";
 import ProductGridView from "@/pages/ProductGridView/ProductGridView";
 import AppProvider from "@/context/AppContext";
 
-const App: React.FC = () => {
+const App: React.FC = memo(() => {
 
   return (
-    <SWRConfig value={SWR_CONFIG}>
+    <SWRConfig value={{ revalidateOnFocus: false }}>
       <ToastContainer />
       <Header />
       <AppProvider>
         <Navigation />
         <Categories />
-        <Routes>
-          <Route path="/" element={<ProductListView />} />
-          <Route path="/products" element={<ProductGridView />} />
-          <Route path={`/product/:id`} element={<ProductDetails />} />
-        </Routes>
+          <Routes>
+            <Route path="/" element={<ProductListView />} />
+            <Route path="/products" element={<ProductGridView />} />
+            <Route path={`/product/:id`} element={<ProductDetails />} />
+          </Routes>
       </AppProvider>
       <Footer />
     </SWRConfig>
   )
-}
+});
 
 export default App
