a4da002856d3f2f42eb3af3f4c7b8ba506fa49f9
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-06-13 08:28:05 +0700

    Refactor: Create component

--- practice-3/src/components/common/Navigation/Navigation.tsx
+++ practice-3/src/components/common/Navigation/Navigation.tsx
@@ -1,5 +1,5 @@
 import React, { memo } from "react";
-import InputSearch from "../../Input/InputSearch/InputSearch";
+import InputSearch from "@/components/InputSearch/InputSearch";
 import "./navigation.css";
 
 const Navigation: React.FC = memo(() => {
