3d568001ba74239d71e1bcc5c7f49b3a24e6e026
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-06-13 08:30:53 +0700

    Refactor: Update fetchApi and type

--- practice-3/src/helpers/fetchApi.ts
+++ practice-3/src/helpers/fetchApi.ts
@@ -1,40 +1,23 @@
 import axios from "axios";
 
 const get = async <T>(url: string) => {
-  try {
-    const res = await axios.get<T>(url);
-    return res.data;
-  } catch(error) {
-      console.log(error);
-  }
+  const res = await axios.get<T>(url);
+  return res.data;
 };
 
 const create = async <T>(url: string, newData: T) => {
-  try {
-    const res = await axios.post(url, newData);
-    return res;
-  }
-  catch(error) {
-    console.log(error);
-  };
+  const res = await axios.post(url, newData);
+  return res;
 }
 
 const update = async <T>(id: string, dataEdit: T) => {
-  try {
-    const res = await axios.put(id, {...dataEdit});
-    return res;
-  } catch(error) {
-      console.log(error);
-  };
+  const res = await axios.put(id, {...dataEdit});
+  return res;
 }
 
 const remove = async (id: string) => {
-  try {
-    const res = await axios.delete(id);
-    return res;
-  } catch(error) {
-    console.log(error);
-  };
+  const res = await axios.delete(id);
+  return res;
 }
 
 export {
