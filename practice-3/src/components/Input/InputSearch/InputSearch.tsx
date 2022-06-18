e38a233dce4e72ccec3cb1833b50014b643ab8a8
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-06-18 19:40:45 +0700

    Refactor: Create component input

---
+++ practice-3/src/components/Input/InputSearch/InputSearch.tsx
@@ -0,0 +1,26 @@
+import React, { memo, useContext } from "react";
+import { SearchContext } from "@/context/SearchContext";
+import "./inputSearch.css";
+
+const InputSearch: React.FC = memo(() => {
+  // handle search with SearchContext
+  const { setSearchValue } = useContext(SearchContext);
+  const handleSearch = (e: { target: { value: string } }) => {
+    // get current name
+    const nameLike = { name_like: e.target.value };
+    setSearchValue?.(nameLike);
+  };
+
+  return (
+    <div data-testid="input-search" className="search">
+      <input
+        className="search__text"
+        type="text"
+        placeholder="Search item"
+        onChange={handleSearch}
+      />
+    </div>
+  );
+});
+
+export default InputSearch;
