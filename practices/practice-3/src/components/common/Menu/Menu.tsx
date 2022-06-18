80ab7e1b0c0ac6d16f13b2ff615b19dc600bf4f2
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-06-18 19:38:24 +0700

    Refactor: Update redirect to home

--- practices/practice-3/src/components/common/Menu/Menu.tsx
+++ practices/practice-3/src/components/common/Menu/Menu.tsx
@@ -1,19 +1,25 @@
 import React, { memo } from "react";
 import { MenuProps, MenuType } from "@/types/menu";
 import "./menu.css";
+import { Link } from "react-router-dom";
 
 const Menu: React.FC<MenuProps> = memo(({ menuList }) => {
   function renderMenuList(list: MenuType[]) {
-    return list.map((item) =>
+    return list.map((item) => (
       <li className="menu__item" key={item.key}>
         <a href="">{item.label}</a>
       </li>
-    );
+    ));
   }
 
   return (
     <div data-testid="menu" className="menu">
-      <ul className="menu__list">{renderMenuList(menuList)}</ul>
+      <ul className="menu__list">
+        <li className="menu__item">
+          <Link to="/">Home</Link>
+        </li>
+        {renderMenuList(menuList)}
+      </ul>
     </div>
   );
 });
