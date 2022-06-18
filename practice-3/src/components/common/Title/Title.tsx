8e840b2045ca1a74521ccfdb4f753eaaa9e1d0f6
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-06-18 19:40:02 +0700

    Refactor: set default props

--- practice-3/src/components/common/Title/Title.tsx
+++ practice-3/src/components/common/Title/Title.tsx
@@ -2,7 +2,7 @@ import React, { memo } from "react";
 import { TitleProps } from "@/types/title";
 import "./title.css";
 
-const Title: React.FC<TitleProps> = memo(({ text, className }) => {
+const Title: React.FC<TitleProps> = memo(({ text="", className }) => {
   return (
     <h2 data-testid="title-test" className={className}>
       {text}
