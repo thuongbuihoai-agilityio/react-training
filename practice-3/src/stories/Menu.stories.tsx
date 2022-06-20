5386f4bf5eed73bd2c5cdff9b723067d29af6b93
Bui Thi Hoai Thuong <thuong.buihoai@asnet.com.vn>
2022-06-20 09:01:22 +0700

    Refactor: Add router for storybook

--- practice-3/src/stories/Menu.stories.tsx
+++ practice-3/src/stories/Menu.stories.tsx
@@ -1,18 +1,23 @@
 import React from "react";
 import MENU_LIST from "@/constants/menu";
 import Menu from "@/components/common/Menu/Menu";
+import { BrowserRouter } from "react-router-dom";
 
 export default {
   /* 👇 The title prop is optional.
-  * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
-  * to learn how to generate automatic titles
-  */
+   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
   title: "Component/Menu",
   component: Menu,
 };
 
 const Default: React.FC = () => {
-  return <Menu menuList={MENU_LIST} />;
-}
+  return (
+    <BrowserRouter>
+      <Menu menuList={MENU_LIST} />
+    </BrowserRouter>
+  );
+};
 
 export { Default };
