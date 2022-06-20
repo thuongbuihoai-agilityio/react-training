20eb092f1bab6a667e8b06fe70ecf844b30d86d8
Bui Thi Hoai Thuong <thuong.buihoai@asnet.com.vn>
2022-06-20 09:01:22 +0700

    Refactor: Add router for storybook

--- practices/practice-3/src/stories/Logo.stories.tsx
+++ practices/practice-3/src/stories/Logo.stories.tsx
@@ -1,18 +1,23 @@
 import React from "react";
 import Logo from "@/components/common/Logo/Logo";
 import url from "@/assets/images/logos/logos.png";
+import { BrowserRouter } from "react-router-dom";
 
 export default {
   /* 👇 The title prop is optional.
-  * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
-  * to learn how to generate automatic titles
-  */
+   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
   title: "Component/Logo",
   component: Logo,
 };
 
 const Default: React.FC = () => {
-  return <Logo src={url} />;
-}
+  return (
+    <BrowserRouter>
+      <Logo src={url} />
+    </BrowserRouter>
+  );
+};
 
 export { Default };
