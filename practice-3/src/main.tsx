6a0013c7066cb86bbafac6a55d4eea4447b91869
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-06-21 07:16:00 +0700

    Refactor: Config why did you render

--- practice-3/src/main.tsx
+++ practice-3/src/main.tsx
@@ -1,10 +1,11 @@
+import "./wdyr.ts"; 
+/// <reference types="@welldone-software/why-did-you-render" />
 import axios from "axios";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App/App";
 import { BrowserRouter } from "react-router-dom";
 import { BASE_URL } from "./constants/url";
-import "./wdyr.ts";
 import "./styles/main.css";
 
 axios.defaults.baseURL = BASE_URL;
