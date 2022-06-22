440ee9a9b6292263ed5a1e7e96ff07e7e0efdd86
Bui Thi Hoai Thuong <thuong.buihoai@asnet.com.vn>
2022-06-22 10:21:09 +0700

    Refactor: Update url

--- practice-3/src/constants/url.ts
+++ practice-3/src/constants/url.ts
@@ -1,6 +1,5 @@
 const BASE_URL = "http://localhost:1234";
-const PRODUCTS_URL = `${BASE_URL}/products?`;
-const PRODUCT_CRUD = `${BASE_URL}/products/`
+const PRODUCTS_URL = `${BASE_URL}/products/`;
 const CATEGORIES_URL = `${BASE_URL}/categories`;
 
-export { BASE_URL, PRODUCTS_URL, PRODUCT_CRUD, CATEGORIES_URL };
+export { BASE_URL, PRODUCTS_URL, CATEGORIES_URL };
