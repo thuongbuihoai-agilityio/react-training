a5a43c48fb4384d290e5eb9adbce4c486d63cacc
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-06-25 23:04:58 +0700

    Refactor: Handle state management

--- practices/practice-3/src/context/DataContext.tsx
+++ practices/practice-3/src/context/DataContext.tsx
@@ -1,32 +1,36 @@
-import { ACTION } from "@/constants/message";
-import useSWR, { mutate } from "swr";
-import { reducer } from "@/reducer/dataReducer";
+import useSWR from "swr";
+import toast from "react-hot-toast";
 import { getData } from "@/helpers/fetchApi";
 import { PRODUCTS_URL } from "@/constants/url";
-import { createContext, useContext, useMemo, useReducer } from "react";
+import { createContext, useContext, useEffect, useMemo, useState } from "react";
 import { Product, ProductContext } from "@/types/product";
-import {SearchContext} from "./SearchContext";
+import { SearchContext } from "./SearchContext";
 
-const DEFAULT_STATE: ProductContext = {
-  data: [],
-  mutate
-};
-
-export const DataContext = createContext<ProductContext>(DEFAULT_STATE);
+export const DataContext = createContext<ProductContext>({} as ProductContext);
 const DataProvider: React.FC<{children: JSX.Element[] | JSX.Element}> = ({ children }) => {
   const { searchValue } = useContext(SearchContext);
   // URLSearchParams: convert searchValue to string => handle search
   const queryParams: URLSearchParams = new URLSearchParams(searchValue);
   const { data, mutate } = useSWR(PRODUCTS_URL + "?" + queryParams.toString(), getData<Product[]>);
-  const [dispatch] = useReducer(reducer, data);
+  const [products, setProducts] = useState<Product[]>([]);
+
+  const getDataProducts = async () => {
+    try {
+      setProducts(data as Product[]);
+    } catch (error) {
+      toast.error(error as string);
+    }
+  }
+
+  useEffect(() => {
+    getDataProducts()
+  }, [data])
 
   const value = useMemo(() => ({
-    data,
     mutate,
-    getProduct: (data: Product) => {
-      dispatch({action: ACTION.GET_DATA, data});
-    }
-  }), [data]);
+    products,
+    setProducts,
+  }), [products]);
 
   return (
     <DataContext.Provider value={value}>
