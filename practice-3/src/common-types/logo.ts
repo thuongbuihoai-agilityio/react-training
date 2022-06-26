39427db14e154bee8e4b3663cf35114828e160b1
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-06-26 17:30:52 +0700

    Refactor: Change folder name types to common-types

---
+++ practice-3/src/common-types/logo.ts
@@ -0,0 +1,5 @@
+export interface LogoProps {
+  src: string;
+  alt?: string;
+  productId?: string;
+}
---
+++ practice-3/src/common-types/product.ts
@@ -0,0 +1,27 @@
+import { KeyedMutator } from "swr";
+
+export interface Product {
+  id: string;
+  name: string;
+  price: number;
+  images: string[];
+  quantity: number;
+  categoryId: string;
+  description: string;
+  product?: {};
+}
+
+export interface ProductItemProps {
+  product: Product;
+  deleteProduct: (id: string) => void;
+}
+
+export interface ProductCardProps {
+  product: Product;
+}
+
+export interface ProductContext {
+  products: Product[];
+  mutate: KeyedMutator<Product[]>;
+  setProducts: (data: Product[]) => void;
+}
