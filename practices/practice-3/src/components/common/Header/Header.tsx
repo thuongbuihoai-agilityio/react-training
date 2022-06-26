f9365c97cff333724c7b2a826bdea5b7d980c9c8
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-06-26 17:33:18 +0700

    Refactor: Fix alias path

--- practices/practice-3/src/components/common/Header/Header.tsx
+++ practices/practice-3/src/components/common/Header/Header.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Menu from "../Menu/Menu";
+import MENU_LIST from "@constants/menu";
+import url from "@assets/images/logos/logos.png";
+import backgroundUrl from "@assets/images/backgrounds/slide-1.jpg";
 import Logo from "../Logo/Logo";
-import MENU_LIST from "@/constants/menu";
+import Menu from "../Menu/Menu";
 import Button from "../Button/Button/Button";
-import url from "@/assets/images/logos/logos.png";
-import backgroundUrl from "@/assets/images/backgrounds/slide-1.jpg";
 import "./header.css";
 
 const Header: React.FC = () => {
