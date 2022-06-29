e8d0db9653d243e8f98044978e1fd1341ff38912
Bui Thi Hoai Thuong <thuong.buihoai@asnet.com.vn>
2022-06-29 09:10:54 +0700

    Refactor: Format code

--- practice-3/src/main.tsx
+++ practice-3/src/main.tsx
@@ -1,13 +1,10 @@
 import "./wdyr.ts";
-import axios from "axios";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./app/App";
+import App from "./App/App";
 import { BrowserRouter } from "react-router-dom";
-import { BASE_URL } from "./constants/url";
 import "./styles/main.css";
 
-axios.defaults.baseURL = BASE_URL;
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
