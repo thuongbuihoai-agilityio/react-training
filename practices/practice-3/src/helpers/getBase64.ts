d0deec9e8747d8826149cbdb9ebba14b4382a647
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-06-29 17:33:36 +0700

    Refactor: Fix type

--- practices/practice-3/src/helpers/getBase64.ts
+++ practices/practice-3/src/helpers/getBase64.ts
@@ -1,10 +1,10 @@
 // getBase64: Get Base64 encode file-data from Input Form to string
 const getBase64 = (file: Blob) => {
-  return new Promise((resolve, reject) => {
+  return new Promise<string>((resolve, reject) => {
     const fileReader = new FileReader();
     fileReader.readAsDataURL(file)
     fileReader.onload = () => {
-      resolve(fileReader.result);
+      resolve(fileReader.result as string);
     }
     fileReader.onerror = (error) => {
       reject(error);
