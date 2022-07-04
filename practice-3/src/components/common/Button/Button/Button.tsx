8d478c554f44c4a5c5ced140985dc9e090034873
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-07-04 17:34:27 +0700

    Refactor: Update component, define state and type of props

--- practice-3/src/components/common/Button/Button/Button.tsx
+++ practice-3/src/components/common/Button/Button/Button.tsx
@@ -3,13 +3,35 @@ import { ButtonProps } from "@common-types/button";
 import "./button.css";
 
 const Button: React.FC<ButtonProps> = memo(
-  ({ text, className, onClick, disabled }) => {
+  ({ text, type, onClick, disabled }) => {
+    let className = "btn";
+    switch (type) {
+      case "primary":
+        className += " btn__primary";
+        break;
+      case "secondary":
+        className += " btn__secondary";
+        break;
+      case "info":
+        className += " btn__info";
+        break;
+      case "update":
+        className += " btn__update";
+        break;
+      case "delete":
+        className += " btn__delete";
+        break;
+      case "warning":
+        className += " btn__warning";
+        break;
+      case "success":
+        className += " btn__success";
+        break;
+      default:
+        break;
+    }
     return (
-      <button
-        className={`btn btn__${className}`}
-        disabled={disabled}
-        onClick={onClick}
-      >
+      <button className={className} disabled={disabled} onClick={onClick}>
         {text}
       </button>
     );
