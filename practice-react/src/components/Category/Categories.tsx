020eb691df76cc55bead684218d66274885444b1
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-07-06 17:11:15 +0700

    Create compoennt categories

---
+++ practice-react/src/components/Category/Categories.tsx
@@ -0,0 +1,14 @@
+import { CATEGORY_LIST } from "@constants/category";
+import React, { memo } from "react";
+import CategoryList from "./CategoryList/CategoryList";
+
+const Categories: React.FC = memo(() => {
+  return (
+    <div data-testid="categories" className="categories">
+      <p className="categories__title">Categories</p>
+      <CategoryList categoryList={CATEGORY_LIST} />
+    </div>
+  );
+});
+
+export default Categories;
