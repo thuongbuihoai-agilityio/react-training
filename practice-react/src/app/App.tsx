27643d9e97252c787d33f658d8be7d224139e32b
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-07-06 17:08:24 +0700

    Add component Categories

--- practice-react/src/app/App.tsx
+++ practice-react/src/app/App.tsx
@@ -1,10 +1,14 @@
 import { memo } from "react";
 import Header from "@components/common/Header/Header";
+import Categories from "@components/Category/Categories";
 
 const App: React.FC = memo(() => {
   return (
-    <Header />
-  )
+    <>
+      <Header />
+      <Categories />
+    </>
+  );
 });
 
 export default App;
