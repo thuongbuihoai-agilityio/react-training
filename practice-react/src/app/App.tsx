0f6f3248b15af6a3a2887d0e6f14deb374021ef0
Bui Thi Hoai Thuong <thuong.buihoai@asnet.com.vn>
2022-07-07 13:39:31 +0700

    Add component CardProduct

--- practice-react/src/app/App.tsx
+++ practice-react/src/app/App.tsx
@@ -1,12 +1,14 @@
 import { memo } from "react";
 import Header from "@components/common/Header/Header";
 import Categories from "@components/Category/Categories";
+import CardProductList from "@components/CardProduct/CardProductList/CardProductList";
 
 const App: React.FC = memo(() => {
   return (
     <>
       <Header />
       <Categories />
+      <CardProductList />
     </>
   );
 });
