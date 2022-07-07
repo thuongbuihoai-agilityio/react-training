376b207caea694e57640db3673d40cfeec165cec
Bui Thi Hoai Thuong <thuong.buihoai@asnet.com.vn>
2022-07-07 17:32:56 +0700

    Add new props

--- practice-react/src/components/CardProduct/CardProduct.tsx
+++ practice-react/src/components/CardProduct/CardProduct.tsx
@@ -3,35 +3,65 @@ import url from "@assets/images/cottage-cheese.png";
 import Text from "@components/common/Text/Text";
 import Price from "@components/common/Price/Price";
 import "./cardProduct.css";
+import Counter from "@components/common/Counter/Counter";
+import Icon from "@components/common/Icon/Icon";
 
 interface CartProductProps {
   type: string;
+  content: string;
+  visibleQuantity?: boolean;
+  visibleDiscountPrice?: boolean;
+  visibleCounter?: boolean;
 }
 
-const CardProduct: React.FC<CartProductProps> = memo(({ type }) => {
-  let className = "";
-  switch (type) {
-    case "offers":
-      className = "card__product--offers";
-      break;
+const CardProduct: React.FC<CartProductProps> = memo(
+  ({ type, visibleQuantity, visibleDiscountPrice, visibleCounter }) => {
+    let className = "";
+    switch (type) {
+      case "offers":
+        className = "card__product--offers";
+        break;
       case "selling":
-      className = "card__product--selling";
-      break;
-    default:
-      break;
-  }
-  return (
-    <div data-testid="card-product" className={className}>
-      <img src={url} alt="This is ice-cream-sundae image" />
-      <div className="card__info">
-        <Text text="Cottage Cheese" />
-        <p className="card__unit">3kg</p>
-        <Price type="original" value={100} currency="$" />
-        <Price type="discount" value={150} currency="$" />
-        <h1 className="test">hello</h1>
+        className = "card__product--selling";
+        break;
+      default:
+        break;
+    }
+
+    let cartInfo = "";
+    switch (type) {
+      case "offers":
+        cartInfo = "card__offers";
+        break;
+      case "selling":
+        cartInfo = "card__selling";
+        break;
+      default:
+        break;
+    }
+
+    return (
+      <div data-testid="card-product" className={className}>
+        <img src={url} alt="This is ice-cream-sundae image" />
+        <div className={cartInfo}>
+          <Text text="Cottage Cheese" />
+          {visibleQuantity && <p className="card__unit">3kg</p>}
+          <Price type="original" value={100} currency="$" />
+          {visibleDiscountPrice && (
+            <Price type="discount" value={150} currency="$" />
+          )}
+        </div>
+        {visibleCounter && (
+          <div className="card__counter">
+            <Counter />
+            <div className="card__icon">
+              <Icon iconName="cart" />
+            </div>
+          </div>
+        )}
       </div>
-    </div>
-  );
-});
+    );
+  }
+);
 
 export default CardProduct;
