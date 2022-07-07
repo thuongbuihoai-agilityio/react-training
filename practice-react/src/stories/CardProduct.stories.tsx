49c6759f5be69b25690535d20f6832bcf2ef7ee2
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-07-07 08:07:55 +0700

    Writing storybook for component card product

---
+++ practice-react/src/stories/CardProduct.stories.tsx
@@ -0,0 +1,17 @@
+import CardProduct from "@components/CardProduct/CardProduct";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+export default {
+  /* 👇 The title prop is optional.
+   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: "Component/CardProduct",
+  component: CardProduct,
+} as ComponentMeta<typeof CardProduct>;
+
+const TemplateCardProduct: ComponentStory<typeof CardProduct> = (args) => (
+  <CardProduct {...args} />
+);
+
+export const CardOffers = TemplateCardProduct.bind({});
