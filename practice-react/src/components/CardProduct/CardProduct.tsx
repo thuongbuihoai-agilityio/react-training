59ae510a726646149a25d1093f4d8a69bd14d5b4
Bui Thi Hoai Thuong <thuong.buihoai@asnet.com.vn>
2022-07-07 13:45:17 +0700

    Add class name

--- practice-react/src/components/CardProduct/CardProduct.tsx
+++ practice-react/src/components/CardProduct/CardProduct.tsx
@@ -1,20 +1,35 @@
 import React, { memo } from "react";
-import url from "@assets/images/ice-cream-sundae.png";
+import url from "@assets/images/cottage-cheese.png";
 import Text from "@components/common/Text/Text";
 import Price from "@components/common/Price/Price";
+import "./cardProduct.css";
 
 interface CartProductProps {
-  type?: string;
+  type: string;
 }
 
-const CardProduct: React.FC = memo(() => {
+const CardProduct: React.FC<CartProductProps> = memo(({ type }) => {
+  let className = "";
+  switch (type) {
+    case "offers":
+      className = "card__product--offers";
+      break;
+      case "selling":
+      className = "card__product--selling";
+      break;
+    default:
+      break;
+  }
   return (
-    <div data-testid="card-product" className="card__product">
+    <div data-testid="card-product" className={className}>
       <img src={url} alt="This is ice-cream-sundae image" />
-      <Text text="Cottage Cheese" />
-      <p className="card__unit">3kg</p>
-      <Price type="original" value={100} currency="$" />
-      <Price type="discount" value={150} currency="$" />
+      <div className="card__info">
+        <Text text="Cottage Cheese" />
+        <p className="card__unit">3kg</p>
+        <Price type="original" value={100} currency="$" />
+        <Price type="discount" value={150} currency="$" />
+        <h1 className="test">hello</h1>
+      </div>
     </div>
   );
 });
