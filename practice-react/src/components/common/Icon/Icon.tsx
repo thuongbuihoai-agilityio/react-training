c4141b27f2e7d542269c4d6f96f72b887a16f4b5
Bui Thi Hoai Thuong <thuong.buihoai@asnet.com.vn>
2022-07-07 13:43:25 +0700

    Create component Icon

--- practice-react/src/components/common/Icon/Icon.tsx
+++ practice-react/src/components/common/Icon/Icon.tsx
@@ -6,13 +6,33 @@ interface IconProps {
 }
 
 const Icon: React.FC<IconProps> = ({ iconName }) => {
-  let className = "fa";
-  if(className) {
-    className += " fa-shop"
+  let className = "";
+  switch (iconName) {
+    case "filter":
+      className = "icon-filter";
+      break;
+    case "plus":
+      className = "icon-plus";
+      break;
+    case "minus":
+      className = "icon-minus";
+      break;
+    case "cart":
+      className = "icon-cart";
+      break;
+    case "fb":
+      className = "icon-fb";
+      break;
+    case "twitter":
+      className = "icon-twitter";
+      break;
+    case "instagram":
+      className = "icon-instagram";
+      break;
+    default:
+      break;
   }
-  return (
-    <i className={iconName} />
-  );
+  return <i className={className} />;
 };
 
 export default Icon;
