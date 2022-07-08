fd046d19ba58feabf6c5707cc97044533a101b00
Bui Thi Hoai Thuong <thuong.buihoai@asnet.com.vn>
2022-07-08 17:08:45 +0700

    Add type checkbox

--- practice-react/src/components/Category/CategoryList/CategoryList.tsx
+++ practice-react/src/components/Category/CategoryList/CategoryList.tsx
@@ -1,24 +1,61 @@
 import React from "react";
-import { CategoryProps, CategoryType } from "@common-types/categoryList";
-import "./categoryList.css";
+import { CategoryType } from "@common-types/categoryList";
 import { Link } from "react-router-dom";
+import Checkbox from "@components/common/Checkbox/Checkbox";
+import "./categoryList.css";
 
-const CategoryList: React.FC<CategoryProps> = ({ categoryList }) => {
+interface CategoryProps {
+  categoryList: CategoryType[];
+  isCheckbox?: boolean;
+  isSelect?: boolean;
+  type: string;
+}
+
+const CategoryList: React.FC<CategoryProps> = ({
+  categoryList,
+  isCheckbox,
+  isSelect,
+  type,
+}) => {
+  let className = "";
+  switch (type) {
+    case "select":
+      className = "categoryList--select";
+      break;
+    case "checkbox":
+      className = "categoryList--checkbox";
+      break;
+    default:
+      break;
+  }
   const renderCategoryList = (list: CategoryType[]) => {
     return list.map((item) => (
-      <div className="categoryList__list" key={item.key}>
-        <figure className="categoryList__image">
-          <img className="categoryList__images" src={item.src} alt={item.alt} />
-        </figure>
-        <Link className="categoryList__text" to="/products">
-          <p>{item.text}</p>
-        </Link>
-      </div>
+      <>
+        {isSelect && (
+          <div className="categoryList__list" key={item.key}>
+            <figure className="categoryList__image">
+              <img
+                className="categoryList__images"
+                src={item.src}
+                alt={item.alt}
+              />
+            </figure>
+            <Link className="categoryList__text" to="/products">
+              <p>{item.text}</p>
+            </Link>
+          </div>
+        )}
+        {isCheckbox && (
+          <div className="categoryList__checkbox">
+            <Checkbox value={item.text} />
+          </div>
+        )}
+      </>
     ));
   };
 
   return (
-    <div data-testid="category-list" className="categoryList">
+    <div data-testid="category-list" className={className}>
       {renderCategoryList(categoryList)}
     </div>
   );
