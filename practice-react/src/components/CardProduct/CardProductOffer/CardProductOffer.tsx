77ee592b1988647641ae1a903787fd0c71a702bd
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-07-08 08:16:46 +0700

    Change name folder

---
+++ practice-react/src/components/CardProduct/CardProductOffer/CardProductOffer.tsx
@@ -0,0 +1,21 @@
+import React, { memo } from "react";
+import Text from "@components/common/Text/Text";
+import CardProduct from "../CardProduct";
+
+const CardProductList: React.FC = memo(() => {
+  return (
+    <div className="cardOffersList">
+      <div className="cardOffersList__title">
+      <Text text="Offers" type="large" />
+      </div>
+      <div className="card__item--offers">
+        <CardProduct type="offers" visibleQuantity={true} visibleDiscountPrice={true} content="offers" />
+        <CardProduct type="offers" visibleQuantity={true} visibleDiscountPrice={true} content="offers" />
+        <CardProduct type="offers" visibleQuantity={true} visibleDiscountPrice={true} content="offers" />
+        <CardProduct type="offers" visibleQuantity={true} visibleDiscountPrice={true} content="offers" />
+      </div>
+    </div>
+  );
+});
+
+export default CardProductList;
