34744eaab34339d8df7e8febddf8fe562a6fdcc9
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-07-11 08:04:50 +0700

    Add link and change props

--- practice-react/src/components/common/NavigationBar/NavigationBar.tsx
+++ practice-react/src/components/common/NavigationBar/NavigationBar.tsx
@@ -1,19 +1,40 @@
 import React from "react";
+import { MENU_LIST } from "@constants/menu";
+import { Link } from "react-router-dom";
 import Logo from "../Logo/Logo";
 import url from "../../../assets/images/logos/logo.png";
 import Menu from "../Menu/Menu";
-import { MENU_LIST } from "@constants/menu";
 import Button from "../Button/Button";
 import "./navigationBar.css";
 
-const NavigationBar: React.FC = () => {
+interface NavigationBarProps {
+  isThemeLight?: boolean;
+  isThemeDark?: boolean;
+}
+
+const NavigationBar: React.FC<NavigationBarProps> = ({
+  isThemeLight,
+  isThemeDark,
+}) => {
   return (
     <nav data-testid="navigation-bar" className="nav">
-      <Logo src={url} />
+      <Link to="/">
+        <Logo src={url} />
+      </Link>
       <div className="nav__about">
         <Menu menuList={MENU_LIST} />
-        <Button text="Sign Up" type="light" />
-        <Button text="Login" type="outline--light" />
+        {isThemeLight && (
+          <>
+            <Button text="Sign Up" type="light" />
+            <Button text="Login" type="outline--light" />
+          </>
+        )}
+        {isThemeDark && (
+          <>
+            <Button text="Sign Up" type="dark" />
+            <Button text="Login" type="outline--dark" />
+          </>
+        )}
       </div>
     </nav>
   );
