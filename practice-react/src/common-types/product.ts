5d9955b49d05ca363b6f45aaaf84fd85ef196566
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-07-11 10:34:46 +0700

    Create type for product

---
+++ practice-react/src/common-types/product.ts
@@ -0,0 +1,27 @@
+import { Categories } from "./categoryList";
+
+export interface Product {
+  productId: string;
+  categoryId: string;
+  productName: string;
+  originalPrice: {
+    value: number;
+    currency: string;
+  };
+  discountPrice: {
+    value: number;
+    currency: string;
+  };
+  offerQuantity: string;
+  offer: boolean;
+  bestSelling: boolean;
+  popular: boolean;
+  images: {
+    src: string;
+    alt: string;
+  }
+}
+
+export interface ProductProps {
+  product?: Product;
+}
