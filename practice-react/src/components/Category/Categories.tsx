3dc7fc3fbf86fa929de1dff46932f226d0ed0ff0
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-07-11 08:05:41 +0700

    Change props

--- practice-react/src/components/Category/Categories.tsx
+++ practice-react/src/components/Category/Categories.tsx
@@ -1,4 +1,3 @@
-import { CATEGORY_LIST } from "@constants/category";
 import React, { memo } from "react";
 import CategoryList from "./CategoryList/CategoryList";
 
@@ -6,7 +5,7 @@ const Categories: React.FC = memo(() => {
   return (
     <div data-testid="categories" className="categories">
       <p className="categories__title">Categories</p>
-      <CategoryList type="select" isSelect={true} categoryList={CATEGORY_LIST} />
+      <CategoryList type="select" isSelect={true} />
     </div>
   );
 });
