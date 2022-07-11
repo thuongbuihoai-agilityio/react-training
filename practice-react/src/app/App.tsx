7b69d2e85c3359d3271e0d0eb0b42f5007190982
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-07-11 08:01:28 +0700

    Implement app

--- practice-react/src/app/App.tsx
+++ practice-react/src/app/App.tsx
@@ -1,4 +1,5 @@
 import { memo } from "react";
+import { Route, Routes } from "react-router-dom";
 import Header from "@components/common/Header/Header";
 import Categories from "@components/Category/Categories";
 import CardProductList from "@components/CardProduct/CardProductOffer/CardProductOffer";
@@ -6,18 +7,32 @@ import CardProductSelling from "@components/CardProduct/CardProductSelling/CardP
 import CardPopular from "@components/CardProduct/CardPopular/CardPopular";
 import SignUpSection from "@components/SignUpSection/SignUpSection";
 import Footer from "@components/common/Footer/Footer";
+import ProductList from "@pages/ProductList/ProductList";
+import ProductDetail from "@pages/ProductDetail/ProductDetail";
+import { SWRConfig } from "swr";
 
 const App: React.FC = memo(() => {
   return (
-    <>
-      <Header />
-      <Categories />
-      <CardProductList />
-      <CardProductSelling />
-      <CardPopular />
-      <SignUpSection />
+    <SWRConfig value={{ revalidateOnFocus: false }}>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Header />
+              <Categories />
+              <CardProductList />
+              <CardProductSelling />
+              <CardPopular />
+              <SignUpSection />
+            </>
+          }
+        />
+        <Route path="/products" element={<ProductList />} />
+        <Route path={`/products/:id`} element={<ProductDetail />} />
+      </Routes>
       <Footer />
-    </>
+    </SWRConfig>
   );
 });
 
