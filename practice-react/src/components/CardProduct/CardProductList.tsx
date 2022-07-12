a9d38d041e8ecf1166cf4082d7bb4b555f3293e3
Bui Thi Hoai Thuong <thuong.buihoai@asnet.com.vn>
2022-07-12 17:25:50 +0700

    Format code

--- practice-react/src/components/CardProduct/CardProductList.tsx
+++ practice-react/src/components/CardProduct/CardProductList.tsx
@@ -1,10 +1,10 @@
 import useSWR from "swr";
-import React, { memo, useContext } from "react";
+import React, { memo, useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Product } from "@common-types/product";
 import { PRODUCTS_URL } from "@constants/url";
-import { CategoriesContext } from "@context/CategoryContext";
 import { getData } from "@helpers/fetchApi";
+import { DataContext } from "@context/DataContext";
 import Text from "@components/common/Text/Text";
 import Price from "@components/common/Price/Price";
 import Counter from "@components/common/Counter/Counter";
@@ -22,7 +22,7 @@ interface CartProductProps {
   visibleCounter?: boolean;
 }
 
-const CardProductList: React.FC<CartProductProps> = memo(
+const CardProductList: React.FC<CartProductProps> = memo (
   ({
     type,
     isOffer,
@@ -32,83 +32,87 @@ const CardProductList: React.FC<CartProductProps> = memo(
     visibleDiscountPrice,
     visibleCounter,
   }) => {
-    const { searchValue } = useContext(CategoriesContext);
-    const filter = (isOffer || isPopular || isBestSelling) ? {
-      offer: isOffer,
-      popular: isPopular,
-      bestSelling: isBestSelling,
-    } : (searchValue) ? {...searchValue} : {};
-    const queryParams: URLSearchParams = new URLSearchParams(filter as {});
-    const { data } = useSWR(PRODUCTS_URL + "?" + queryParams.toString(), getData<Product[]>);
 
-    let className = "";
-    switch (type) {
-      case "offers":
-        className = "card__product--offers";
-        break;
-      case "selling":
-        className = "card__product--selling";
-        break;
-      case "popular":
-        className = "card__product--popular";
-        break;
-      default:
-        break;
-    }
+  const { searchValue, setProducts } = useContext(DataContext);
+  const filter = (isOffer || isPopular || isBestSelling) ? {
+    offer: isOffer,
+    popular: isPopular,
+    bestSelling: isBestSelling,
+  } : (searchValue) ? {...searchValue} : {};
+  const queryParams: URLSearchParams = new URLSearchParams(filter as {});
+  const { data } = useSWR(PRODUCTS_URL + "?" + queryParams.toString(), getData<Product[]>);
 
-    let cartInfo = "";
-    switch (type) {
-      case "offers":
-        cartInfo = "card__offers";
-        break;
-      case "selling":
-        cartInfo = "card__selling";
-        break;
-      case "popular":
-        cartInfo = "card__popular";
-        break;
-      default:
-        break;
-    }
+  useEffect(() => {
+    setProducts(data)
+  }, [data]);
 
-    return (
-      <>
-        {data?.map((product) => (
-          <div key={product.productId} className={className}>
-            <img className="card__image" src={product.images.src} alt={product.images.alt} />
-            <div className={cartInfo}>
-              <Link className="cart__link" to={`/products/${product?.productId}`}>
-                <Text text={product?.productName} />
-              </Link>
-              {visibleQuantity && (
-                <p className="card__unit">{product?.offerQuantity}</p>
-              )}
+  let className = "";
+  switch (type) {
+    case "offers":
+      className = "card__product--offers";
+      break;
+    case "selling":
+      className = "card__product--selling";
+      break;
+    case "popular":
+      className = "card__product--popular";
+      break;
+    default:
+      break;
+  }
+
+  let cartInfo = "";
+  switch (type) {
+    case "offers":
+      cartInfo = "card__offers";
+      break;
+    case "selling":
+      cartInfo = "card__selling";
+      break;
+    case "popular":
+      cartInfo = "card__popular";
+      break;
+    default:
+      break;
+  }
+
+  return (
+    <>
+      {data?.map((product: Product) => (
+        <div data-testid="card-product-list" key={product.productId} className={className}>
+          <img className="card__image" src={product.images.src} alt={product.images.alt} />
+          <div className={cartInfo}>
+            <Link className="cart__link" to={`/products/${product?.productId}`}>
+              <Text type="normal" text={product?.productName} />
+            </Link>
+            {visibleQuantity && (
+              <p className="card__unit">{product?.offerQuantity}</p>
+            )}
+            <Price
+              type="original"
+              value={product?.originalPrice.value}
+              currency={product?.originalPrice.currency}
+            />
+            {visibleDiscountPrice && (
               <Price
-                type="original"
-                value={product?.originalPrice.value}
-                currency={product?.originalPrice.currency}
+                type="discount"
+                value={product?.discountPrice.value}
+                currency={product?.discountPrice.currency}
               />
-              {visibleDiscountPrice && (
-                <Price
-                  type="discount"
-                  value={product?.discountPrice.value}
-                  currency={product?.discountPrice.currency}
-                />
-              )}
-            </div>
-            {visibleCounter && (
-              <div className="card__counter">
-                <Counter counter={product.productQuantity} />
-                <div className="card__icon">
-                  <Icon iconName="cart" />
-                </div>
-              </div>
             )}
           </div>
-        ))}
-      </>
-    );
-  }
+          {visibleCounter && (
+            <div className="card__counter">
+              <Counter counter={product.productQuantity} />
+              <div className="card__icon">
+                <Icon iconName="cart" />
+              </div>
+            </div>
+          )}
+        </div>
+      ))}
+    </>
+  )}
 );
 
 export default CardProductList;
