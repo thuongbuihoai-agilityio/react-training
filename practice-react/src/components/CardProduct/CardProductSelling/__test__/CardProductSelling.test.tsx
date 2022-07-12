a991b9a2d1f77d3c088d04d1cb23a54253a8491d
Bui Thi Hoai Thuong <thuong.buihoai@asnet.com.vn>
2022-07-12 17:29:08 +0700

    Writing unit test for component CardProductSelling

--- practice-react/src/components/CardProduct/CardProductSelling/__test__/CardProductSelling.test.tsx
+++ practice-react/src/components/CardProduct/CardProductSelling/__test__/CardProductSelling.test.tsx
@@ -3,14 +3,27 @@ import { createMemoryHistory } from "history";
 import { Router } from "react-router-dom";
 import "@testing-library/jest-dom";
 import CardProductSelling from "../CardProductSelling";
+import { DataContextProps } from "@common-types/data";
+import { DataContext } from "@context/DataContext";
+
+const contextProductMock: DataContextProps = {
+  searchValue: {},
+  setSearchValue: jest.fn(),
+  categories: [],
+  setCategories: jest.fn(),
+  products: [],
+  setProducts: jest.fn(),
+};
 
 describe("CardProductSelling component", () => {
   test("should render CardProductSelling component", () => {
     const history = createMemoryHistory();
     const { getByTestId } = render(
-      <Router location={history.location} navigator={history}>
-        <CardProductSelling />
-      </Router>
+      <DataContext.Provider value={contextProductMock}>
+        <Router location={history.location} navigator={history}>
+          <CardProductSelling />
+        </Router>
+      </DataContext.Provider>
     );
     expect(getByTestId("card-product-selling")).toBeInTheDocument();
   });
@@ -18,9 +31,11 @@ describe("CardProductSelling component", () => {
   test("matches snapshot", () => {
     const history = createMemoryHistory();
     const { asFragment } = render(
-      <Router location={history.location} navigator={history}>
-        <CardProductSelling />
-      </Router>
+      <DataContext.Provider value={contextProductMock}>
+        <Router location={history.location} navigator={history}>
+          <CardProductSelling />
+        </Router>
+      </DataContext.Provider>
     );
     expect(asFragment()).toMatchSnapshot();
   });
