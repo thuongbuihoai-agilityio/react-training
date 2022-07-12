b8ed9c7b7662bfb1d6ceae2a5bd89164e395a018
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-07-12 07:15:29 +0700

    Update storybook

--- practice-react/src/stories/CardProduct.stories.tsx
+++ practice-react/src/stories/CardProduct.stories.tsx
@@ -1,30 +1,30 @@
-import CardProduct from "@components/CardProduct/CardProduct";
 import { BrowserRouter } from "react-router-dom";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
+import CardProductList from "@components/CardProduct/CardProductList";
 
 export default {
   /* 👇 The title prop is optional.
    * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
    * to learn how to generate automatic titles
    */
-  title: "Component/CardProduct",
-  component: CardProduct,
-} as ComponentMeta<typeof CardProduct>;
+  title: "Component/CardProductList",
+  component: CardProductList,
+} as ComponentMeta<typeof CardProductList>;
 
-const TemplateCardProduct: ComponentStory<typeof CardProduct> = (args) => (
+const TemplateCardProductList: ComponentStory<typeof CardProductList> = (args) => (
   <BrowserRouter>
-    <CardProduct {...args} />
+    <CardProductList {...args} />
   </BrowserRouter>
 );
 
-export const CardOffers = TemplateCardProduct.bind({});
+export const CardOffers = TemplateCardProductList.bind({});
 CardOffers.args = {
   type: "offers",
   visibleQuantity: true,
   visibleDiscountPrice: true,
 };
 
-export const CardSelling = TemplateCardProduct.bind({});
+export const CardSelling = TemplateCardProductList.bind({});
 CardSelling.args = {
   type: "selling",
   visibleCounter: true,
