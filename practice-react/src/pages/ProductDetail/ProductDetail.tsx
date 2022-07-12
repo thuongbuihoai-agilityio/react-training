0d8e118d7cfb86cfa3a15a87d95e79f9ea7b5cee
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-07-12 07:14:47 +0700

    Add props counter

--- practice-react/src/pages/ProductDetail/ProductDetail.tsx
+++ practice-react/src/pages/ProductDetail/ProductDetail.tsx
@@ -1,13 +1,13 @@
 import React, { memo } from "react";
 import NavigationBar from "@components/common/NavigationBar/NavigationBar";
-import CardProduct from "@components/CardProduct/CardProduct";
+import CardProductList from "@components/CardProduct/CardProductList";
 import url from "@assets/images/eco-face-cream.png";
 import background from "@assets/images/backgrounds/gray.png";
-import "./productDetail.css";
 import Text from "@components/common/Text/Text";
 import Price from "@components/common/Price/Price";
 import Counter from "@components/common/Counter/Counter";
 import Button from "@components/common/Button/Button";
+import "./productDetail.css";
 
 const ProductDetail: React.FC = memo(() => {
   return (
@@ -28,7 +28,7 @@ const ProductDetail: React.FC = memo(() => {
         <p className="productDetail__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam molestie ultrices donec pulvinar quis est bibendum suspendisse viverra.</p>
         <p className="productDetail__quantity">Quantity</p>
         <div className="productDetail__counter">
-          <Counter />
+          <Counter counter={0} />
         </div>
         <div className="productDetail--btn">
           <Button text="Add to cart" type="large" />
@@ -39,10 +39,7 @@ const ProductDetail: React.FC = memo(() => {
           <Text text="Products you may like" type="large-dark" />
         </div>
         <div className="productDetail-popular">
-          <CardProduct type="popular" visibleCounter={true} content="popular" />
-          <CardProduct type="popular" visibleCounter={true} content="popular" />
-          <CardProduct type="popular" visibleCounter={true} content="popular" />
-          <CardProduct type="popular" visibleCounter={true} content="popular" />
+          <CardProductList type="popular" visibleCounter={true} content="popular" />
         </div>
       </div>
     </div>
