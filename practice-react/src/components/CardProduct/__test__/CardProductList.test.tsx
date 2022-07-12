f638e3ecee9bebb6f56c1dbfaa0e773991ae62af
Bui Thi Hoai Thuong <thuong.buihoai@asnet.com.vn>
2022-07-12 17:29:32 +0700

    Writing unit test for component CardProductList

--- practice-react/src/components/CardProduct/__test__/CardProductList.test.tsx
+++ practice-react/src/components/CardProduct/__test__/CardProductList.test.tsx
@@ -1,46 +1,57 @@
+import mockAxios from "@__mocks__/axios";
+import CardProductList from "../CardProductList";
+import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
 import { createMemoryHistory } from "history";
 import { Router } from "react-router-dom";
-import CardProductList from "../CardProductList";
-import "@testing-library/jest-dom";
+import { DataContextProps } from "@common-types/data";
+import { useState } from "react";
+import { DataContext } from "@context/DataContext";
+import { PRODUCT_MOCKING_LIST } from "@__mocks__/constants/product";
+import { PRODUCTS_URL } from "@constants/url";
+import { getData } from "@helpers/fetchApi";
+import { CATEGORY_MOCKING_LIST } from "@__mocks__/constants/category";
+
+jest.mock("react", () => ({
+  ...jest.requireActual("react"),
+  useState: jest.fn(),
+}));
+
+const contextProductMock: DataContextProps = {
+  searchValue: {},
+  setSearchValue: jest.fn(),
+  categories: CATEGORY_MOCKING_LIST,
+  setCategories: jest.fn(),
+  products: PRODUCT_MOCKING_LIST,
+  setProducts: jest.fn(),
+};
 
 describe("CardProductList component", () => {
-  test("should render CardProductList - type is offers component", () => {
-    const history = createMemoryHistory();
-    const { getByTestId } = render(
-      <Router location={history.location} navigator={history}>
-        <CardProductList type="offers" content="offers" />
-      </Router>
+  beforeEach(() => {
+    (useState as jest.Mock).mockImplementation(
+      jest.requireActual("react").useState
     );
-    expect(getByTestId("card-product-list")).toBeInTheDocument();
   });
 
-  test("should render CardProductList - type is best-selling component", () => {
-    const history = createMemoryHistory();
-    const { getByTestId } = render(
-      <Router location={history.location} navigator={history}>
-        <CardProductList type="selling" content="selling" />
-      </Router>
-    );
-    expect(getByTestId("card-product-list")).toBeInTheDocument();
+  afterEach(() => {
+    mockAxios.reset();
   });
 
-  test("should render CardProductList - type is popular component", () => {
-    const history = createMemoryHistory();
-    const { getByTestId } = render(
-      <Router location={history.location} navigator={history}>
-        <CardProductList type="popular" content="popular" />
-      </Router>
-    );
-    expect(getByTestId("card-product-list")).toBeInTheDocument();
+  test("get categories item should call", async () => {
+    mockAxios.get.mockResolvedValueOnce({ data: PRODUCT_MOCKING_LIST });
+    const result = await getData(PRODUCTS_URL);
+    expect(mockAxios.get).toHaveBeenCalledWith(PRODUCTS_URL);
+    expect(result).toEqual(PRODUCT_MOCKING_LIST);
   });
 
   test("matches snapshot", () => {
     const history = createMemoryHistory();
     const { asFragment } = render(
-      <Router location={history.location} navigator={history}>
-        <CardProductList type="popular" content="popular" />
-      </Router>
+      <DataContext.Provider value={contextProductMock}>
+        <Router location={history.location} navigator={history}>
+          <CardProductList type="offers" content="offers" />
+        </Router>
+      </DataContext.Provider>
     );
     expect(asFragment()).toMatchSnapshot();
   });
