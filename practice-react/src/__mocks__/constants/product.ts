79a252d1019c6da7a29be08f89fe9db5ae912e1b
Bui Thi Hoai Thuong <thuong.buihoai@asnet.com.vn>
2022-07-12 17:38:16 +0700

    Create mock data

---
+++ practice-react/src/__mocks__/constants/product.ts
@@ -0,0 +1,54 @@
+import { Product } from "@common-types/product";
+
+const PRODUCT_MOCKING_LIST: Product[] = [
+  {
+    productId: "16466545464646547",
+    categoryId: "1651454657522",
+    productName: "Ice Cream Sundae",
+    productDescription:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam molestie ultrices donec pulvinar quis est bibendum suspendisse viverra.",
+    productQuantity: 5,
+    originalPrice: {
+      value: 20,
+      currency: "$",
+    },
+    discountPrice: {
+      value: 20,
+      currency: "$",
+    },
+    offerQuantity: "3kg",
+    offer: false,
+    bestSelling: true,
+    popular: false,
+    images: {
+      src: "/src/assets/images/ice-cream-sundae.png",
+      alt: "This is ice-cream-sundae images",
+    },
+  },
+  {
+    productId: "1646654668992646547",
+    categoryId: "1651924356334",
+    productName: "Beef-Steak",
+    productDescription:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam molestie ultrices donec pulvinar quis est bibendum suspendisse viverra.",
+    productQuantity: 9,
+    originalPrice: {
+      value: 50,
+      currency: "$",
+    },
+    discountPrice: {
+      value: 50,
+      currency: "$",
+    },
+    offerQuantity: "5kg",
+    offer: false,
+    bestSelling: false,
+    popular: true,
+    images: {
+      src: "/src/assets/images/beef-steak.png",
+      alt: "This is beef-steak images",
+    },
+  },
+];
+
+export { PRODUCT_MOCKING_LIST };
