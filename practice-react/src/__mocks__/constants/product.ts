0dc525974dedba73e729287db38918739debb7df
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-07-13 07:21:09 +0700

    Add constants

--- practice-react/src/__mocks__/constants/product.ts
+++ practice-react/src/__mocks__/constants/product.ts
@@ -1,5 +1,30 @@
 import { Product } from "@common-types/product";
+import iceCreamSundae from "@assets/images/ice-cream-sundae.png";
 
+const PRODUCT_MOCKING: Product = {
+  productId: "16466545464646547",
+  categoryId: "1651454657522",
+  productName: "Ice Cream Sundae",
+  productDescription:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam molestie ultrices donec pulvinar quis est bibendum suspendisse viverra.",
+  productQuantity: 5,
+  originalPrice: {
+    value: 20,
+    currency: "$",
+  },
+  discountPrice: {
+    value: 20,
+    currency: "$",
+  },
+  offerQuantity: "3kg",
+  offer: false,
+  bestSelling: true,
+  popular: false,
+  images: {
+    src: iceCreamSundae,
+    alt: "This is ice-cream-sundae images",
+  },
+};
 const PRODUCT_MOCKING_LIST: Product[] = [
   {
     productId: "16466545464646547",
@@ -51,4 +76,4 @@ const PRODUCT_MOCKING_LIST: Product[] = [
   },
 ];
 
-export { PRODUCT_MOCKING_LIST };
+export { PRODUCT_MOCKING, PRODUCT_MOCKING_LIST };
