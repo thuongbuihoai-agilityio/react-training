4d2c2e12ba622101c331dac719459db8985b4cbd
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-07-13 17:16:56 +0700

    Change path route

--- practice-react/src/app/App.tsx
+++ practice-react/src/app/App.tsx
@@ -2,35 +2,18 @@ import { memo } from "react";
 import { Route, Routes } from "react-router-dom";
 import { SWRConfig } from "swr";
 import { DataProvider } from "@context/DataContext";
-import Header from "@components/common/Header/Header";
-import Categories from "@components/Category/Categories";
-import CardProductOffer from "@components/CardProduct/CardProductOffer/CardProductOffer";
-import CardProductSelling from "@components/CardProduct/CardProductSelling/CardProductSelling";
-import CardProductPopular from "@components/CardProduct/CardProductPopular/CardProductPopular";
-import SignUpSection from "@components/SignUpSection/SignUpSection";
 import Footer from "@components/common/Footer/Footer";
 import ProductList from "@pages/ProductList/ProductList";
 import ProductDetail from "@pages/ProductDetail/ProductDetail";
+import Home from "@pages/Home/Home";
 
 const App: React.FC = memo(() => {
   return (
     <SWRConfig value={{ revalidateOnFocus: false }}>
       <DataProvider>
         <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <Header />
-                <Categories />
-                <CardProductOffer />
-                <CardProductSelling />
-                <CardProductPopular />
-                <SignUpSection />
-              </>
-            }
-          />
-          <Route path="/products" element={<ProductList />} />
+          <Route path="/" element={<Home />} />
+          <Route path={`/products/category/:id`} element={<ProductList />} />
           <Route path={`/products/:id`} element={<ProductDetail />} />
         </Routes>
       </DataProvider>
