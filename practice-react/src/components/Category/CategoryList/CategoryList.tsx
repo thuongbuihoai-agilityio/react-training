e43abe7401528cbe7e964680af41012e63b39f10
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-07-13 17:21:27 +0700

    Add props and change component

--- practice-react/src/components/Category/CategoryList/CategoryList.tsx
+++ practice-react/src/components/Category/CategoryList/CategoryList.tsx
@@ -1,37 +1,35 @@
 import useSWR from "swr";
 import React, { useContext, useEffect } from "react";
 import { Categories } from "@common-types/category";
-import { Link } from "react-router-dom";
 import { getData } from "@helpers/fetchApi";
 import { CATEGORIES_URL } from "@constants/url";
 import { DataContext } from "@context/DataContext";
 import Checkbox from "@components/common/Checkbox/Checkbox";
+import CardCategory from "../CardCategory/CardCategory";
 import "./categoryList.css";
 
 interface CategoryProps {
-  isCheckbox?: boolean;
-  isSelect?: boolean;
   type: string;
+  isSelect?: boolean;
+  isCheckbox?: boolean;
+  onToggleCategory: Function;
+  selectedCategories: string[];
 }
 
 const CategoryList: React.FC<CategoryProps> = ({
-  isCheckbox,
-  isSelect,
   type,
+  isSelect,
+  isCheckbox,
+  onToggleCategory,
+  selectedCategories
 }) => {
   // fetch data with useSWR
   const { data } = useSWR(CATEGORIES_URL, getData<Categories[]>);
-  const { setSearchValue, setCategories, categories } = useContext(DataContext);
+  const { setCategories, categories } = useContext(DataContext);
 
   useEffect(() => {
     setCategories(data);
   }, [data]);
-
-  const handleSearch = (e: React.MouseEvent<HTMLElement>) => {
-    // get current categoryId
-    const categoryId = { categoryId: e.currentTarget.dataset.index };
-    setSearchValue?.(categoryId);
-  };
 
   let className = "";
   switch (type) {
@@ -50,21 +48,19 @@ const CategoryList: React.FC<CategoryProps> = ({
       <div key={category.id}>
         {isSelect && (
           <div className="categoryList__list">
-            <figure className="categoryList__image">
-              <img
-                className="categoryList__images"
-                src={category.images.src}
-                alt={category.images.alt}
-              />
-            </figure>
-            <Link data-index={category.id} className="categoryList__text" to="/products" onClick={handleSearch}>
-              <p data-testid="category-item">{category.name}</p>
-            </Link>
+            <CardCategory
+              onClick={onToggleCategory}
+              category={category} categoryId={category.id} />
           </div>
         )}
         {isCheckbox && (
           <div className="categoryList__checkbox">
-            <Checkbox categoryId={category.id} onClick={handleSearch} value={category.name} />
+            <Checkbox
+              onClick={onToggleCategory}
+              categoryId={category.id}
+              text={category.name}
+              value={selectedCategories.includes(category.id)}
+            />
           </div>
         )}
       </div>
@@ -73,6 +69,16 @@ const CategoryList: React.FC<CategoryProps> = ({
 
   return (
     <div data-testid="category-list" className={className}>
+      {isCheckbox &&
+        <div className="categoryList__checkbox">
+          <Checkbox
+            onClick={onToggleCategory}
+            categoryId=""
+            text="All"
+            value={!selectedCategories.length}
+          />
+        </div>
+      }
       {renderCategoryList()}
     </div>
   );
