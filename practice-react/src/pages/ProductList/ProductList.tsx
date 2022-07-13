ae906bfd54b8e39d83c8c2e5741e51e7fae6e552
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-07-13 17:24:54 +0700

    Handle filter product by categoryId

--- practice-react/src/pages/ProductList/ProductList.tsx
+++ practice-react/src/pages/ProductList/ProductList.tsx
@@ -1,10 +1,46 @@
-import React, { memo } from "react";
+import React, { memo, useCallback, useContext, useMemo, useState } from "react";
+import { DataContext } from "@context/DataContext";
+import { useParams } from "react-router-dom";
 import CategoryList from "@components/Category/CategoryList/CategoryList";
 import NavigationBar from "@components/common/NavigationBar/NavigationBar";
 import CardProductList from "@components/CardProduct/CardProductList";
 import "./productList.css";
 
 const ProductList: React.FC = memo(() => {
+  const { products } = useContext(DataContext);
+  const { id } = useParams();
+  const defaultCategories = useMemo(() => {
+    return id ? [id] : [];
+  }, [id]);
+
+  const [selectedCategories, setSelectedCategories] = useState<string[]>(defaultCategories);
+  const productList = useMemo(() => {
+    return selectedCategories.length
+      ? products.filter((product) =>
+          selectedCategories.includes(product.categoryId)
+        )
+      : products;
+  }, [products, selectedCategories]);
+
+  const handleToggleCategory = useCallback(
+    (categoryId: string, value: string) => {
+      if (categoryId) {
+        if (value) {
+          setSelectedCategories([...selectedCategories, categoryId]);
+        } else {
+          setSelectedCategories(
+            selectedCategories.filter((item) => item !== categoryId)
+          );
+        }
+      } else {
+        if (value) {
+          setSelectedCategories([]);
+        }
+      }
+    },
+    [selectedCategories]
+  );
+
   return (
     <div data-testid="product-list">
       <NavigationBar isThemeDark={true} />
@@ -12,13 +48,19 @@ const ProductList: React.FC = memo(() => {
       <div className="productList">
         <div className="productList__select">
           <p className="productList__select--category">Categories</p>
-          <CategoryList type="checkbox" isCheckbox={true} />
+          <CategoryList
+            selectedCategories={selectedCategories}
+            onToggleCategory={handleToggleCategory}
+            type="checkbox"
+            isCheckbox={true}
+          />
         </div>
         <div className="productList--popular">
           <CardProductList
             type="popular"
-            visibleCounter={true}
             content="popular"
+            visibleCounter={true}
+            productList={productList}
           />
         </div>
       </div>
