c81aeba5d2bef89c5722b97e19e7455f697ab717
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-07-13 17:22:30 +0700

    Handle filter

--- practice-react/src/components/common/Checkbox/Checkbox.tsx
+++ practice-react/src/components/common/Checkbox/Checkbox.tsx
@@ -1,22 +1,29 @@
-import React, { MouseEventHandler } from "react";
+import React from "react";
 import "./checkbox.css";
 
 interface CheckboxProps {
-  value: string;
+  text: string;
   categoryId: string;
-  onClick: MouseEventHandler<HTMLInputElement>;
+  value: boolean;
+  onClick: Function;
 }
 
-const Checkbox: React.FC<CheckboxProps> = ({ value, onClick, categoryId }) => {
+const Checkbox: React.FC<CheckboxProps> = ({ text, onClick, categoryId }) => {
+  const handleCheck = (event: any) => {
+    const value = event.target.checked
+    onClick(categoryId, value);
+  }
+
   return (
     <label className="checkbox__value">
       <input
         type="checkbox"
         name="checkbox"
+        multiple
         data-index={categoryId}
-        onClick={onClick}
+        onClick={handleCheck}
       />
-      {value}
+      {text}
     </label>
   );
 };
