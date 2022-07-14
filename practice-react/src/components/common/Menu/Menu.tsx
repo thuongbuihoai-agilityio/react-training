7841210ba51c5f944192d74ae93b34b5a5a32840
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-07-14 08:04:00 +0700

    Delete handle filter

--- practice-react/src/components/common/Menu/Menu.tsx
+++ practice-react/src/components/common/Menu/Menu.tsx
@@ -1,15 +1,9 @@
-import React, { memo, useContext } from "react";
+import React, { memo } from "react";
 import { Link } from "react-router-dom";
 import { MenuProps, MenuType } from "@common-types/menu";
-import { DataContext } from "@context/DataContext";
 import "./menu.css";
 
 const Menu: React.FC<MenuProps> = memo(({ menuList }) => {
-  const { setSearchValue } = useContext(DataContext);
-  const handleDefaultCategory = () => {
-    const categoryId = "";
-    setSearchValue?.(categoryId);
-  };
   const renderMenuList = (list: MenuType[]) => {
     return list.map((item) => (
       <li className="menu__item" key={item.key}>
@@ -22,7 +16,7 @@ const Menu: React.FC<MenuProps> = memo(({ menuList }) => {
     <div data-testid="menu" className="menu">
       <ul className="menu__list">
         <li data-testid="menu-item" className="menu__item">
-          <Link onClick={handleDefaultCategory} to="/products">Shop</Link>
+          <Link to="/products">Shop</Link>
         </li>
         {renderMenuList(menuList)}
       </ul>
