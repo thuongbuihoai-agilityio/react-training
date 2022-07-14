64cef6772caf3afa75cb58ae35970134d0fd5069
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-07-14 08:01:07 +0700

    Update mock data

--- practice-react/src/__mocks__/constants/product.ts
+++ practice-react/src/__mocks__/constants/product.ts
@@ -1,5 +1,6 @@
 import { Product } from "@common-types/product";
 import iceCreamSundae from "@assets/images/ice-cream-sundae.png";
+import beefSteak from "@assets/images/beef-steak.png";
 
 const PRODUCT_MOCKING: Product = {
   productId: "16466545464646547",
@@ -46,34 +47,10 @@ const PRODUCT_MOCKING_LIST: Product[] = [
     bestSelling: true,
     popular: false,
     images: {
-      src: "/src/assets/images/ice-cream-sundae.png",
+      src: iceCreamSundae,
       alt: "This is ice-cream-sundae images",
     },
-  },
-  {
-    productId: "1646654668992646547",
-    categoryId: "1651924356334",
-    productName: "Beef-Steak",
-    productDescription:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam molestie ultrices donec pulvinar quis est bibendum suspendisse viverra.",
-    productQuantity: 9,
-    originalPrice: {
-      value: 50,
-      currency: "$",
-    },
-    discountPrice: {
-      value: 50,
-      currency: "$",
-    },
-    offerQuantity: "5kg",
-    offer: false,
-    bestSelling: false,
-    popular: true,
-    images: {
-      src: "/src/assets/images/beef-steak.png",
-      alt: "This is beef-steak images",
-    },
-  },
+  }
 ];
 
 export { PRODUCT_MOCKING, PRODUCT_MOCKING_LIST };
