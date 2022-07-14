564ab11636ce97d84b6a322a6d86fba1ee5e79f8
Bui Thi Hoai Thuong <thuong.buihoai@asnet.com.vn>
2022-07-14 17:19:01 +0700

    Refactor code

--- practice-react/src/pages/ProductDetail/ProductDetail.tsx
+++ practice-react/src/pages/ProductDetail/ProductDetail.tsx
@@ -2,12 +2,12 @@ import React, { memo, useContext } from "react";
 import { DataContext } from "@context/DataContext";
 import { useParams } from "react-router-dom";
 import NavigationBar from "@components/common/NavigationBar/NavigationBar";
-import CardProductList from "@components/CardProduct/CardProductList";
 import background from "@assets/images/backgrounds/gray.png";
 import Text from "@components/common/Text/Text";
 import Price from "@components/common/Price/Price";
 import Counter from "@components/common/Counter/Counter";
 import Button from "@components/common/Button/Button";
+import ProductList from "@components/ProductList/ProductList";
 import "./productDetail.css";
 
 const ProductDetail: React.FC = memo(() => {
@@ -17,34 +17,46 @@ const ProductDetail: React.FC = memo(() => {
 
   return (
     <div data-testid="product-detail" className="productDetail">
-      <NavigationBar isThemeDark={true} />
+      <NavigationBar
+        mode={{
+          dark: true,
+        }}
+      />
       <figure>
-        <img className="productDetail--image" src={dataElement?.images.src} alt={dataElement?.images.alt} />
         <img
-          className="productDetail--background"
+          className="productDetail__image"
+          src={dataElement?.images.src}
+          alt={dataElement?.images.alt}
+        />
+        <img
+          className="productDetail__background"
           src={background}
           alt="This is background"
         />
       </figure>
       <div className="productDetail__info">
-        <Text text={dataElement?.productName} type="large" />
+        <Text text={dataElement?.productName} size="large" />
         <p className="productDetail__price">Price</p>
-        <Price value={dataElement?.originalPrice.value} type="original" currency={dataElement?.originalPrice.currency} />
+        <Price
+          value={dataElement?.originalPrice.value}
+          type="original"
+          currency={dataElement?.originalPrice.currency}
+        />
         <p className="productDetail__description"></p>
         <p className="productDetail__quantity">Quantity</p>
         <div className="productDetail__counter">
           <Counter counter={dataElement?.productQuantity} />
         </div>
-        <div className="productDetail--btn">
+        <div className="productDetail__btn">
           <Button text="Add to cart" type="large" />
         </div>
       </div>
-      <div className="productDetail--more">
+      <div className="productDetail__more">
         <div className="productDetail__title">
-          <Text text="Products you may like" type="large-dark" />
+          <Text text="Products you may like" size="large-dark" />
         </div>
-        <div className="productDetail-popular">
-          <CardProductList type="popular" visibleCounter={true} content="popular" />
+        <div className="productDetail__productMore">
+          <ProductList type="popular" visibleCounter={true} content="popular" />
         </div>
       </div>
     </div>
