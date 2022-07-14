f3f82238939978d3689bb7f1699d63ab83e489b3
Bui Thi Hoai Thuong <thuong.buihoai@asnet.com.vn>
2022-07-14 17:11:34 +0700

    Rename component ProductList to Shop

--- practice-react/src/app/App.tsx
+++ practice-react/src/app/App.tsx
@@ -3,9 +3,9 @@ import { Route, Routes } from "react-router-dom";
 import { SWRConfig } from "swr";
 import { DataProvider } from "@context/DataContext";
 import Footer from "@components/common/Footer/Footer";
-import ProductList from "@pages/ProductList/ProductList";
 import ProductDetail from "@pages/ProductDetail/ProductDetail";
 import Home from "@pages/Home/Home";
+import Shop from "@pages/Shop/Shop";
 
 const App: React.FC = memo(() => {
   return (
@@ -13,8 +13,8 @@ const App: React.FC = memo(() => {
       <DataProvider>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path={`/products/category/:id`} element={<ProductList />} />
-          <Route path="/products" element={<ProductList />} />
+          <Route path={`/products/category/:id`} element={<Shop />} />
+          <Route path="/products" element={<Shop />} />
           <Route path={`/products/:id`} element={<ProductDetail />} />
         </Routes>
       </DataProvider>
