0acc50a2b2471fea3943301fa5c57f7aa5e68507
Bui Thi Hoai Thuong <thuong.buihoai@asnet.com.vn>
2022-07-14 17:29:25 +0700

    Change name file Checkbox to CategoryCheck

---
+++ practice-react/src/components/CategoryCheck/CategoryCheck.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./categoryCheck.css";
+
+interface CategoryCheckProps {
+  text: string;
+  categoryId: string;
+  value: boolean;
+  onToggleCategory: Function;
+  checked: boolean;
+}
+
+const CategoryCheck: React.FC<CategoryCheckProps> = ({ text, onToggleCategory, categoryId, checked }) => {
+  const handleCheck = (event: any) => {
+    const value = event.target.checked
+    onToggleCategory(categoryId, value);
+  }
+
+  return (
+    <label className="checkbox__value">
+      <input
+        type="checkbox"
+        name="checkbox"
+        multiple
+        defaultChecked={checked}
+        data-index={categoryId}
+        onClick={handleCheck}
+      />
+      {text}
+    </label>
+  );
+};
+
+export default CategoryCheck;
