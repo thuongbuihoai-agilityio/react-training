f343a448dbd2531133ca3fd347a59261cd3c155c
Bui Thi Hoai Thuong <thuong.buihoai@asnet.com.vn>
2022-07-15 17:21:57 +0700

    Use .env and import

--- practice-react/src/constants/url.ts
+++ practice-react/src/constants/url.ts
@@ -1,5 +1,4 @@
-const BASE_URL = "http://localhost:1234";
-const PRODUCTS_URL = `${BASE_URL}/products`;
-const CATEGORIES_URL = `${BASE_URL}/categories`;
+const PRODUCTS_URL = `${import.meta.env.VITE_BASE_URL}/products`;
+const CATEGORIES_URL = `${import.meta.env.VITE_BASE_URL}/categories`;
 
-export { BASE_URL, PRODUCTS_URL, CATEGORIES_URL };
+export { PRODUCTS_URL, CATEGORIES_URL };
