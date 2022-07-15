0903ad909700d06b023408c949d485c286920627
Bui Thi Hoai Thuong <thuong.buihoai@asnet.com.vn>
2022-07-15 17:20:06 +0700

    Format code and add key

--- practice-react/src/components/common/IconLink/IconLink.tsx
+++ practice-react/src/components/common/IconLink/IconLink.tsx
@@ -6,12 +6,21 @@ import "./iconLink.css";
 const IconLink: React.FC<IconProps> = ({ iconList }) => {
   const renderIconList = (list: IconType[]) => {
     return list.map((item) => (
-      <a className="icon__item" title={item.title} href={item.url}>
+      <a
+        className="icon__item"
+        key={item.key}
+        title={item.title}
+        href={item.url}
+      >
         <Icon iconName={item.iconName} />
       </a>
     ));
   };
-  return <div data-testid="icon-link" className="icon__list">{renderIconList(iconList)}</div>;
+  return (
+    <div data-testid="icon-link" className="icon__list">
+      {renderIconList(iconList)}
+    </div>
+  );
 };
 
 export default IconLink;
