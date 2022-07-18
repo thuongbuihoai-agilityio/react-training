6c4340c57d912051895c33364eb2812a7448db0c
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-07-18 10:28:16 +0700

    Refactor code and fix config .env in jest

--- practice-react/babel.config.ts
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: ["babel-plugin-transform-vite-meta-env"],
-  presets: [
-    ["@babel/preset-env", { targets: { node: "current" } }],
-    ["@babel/preset-typescript"],
-    ["babel-preset-vite"],
-  ],
-};
