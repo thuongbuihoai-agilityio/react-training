e570324ea0462e6ffe79d8c6a440929300c0a976
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-07-18 13:51:06 +0700

    Format code

--- practice-react/src/components/CategoryCheck/CategoryCheck.tsx
+++ practice-react/src/components/CategoryCheck/CategoryCheck.tsx
@@ -9,25 +9,29 @@ interface CategoryCheckProps {
   checked: boolean;
 }
 
-const CategoryCheck: React.FC<CategoryCheckProps> = memo(({ text, onToggleCategory, categoryId, checked }) => {
-  const handleCheck = (event: MouseEvent<HTMLInputElement> & {target: HTMLInputElement}) => {
-    const value = event.target.checked
-    onToggleCategory(categoryId, value);
-  }
+const CategoryCheck: React.FC<CategoryCheckProps> = memo(
+  ({ text, onToggleCategory, categoryId, checked }) => {
+    const handleCheck = (
+      event: MouseEvent<HTMLInputElement> & { target: HTMLInputElement }
+    ) => {
+      const value = event.target.checked;
+      onToggleCategory(categoryId, value);
+    };
 
-  return (
-    <label data-testid="category-check" className="checkbox__value">
-      <input
-        type="checkbox"
-        name="checkbox"
-        multiple
-        defaultChecked={checked}
-        data-index={categoryId}
-        onClick={handleCheck}
-      />
-      {text}
-    </label>
-  );
-});
+    return (
+      <label data-testid="category-check" className="checkbox__value">
+        <input
+          type="checkbox"
+          name="checkbox"
+          multiple
+          defaultChecked={checked}
+          data-index={categoryId}
+          onClick={handleCheck}
+        />
+        {text}
+      </label>
+    );
+  }
+);
 
 export default CategoryCheck;
