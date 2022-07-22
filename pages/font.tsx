b52bcd161321a9e460c2c0d245d9b3d33eb54aff
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-07-22 17:19:50 +0700

    Example: Font

---
+++ pages/font.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import style from "../styles/Home.module.css";
+
+const MyPage: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <p className={style.font}>Hello world!</p>
+    </div>
+  );
+};
+
+export default MyPage;
