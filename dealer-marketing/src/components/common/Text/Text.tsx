b2b7fcdcbe523724cedfea251284e35b59c6859e
Bui Thi Hoai Thuong <thuong.buihoai@asnet.com.vn>
2022-08-01 11:07:02 +0700

    Create Research section

--- dealer-marketing/src/components/common/Text/Text.tsx
+++ dealer-marketing/src/components/common/Text/Text.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import styleText from "./text.module.css";
 
-const Text = () => {
-  return <div>Text</div>;
+interface TextProps {
+  size?: string;
+  text?: string;
+}
+
+const Text: React.FC<TextProps> = ({ text, size }) => {
+  let className = "text";
+  switch (size) {
+    case "normal":
+      className = `${className}-${size}`;
+      break;
+    case "medium":
+      className = `${className}-${size}`;
+      break;
+    case "large":
+      className = `${className}-${size}`;
+      break;
+    default:
+  }
+
+  return <p className={styleText[className]}>{text}</p>;
 };
 
 export default Text;
