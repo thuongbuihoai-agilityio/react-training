9637b46e493165a530d2073869aa7d85b5ddaf0c
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-08-02 07:24:56 +0700

    Create ResearchSection

--- dealer-marketing/src/sections/ResearchSection.tsx
+++ dealer-marketing/src/sections/ResearchSection.tsx
@@ -1,6 +1,7 @@
+import Button from "@src/components/common/Button/Button";
+import Text from "@src/components/common/Text/Text";
 import Image from "next/image";
 import React from "react";
-import Text from "@src/components/common/Text/Text";
 import styleResearch from "./researchSection.module.css";
 
 const ResearchSection = () => {
@@ -16,18 +17,27 @@ const ResearchSection = () => {
             height={438}
           />
           <div className={styleResearch["research-content"]}>
-            <p className="">Research & analysis</p>
+            <p className={styleResearch["research-title"]}>
+              Research & analysis
+            </p>
             <Text
               size="medium"
               text="Exploring Influential and Impactful Automotive Advertising Campaigns"
             />
-            <p>
-              By <strong>Donna Welker</strong> -- July 28, 2022
+            <p className={styleResearch["research-description"]}>
+              By{" "}
+              <span className={styleResearch["research-author"]}>
+                Donna Welker{" "}
+              </span>
+              - July 28, 2022
             </p>
             <Text
               size="normal"
               text="A look at the past, present and future of disruptive marketing in the auto industry Advertising has come a long way since its humble beginnings. There's no doubt about it – marketing remains an ever-changing landscape: What worked a few years ago may not be effective today, and what's popular now may be out of style in a few months...."
             />
+            <div className={styleResearch["research-button"]}>
+              <Button type="primary" text="Read more" />
+            </div>
           </div>
         </div>
       </div>
