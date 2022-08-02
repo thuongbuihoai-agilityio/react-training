4d2b057200cf70505b0455faa567d55be766f578
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-08-02 11:44:07 +0700

    Add props

--- dealer-marketing/src/components/CartBlog/CartBlog.tsx
+++ dealer-marketing/src/components/CartBlog/CartBlog.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import ResearchSection from "@src/sections/ResearchSection";
 import styleCartBlog from "./cartBlog.module.css";
+import { Blogs } from "@src/common-types/blog";
 
-const CartBlog = () => {
+interface CartBlogProps {
+  blog: Blogs;
+}
+
+const CartBlog: React.FC<CartBlogProps> = ({ blog }) => {
   return (
     <div className={styleCartBlog["cart-blog"]}>
       <ResearchSection
+        blog={blog}
         content="left"
         layout="gird"
         isButton={false}
