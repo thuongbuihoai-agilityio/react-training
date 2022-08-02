e06b4c9c735c46381211e3fae002b7262ef0a058
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-08-02 15:03:22 +0700

    Add ContactSection

--- dealer-marketing/src/sections/MainSection/MainSection.tsx
+++ dealer-marketing/src/sections/MainSection/MainSection.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import BlogList from "@src/components/BlogList/BlogList";
-import ResearchSection from "../ResearchSection";
+import ResearchSection from "../ResearchSection/ResearchSection";
+import ContactSection from "../ContactSection/ContactSection";
 
 const MainSection = () => {
   return (
     <div className="container">
       <ResearchSection layout="center" />
       <BlogList />
+      <ContactSection />
     </div>
   );
 };
