3a0c19bb23b1cd761893389aa345a0d509895dd9
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-08-02 17:24:05 +0700

    Create ExpertSection

---
+++ dealer-marketing/src/sections/ExpertSection/ExpertSection.tsx
@@ -0,0 +1,88 @@
+import React from "react";
+import Image from "next/image";
+import Text from "@src/components/common/Text/Text";
+import styleExpertSection from "./expertSection.module.css";
+
+const ExpertSection = () => {
+  return (
+    <div className={styleExpertSection.expert}>
+      <div className={styleExpertSection["expert-title"]}>
+        <Text size="large" text="DMM Expert Podcast Interviews" />
+      </div>
+      <div className={styleExpertSection["expert-list"]}>
+        <div className={styleExpertSection["expert-section"]}>
+          <figure className={styleExpertSection["expert-layout"]}>
+            <Image
+              src="/images/avatar/ibrahim-mesbah.avif"
+              alt="This is image of Mr adam dennis"
+              className={styleExpertSection["expert-image"]}
+              width={130}
+              height={130}
+            />
+          </figure>
+          <p className={styleExpertSection["expert-description"]}>
+            Evaluating OEM Programs and Providing Better Customer Experiences
+            with Ibrahim Mesbah
+          </p>
+          <figure className={styleExpertSection["expert-contact"]}>
+            <Image
+              src="/images/inside-auto.png"
+              alt="This is image inside auto"
+              className={styleExpertSection["expert-image-contact"]}
+              width={135}
+              height={60}
+            />
+          </figure>
+        </div>
+        <div className={styleExpertSection["expert-section"]}>
+          <figure className={styleExpertSection["expert-layout"]}>
+            <Image
+              src="/images/avatar/ilana-shabtay.avif"
+              alt="This is image of Mr adam dennis"
+              className={styleExpertSection["expert-image"]}
+              width={130}
+              height={130}
+            />
+          </figure>
+          <p className={styleExpertSection["expert-description"]}>
+            Experimarketing Episode 27: No Inventory? No Problem.
+          </p>
+          <figure className={styleExpertSection["expert-contact"]}>
+            <Image
+              src="/images/experimaketing.png"
+              alt="This is image inside auto"
+              className={styleExpertSection["expert-image-contact"]}
+              width={135}
+              height={60}
+            />
+          </figure>
+        </div>
+        <div className={styleExpertSection["expert-section"]}>
+          <figure className={styleExpertSection["expert-layout"]}>
+            <Image
+              src="/images/avatar/brent-albrecht.png"
+              alt="This is image of Mr adam dennis"
+              className={styleExpertSection["expert-image"]}
+              width={130}
+              height={130}
+            />
+          </figure>
+          <p className={styleExpertSection["expert-description"]}>
+            Website Clicks?! they`re Still Clickin
+          </p>
+          <figure className={styleExpertSection["expert-contact"]}>
+            <Image
+              src="/images/experimaketing.png"
+              alt="This is image inside auto"
+              className={styleExpertSection["expert-image-contact"]}
+              width={135}
+              height={60}
+            />
+          </figure>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ExpertSection;
