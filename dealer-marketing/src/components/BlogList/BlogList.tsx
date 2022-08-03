7dbd8f4f6113dfff57c2ab69ad5d709d1f43d273
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-08-03 10:48:25 +0700

    Update code, show three products

--- dealer-marketing/src/components/BlogList/BlogList.tsx
+++ dealer-marketing/src/components/BlogList/BlogList.tsx
@@ -1,17 +1,17 @@
+import React from "react";
+import useSWR from "swr";
 import { Blogs } from "@src/common-types/blog";
 import { BLOG_URL } from "@src/constants/url";
 import { getData } from "@src/helpers/fetchApi";
-import React from "react";
-import useSWR from "swr";
 import CardBlog from "../CardBlog/CardBlog";
 import styleBlogList from "./blogList.module.css";
 
 const BlogList = () => {
   const { data } = useSWR(BLOG_URL, getData<Blogs[]>);
-
+  const arrBlog = data?.slice(0, 3);
   return (
     <div className={styleBlogList["blog-list"]}>
-      {data?.map((blog) => (
+      {arrBlog?.map((blog) => (
         <div key={blog.blogId}>
           <CardBlog blog={blog} />
         </div>
