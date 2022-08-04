09551df4d7c1911aa7982a5d4c867a41b4638d62
Bui Thi Hoai Thuong <thuong.buihoai@asnet.com.vn>
2022-08-04 17:10:13 +0700

    Save data to context

--- dealer-marketing/src/components/BlogList/BlogList.tsx
+++ dealer-marketing/src/components/BlogList/BlogList.tsx
@@ -1,14 +1,21 @@
-import React from "react";
 import useSWR from "swr";
+import React, { useContext, useEffect } from "react";
 import { Blogs } from "@common-types/blog";
 import { BLOG_URL } from "@constants/url";
 import { getData } from "@helpers/fetchApi";
+import { DataContext } from "@context/DataContext";
 import CardBlog from "../CardBlog/CardBlog";
 import styleBlogList from "./blogList.module.css";
 
 const BlogList = () => {
+  const { blogs, setBlogs } = useContext(DataContext);
   const { data } = useSWR(BLOG_URL, getData<Blogs[]>);
-  const arrBlog = data?.slice(0, 3);
+
+  useEffect(() => {
+    setBlogs(data);
+  }, [data]);
+
+  const arrBlog = blogs?.slice(0, 3);
   return (
     <div className={styleBlogList["blog-list"]}>
       {arrBlog?.map((blog) => (
