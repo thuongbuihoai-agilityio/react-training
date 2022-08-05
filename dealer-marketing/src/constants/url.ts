dee0b53883afd0bb3feeaa4d7b76980c50c8fd92
Bui Thi Hoai Thuong <thuong.buihoai@asnet.com.vn>
2022-08-05 09:44:35 +0700

    Set environment variables

--- dealer-marketing/src/constants/url.ts
+++ dealer-marketing/src/constants/url.ts
@@ -1,5 +1,4 @@
-const BASE_URL = "http://localhost:1234";
-const BLOG_URL = `${BASE_URL}/blogs`;
-const EXPERT_URL = `${BASE_URL}/experts`;
+const BLOG_URL = `${process.env.NEXT_PUBLIC_BASE_URL}/blogs`;
+const EXPERT_URL = `${process.env.NEXT_PUBLIC_BASE_URL}/experts`;
 
 export { BLOG_URL, EXPERT_URL };
