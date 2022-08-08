f356cb7d6015657b2cc013e633ec2b2a75fca1c8
Bui Thi Hoai Thuong <thuong.buihoai@asnet.com.vn>
2022-08-08 15:59:29 +0700

    Refactor: Handle error

--- dealer-marketing/src/components/ExpertList/index.tsx
+++ dealer-marketing/src/components/ExpertList/index.tsx
@@ -1,10 +1,15 @@
 import React, { useContext } from "react";
+import Error from "next/error";
 import { DataContext } from "@context/DataContext";
 import CardExpert from "../CardExpert";
 import styleExpertList from "./expertList.module.css";
 
 const ExpertList = () => {
-  const { experts } = useContext(DataContext);
+  const { experts, errorCode } = useContext(DataContext);
+
+  if (errorCode) {
+    return <Error statusCode={errorCode} />;
+  }
 
   return (
     <div className={styleExpertList["expert-list"]}>
