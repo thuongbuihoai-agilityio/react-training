00aedabc9bc11cab3d2d040d9038a7cf65bc273c
Bui Thi Hoai Thuong <thuong.buihoai@asnet.com.vn>
2022-08-09 09:44:48 +0700

    Refactor: Create data mocking for blog

---
+++ dealer-marketing/src/constants/blog.ts
@@ -0,0 +1,17 @@
+import { Blog } from "@common-types/blog";
+
+const BLOG_MOCKING: Blog = {
+  blogId: "1",
+  title: "Exploring Influential and Impactful Automotive Advertising Campaigns",
+  slug: "exploring-influential-and-impactful-automotive-advertising-campaigns",
+  createDate: "July 28, 2022",
+  expertId: "Donna Welker",
+  image: {
+    url: "/images/past-present-future.png",
+    alt: "This is image past present future",
+  },
+  description:
+    "Advertising has come a long way since its humble beginnings. There's no doubt about it – marketing remains an ever-changing landscape: What worked a few years ago may not be effective today, and what's popular now may be out of style in a few months. Nowhere is this more relevant than in the automotive industry, where new campaigns hold the potential to make or break a company. This article will explore some disruptive and impactful automotive marketing campaigns throughout the past century. We'll explore what made them successful and see how they changed the marketing landscape forever.",
+};
+
+export { BLOG_MOCKING };
