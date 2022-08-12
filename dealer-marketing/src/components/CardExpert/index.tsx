286d35b3c12eefe7ff11ae6e60715841e130d270
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-08-12 16:50:27 +0700

    Create constants default value

--- dealer-marketing/src/components/CardExpert/index.tsx
+++ dealer-marketing/src/components/CardExpert/index.tsx
@@ -1,28 +1,16 @@
 import React from "react";
 import Image from "next/image";
-import Link from "next/link";
 import { Expert } from "@common-types/expert";
 import { Button, Text } from "@components/common";
 import styleCardExpert from "./cardExpert.module.css";
 import { useRouter } from "next/router";
+import { EXPERT_MOCKING } from "@constants/expert";
 
 interface CardExpertProps {
   expert: Expert;
 }
 
-const CardExpert: React.FC<CardExpertProps> = ({
-  expert = {
-    name: "Matt-Childers",
-    slug: "matt-childers",
-    info: "DealersLink",
-    image: {
-      url: "/images/avatar/matt-childers.png",
-      alt: "This is image of Mr Matt-Childers",
-    },
-    description:
-      "Brent Albrecht is currently the Vice President of Business Development at Friendemic. Friendemic provides social media and online reputation services for clients and agencies across the globe. Specialties: Marketing Strategy, Product development and launch, Social Media Marketing, Business Development, Lead Generation, Sales Promotion, Digital Marketing, Database Marketing.",
-  },
-}) => {
+const CardExpert: React.FC<CardExpertProps> = ({ expert = EXPERT_MOCKING }) => {
   const router = useRouter();
   return (
     <div
@@ -47,11 +35,12 @@ const CardExpert: React.FC<CardExpertProps> = ({
       <div className={styleCardExpert["card-info"]}>
         <Text itemProp="description" text={expert?.info} />
       </div>
-      <Link itemProp="url" href={`/expert-page/${expert.slug}`}>
-        <div className={styleCardExpert["card-button"]}>
-          <Button icon text="" />
-        </div>
-      </Link>
+      <div className={styleCardExpert["card-button"]}>
+        <Button
+          icon
+          onClick={() => router.push(`/expert-page/${expert.slug}`)}
+        />
+      </div>
     </div>
   );
 };
