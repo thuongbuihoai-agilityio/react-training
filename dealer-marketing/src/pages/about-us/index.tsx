5c221949d65f5d2c004cd67709db1e10bbe2727c
Bui Thi Hoai Thuong <thuong.buihoai@asnet.com.vn>
2022-08-15 08:17:05 +0700

    Refactor: Delete <noscript>

--- dealer-marketing/src/pages/about-us/index.tsx
+++ dealer-marketing/src/pages/about-us/index.tsx
@@ -38,15 +38,6 @@ const AboutPage = () => (
     </Head>
 
     <body>
-      {/* Google Tag Manager (noscript) */}
-      <noscript>
-        <iframe
-          src="https://www.googletagmanager.com/ns.html?id=GTM-WK9ZN93"
-          height="0"
-          width="0"
-          style="display:none;visibility:hidden"></iframe>
-      </noscript>
-      {/* End Google Tag Manager (noscript) */}
       <Header />
       <Navigation />
       <Banner
