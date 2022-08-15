cd5209011889cbb2321abb14ba0a41fd4f6fb384
Bui Thi Hoai Thuong <thuong.buihoai@asnet.com.vn>
2022-08-15 15:13:52 +0700

    Create type for linkingData

---
+++ dealer-marketing/src/constants/linkingData.ts
@@ -0,0 +1,10 @@
+import { SchemaPage } from "../common-types/linkingData";
+
+const jsonLinkingData: SchemaPage = {
+  description: "This is web dealer-marketing",
+  createDate: "29/7/2022",
+  title: "Dealer-Marketing",
+  image: "/favicon.ico",
+};
+
+export { jsonLinkingData };
