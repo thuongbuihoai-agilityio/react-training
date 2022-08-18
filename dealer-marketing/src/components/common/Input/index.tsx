412ca1f5717d6820f9706d2f5f4b6f60a8fbad5b
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-08-18 18:13:43 +0700

    refactor: create component SearchBox and handle search in component SearchBox

--- dealer-marketing/src/components/common/Input/index.tsx
+++ dealer-marketing/src/components/common/Input/index.tsx
@@ -1,13 +1,29 @@
-import React, { memo } from "react";
+import React, { ChangeEventHandler, memo, MouseEventHandler } from "react";
 import styleInput from "./input.module.css";
 
 interface InputProps {
   type: string;
   placeholder: string;
+  onChange?: ChangeEventHandler<HTMLInputElement>;
+  onClick?: MouseEventHandler<HTMLInputElement>;
+  onBlur?: ChangeEventHandler<HTMLInputElement>;
 }
 
-const Input: React.FC<InputProps> = ({ type, placeholder }) => (
-  <input className={styleInput.input} type={type} placeholder={placeholder} />
+const Input: React.FC<InputProps> = ({
+  type,
+  placeholder,
+  onClick = () => null,
+  onChange = () => null,
+  onBlur = () => null,
+}) => (
+  <input
+    onChange={onChange}
+    onClick={onClick}
+    onBlur={onBlur}
+    className={styleInput.input}
+    type={type}
+    placeholder={placeholder}
+  />
 );
 
 export default memo(Input);
--- dealer-marketing/src/components/common/Menu/index.tsx
+++ dealer-marketing/src/components/common/Menu/index.tsx
@@ -1,12 +1,8 @@
 import React, { memo } from "react";
 import { MenuProps, MenuType, MenuTypeProp } from "@common-types/menu";
-import { MENU_LIST } from "@constants/menu";
 import menuStyle from "./menu.module.css";
 
-const Menu: React.FC<MenuProps> = ({
-  menuList = MENU_LIST,
-  type = MenuTypeProp.dark,
-}) => {
+const Menu: React.FC<MenuProps> = ({ menuList, type = MenuTypeProp.dark }) => {
   const renderMenuList = (list: MenuType[]) => {
     return list?.map((item) => (
       <li className={menuStyle[`menu-item-${type}`]} key={item.key}>
