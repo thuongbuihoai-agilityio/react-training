9612e4f1243fd278bae05fd5dd5f24481c050de2
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-08-18 11:53:11 +0700

    refactor: move handle to context

--- dealer-marketing/src/components/common/Navigation/index.tsx
+++ dealer-marketing/src/components/common/Navigation/index.tsx
@@ -1,16 +1,30 @@
-import React, { useCallback, useContext, useState } from "react";
+import React, { useCallback, useContext, useEffect, useState } from "react";
 import Icon, { IconType } from "../Icon";
 import { MENU_LIST } from "@constants/menu";
 import { MenuTypeProp } from "@common-types/menu";
-import { DataContext } from "@context/DataContext";
 import { IMAGE } from "@constants/image";
+import { BlogContext } from "@context/BlogContext";
 import Logo from "../Logo";
 import Menu from "../Menu";
 import styleNavigation from "./navigation.module.css";
 
 const Navigation: React.FC = () => {
   const [openModal, setOpenModal] = useState<boolean>(false);
-  const { setSearchValue } = useContext(DataContext);
+  const { setSearchValue } = useContext(BlogContext);
+  const [viewOnBlogs, setViewOnBlogs] = useState<boolean>(true);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      setViewOnBlogs(viewOnBlogs);
+    });
+  }, []);
+
+  const scrollToBlogs = () => {
+    window.scrollTo({
+      top: 1300,
+      behavior: "smooth", // for smoothly scrolling
+    });
+  };
 
   const handleToggleModal = useCallback(() => {
     setOpenModal(!openModal);
@@ -36,6 +50,7 @@ const Navigation: React.FC = () => {
             type="text"
             placeholder="Search the site..."
             onChange={handleSearch}
+            onClick={scrollToBlogs}
           />
         </div>
       )}
