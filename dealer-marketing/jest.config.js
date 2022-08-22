c9e12eb2a1ca68048df14a2483a7993ff8af68c0
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-08-22 08:13:31 +0700

    feat: storybook and unit test

---
+++ dealer-marketing/jest.config.js
@@ -0,0 +1,94 @@
+// module.exports = {
+//   preset: "ts-jest",
+//   roots: ["<rootDir>/src"],
+//   modulePaths: ["<rootDir>/src"],
+//   collectCoverage: true,
+//   transform: {
+//     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+//       "<rootDir>/__mocks__/fileMock.js",
+//     "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js",
+//     "\\.(css|less)$": "identity-obj-proxy",
+//     "^.+\\js$": "babel-jest",
+//   },
+//   // on node 14.x coverage provider v8 offers good speed and more or less good report
+//   coverageProvider: "v8",
+//   moduleNameMapper: {
+//     "^@root(.*)$": "<rootDir>/src$1",
+//     "^@/(.*)$": "<rootDir>/src/$1",
+//     "^@pages(.*)$": "<rootDir>/src/pages/$1",
+//     "^@components(.*)$": "<rootDir>/src/components/$1",
+//     "^@common(.*)$": "<rootDir>/src/components/common/$1",
+//     "^@layouts(.*)$": "<rootDir>/src/layouts/$1",
+//     "^@common-types(.*)$": "<rootDir>/src/common-types/$1",
+//     "^@constants(.*)$": "<rootDir>/src/constants/$1",
+//     "^@context(.*)$": "<rootDir>/src/context/$1",
+//   },
+//   // Add more setup options before each test is run
+//   testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
+//   setupFilesAfterEnv: ["./setupTests.ts"],
+//   transform: {
+//     // Use babel-jest to transpile tests with the next/babel preset
+//     // https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
+//     "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }],
+//   },
+//   transformIgnorePatterns: [
+//     "/node_modules/",
+//     "^.+\\.module\\.(css|sass|scss)$",
+//   ],
+//   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(tsx|ts)$",
+//   testEnvironment: "jsdom",
+//   moduleDirectories: ["node_modules", "bower_components", "shared", "src"],
+//   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+// };
+
+// jest.config.js
+module.exports = {
+  roots: ["<rootDir>/src"],
+  modulePaths: ["<rootDir>/src"],
+  collectCoverage: true,
+  // on node 14.x coverage provider v8 offers good speed and more or less good report
+  coverageProvider: "v8",
+  collectCoverageFrom: [
+    "**/*.{js,jsx,ts,tsx}",
+    "!**/*.d.ts",
+    "!**/*.stories.tsx",
+    "!**/*.props.ts",
+    "!**/node_modules/**",
+    "!<rootDir>/out/**",
+    "!<rootDir>/.next/**",
+    "!<rootDir>/*.config.js",
+    "!<rootDir>/coverage/**",
+    "!<rootDir>/src/pages/api/**",
+    "!<rootDir>/src/types/**",
+    "!<rootDir>/src/services/controllers/**",
+    "!**/*fileSystem.ts",
+  ],
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/__mocks__/fileMock.js",
+    "\\.css$": "<rootDir>/src/__mocks__/styleMock.js",
+    "@mocks/(.*)$": "<rootDir>/src/__mocks__/mockData.js",
+    "^@root(.*)$": "<rootDir>/src$1",
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "^@pages(.*)$": "<rootDir>/src/pages/$1",
+    "^@components(.*)$": "<rootDir>/src/components/$1",
+    "^@common(.*)$": "<rootDir>/src/components/common/$1",
+    "^@layouts(.*)$": "<rootDir>/src/layouts/$1",
+    "^@common-types(.*)$": "<rootDir>/src/common-types/$1",
+    "^@constants(.*)$": "<rootDir>/src/constants/$1",
+    "^@context(.*)$": "<rootDir>/src/context/$1",
+  },
+  // Add more setup options before each test is run
+  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  testEnvironment: "jest-environment-jsdom",
+  transform: {
+    // Use babel-jest to transpile tests with the next/babel preset
+    // https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
+    "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }],
+  },
+  transformIgnorePatterns: [
+    "/node_modules/",
+    "^.+\\.module\\.(css|sass|scss)$",
+  ],
+};
