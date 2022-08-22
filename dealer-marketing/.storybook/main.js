d9d7136de1e8bdf7e36ad25d1458748f74b90ff7
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-08-22 17:16:32 +0700

    ci: config storybook

--- dealer-marketing/.storybook/main.js
+++ dealer-marketing/.storybook/main.js
@@ -1,9 +1,7 @@
+const path = require("path");
 const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 module.exports = {
-  stories: [
-    "../src/**/*.stories.@(tsx|mdx)",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)",
-  ],
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
--- dealer-marketing/.storybook/preview.js
+++ dealer-marketing/.storybook/preview.js
@@ -1,4 +1,6 @@
+import * as NextImage from "next/image";
 import "../src/styles/main.css";
+const OriginalNextImage = NextImage.default;
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -8,3 +10,7 @@ export const parameters = {
     },
   },
 };
+Object.defineProperty(NextImage, "default", {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+});
