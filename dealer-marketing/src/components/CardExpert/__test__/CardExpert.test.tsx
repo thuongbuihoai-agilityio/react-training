4cdeda4246543500efb37ccf3c40b38815051994
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-08-23 15:46:04 +0700

    feat: writing unit test for component CardExpert

---
+++ dealer-marketing/src/components/CardExpert/__test__/CardExpert.test.tsx
@@ -0,0 +1,34 @@
+import { ExpertContextProps } from "@common-types/expert";
+import { EXPERT_MOCKING, EXPERT_MOCKING_LIST } from "@constants/expert";
+import { ExpertContext } from "@context/ExpertContext";
+import { render } from "@testing-library/react";
+import CardExpert from "..";
+
+const contextExpertMocking: ExpertContextProps = {
+  experts: EXPERT_MOCKING_LIST,
+  errorCode: 0,
+  setExperts: jest.fn(),
+  setErrorCode: jest.fn(),
+  handleUpdateExperts: jest.fn(),
+};
+
+describe("CardExpert component", () => {
+  test("Should render CardExpert component", () => {
+    const { getByTestId } = render(
+      <ExpertContext.Provider value={contextExpertMocking}>
+        <CardExpert expert={EXPERT_MOCKING} />
+      </ExpertContext.Provider>,
+    );
+    const cardExpert = getByTestId("card-expert");
+    expect(cardExpert).toBeInTheDocument();
+  });
+
+  test("Matches snapshot", () => {
+    const { container } = render(
+      <ExpertContext.Provider value={contextExpertMocking}>
+        <CardExpert expert={EXPERT_MOCKING} />
+      </ExpertContext.Provider>,
+    );
+    expect(container).toMatchSnapshot();
+  });
+});
