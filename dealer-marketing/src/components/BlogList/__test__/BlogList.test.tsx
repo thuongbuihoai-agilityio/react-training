1b2eff981d820eca9850b4bb0113432027cc31cb
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-08-23 15:42:32 +0700

    feat: writing unit test for component Banner

---
+++ dealer-marketing/src/components/BlogList/__test__/BlogList.test.tsx
@@ -0,0 +1,37 @@
+import { BlogContextProps } from "@common-types/blog";
+import { BLOG_MOCKING_LIST } from "@constants/blog";
+import { BlogContext } from "@context/BlogContext";
+import { render } from "@testing-library/react";
+import BlogList from "..";
+
+const contextBlogMocking: BlogContextProps = {
+  searchValue: "",
+  setSearchValue: jest.fn(),
+  blogs: BLOG_MOCKING_LIST,
+  blogList: BLOG_MOCKING_LIST,
+  errorCode: 0,
+  setBlogs: jest.fn(),
+  setErrorCode: jest.fn(),
+  handleUpdateBlogs: jest.fn(),
+};
+
+describe("BlogList component", () => {
+  test("Should render BlogList component", () => {
+    const { getByTestId } = render(
+      <BlogContext.Provider value={contextBlogMocking}>
+        <BlogList />
+      </BlogContext.Provider>,
+    );
+    const blogList = getByTestId("blog-list");
+    expect(blogList).toBeInTheDocument();
+  });
+
+  test("Matches snapshot", () => {
+    const { container } = render(
+      <BlogContext.Provider value={contextBlogMocking}>
+        <BlogList />
+      </BlogContext.Provider>,
+    );
+    expect(container).toMatchSnapshot();
+  });
+});
