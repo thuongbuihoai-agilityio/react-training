c7cf7858b492c7220b39e8a07c0be69e06985830
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-08-23 15:49:01 +0700

    feat: create constants data mocking

--- dealer-marketing/src/constants/expert.ts
+++ dealer-marketing/src/constants/expert.ts
@@ -13,4 +13,30 @@ const EXPERT_MOCKING: Expert = {
     "Brent Albrecht is currently the Vice President of Business Development at Friendemic. Friendemic provides social media and online reputation services for clients and agencies across the globe. Specialties: Marketing Strategy, Product development and launch, Social Media Marketing, Business Development, Lead Generation, Sales Promotion, Digital Marketing, Database Marketing.",
 };
 
-export { EXPERT_MOCKING };
+const EXPERT_MOCKING_LIST: Expert[] = [
+  {
+    expertId: "1",
+    name: "David Adcock",
+    slug: "david-adcock",
+    info: "Binary Auto",
+    image: {
+      url: "/images/avatar/david-adcock.png",
+      alt: "This is image of Mr adam-dennis",
+    },
+    description:
+      "Brent Albrecht is currently the Vice President of Business Development at Friendemic. Friendemic provides social media and online reputation services for clients and agencies across the globe. Specialties: Marketing Strategy, Product development and launch, Social Media Marketing, Business Development, Lead Generation, Sales Promotion, Digital Marketing, Database Marketing.",
+  },
+  {
+    expertId: "2",
+    name: "Brent-Albrecht",
+    slug: "brent-albrecht",
+    info: "Friendemic",
+    image: {
+      url: "/images/avatar/brent-albrecht.png",
+      alt: "This is image of Mr adam-dennis",
+    },
+    description:
+      "Brent Albrecht is currently the Vice President of Business Development at Friendemic. Friendemic provides social media and online reputation services for clients and agencies across the globe. Specialties: Marketing Strategy, Product development and launch, Social Media Marketing, Business Development, Lead Generation, Sales Promotion, Digital Marketing, Database Marketing.",
+  },
+];
+export { EXPERT_MOCKING, EXPERT_MOCKING_LIST };
