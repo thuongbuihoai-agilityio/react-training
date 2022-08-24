59e50e7873e34204b2ca59ab34f5371e1d893007
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-08-24 17:25:29 +0700

    feat: update test with default value

--- dealer-marketing/src/components/common/Icon/__test__/Icon.test.tsx
+++ dealer-marketing/src/components/common/Icon/__test__/Icon.test.tsx
@@ -2,38 +2,29 @@ import { fireEvent, render } from "@testing-library/react";
 import Icon, { IconType } from "../index";
 
 describe("Icon component", () => {
-  const onClickButtonIcon = jest.fn();
   test("Should render Icon component with iconSearch", () => {
-    const { getByTestId } = render(
-      <Icon onClick={onClickButtonIcon} iconName={IconType.search} />,
-    );
+    const { getByTestId } = render(<Icon iconName={IconType.search} />);
     const icon = getByTestId("icon");
     fireEvent.click(icon);
-    expect(onClickButtonIcon).toHaveBeenCalled();
+    expect(icon).toBeInTheDocument();
   });
 
   test("Should render Icon component with iconLetter", () => {
-    const { getByTestId } = render(
-      <Icon onClick={onClickButtonIcon} iconName={IconType.letter} />,
-    );
+    const { getByTestId } = render(<Icon iconName={IconType.letter} />);
     const icon = getByTestId("icon");
     fireEvent.click(icon);
-    expect(onClickButtonIcon).toHaveBeenCalled();
+    expect(icon).toBeInTheDocument();
   });
 
   test("Should render Icon component with iconArrowRight", () => {
-    const { getByTestId } = render(
-      <Icon onClick={onClickButtonIcon} iconName={IconType.arrowRight} />,
-    );
+    const { getByTestId } = render(<Icon iconName={IconType.arrowRight} />);
     const icon = getByTestId("icon");
     fireEvent.click(icon);
-    expect(onClickButtonIcon).toHaveBeenCalled();
+    expect(icon).toBeInTheDocument();
   });
 
   test("Matches snapshot", () => {
-    const { container } = render(
-      <Icon onClick={onClickButtonIcon} iconName={IconType.search} />,
-    );
+    const { container } = render(<Icon iconName={IconType.search} />);
     expect(container).toMatchSnapshot();
   });
 });
