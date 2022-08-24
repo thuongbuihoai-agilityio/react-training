ca92e383ea4a201b26b2a410525eb65f8e92dbd5
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-08-24 17:28:19 +0700

    feat: create test for Expert page

---
+++ dealer-marketing/src/pages/expert-page/__test__/ExpertPage/ExpertPage.test.tsx
@@ -0,0 +1,34 @@
+import { ExpertContextProps } from "@common-types/expert";
+import { EXPERT_MOCKING_LIST } from "@constants/expert";
+import { ExpertContext } from "@context/ExpertContext";
+import { render } from "@testing-library/react";
+import OurExpertPage from "@pages/expert-page";
+
+const contextExpertMocking: ExpertContextProps = {
+  experts: EXPERT_MOCKING_LIST,
+  errorCode: 0,
+  setExperts: jest.fn(),
+  setErrorCode: jest.fn(),
+  handleUpdateExperts: jest.fn(),
+};
+
+describe("ExpertPage", () => {
+  test("Should render ExpertPage", () => {
+    const { getByTestId } = render(
+      <ExpertContext.Provider value={contextExpertMocking}>
+        <OurExpertPage experts={EXPERT_MOCKING_LIST} errorCode={""} />
+      </ExpertContext.Provider>,
+    );
+    const expertPage = getByTestId("layout");
+    expect(expertPage).toBeInTheDocument();
+  });
+
+  test("Matches snapshot", () => {
+    const { container } = render(
+      <ExpertContext.Provider value={contextExpertMocking}>
+        <OurExpertPage experts={EXPERT_MOCKING_LIST} errorCode={""} />
+      </ExpertContext.Provider>,
+    );
+    expect(container).toMatchSnapshot();
+  });
+});
