63cb5e9cebdee438e0fed6e0b1c292109b1000d8
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-08-24 17:29:56 +0700

    feat: writing unit test for pages

--- dealer-marketing/src/constants/url.ts
+++
@@ -1,4 +0,0 @@
-const BLOG_URL = `${process.env.NEXT_PUBLIC_BASE_URL}/blogs`;
-const EXPERT_URL = `${process.env.NEXT_PUBLIC_BASE_URL}/experts`;
-
-export { BLOG_URL, EXPERT_URL };
--- dealer-marketing/src/pages/_app.tsx
+++ dealer-marketing/src/pages/_app.tsx
@@ -3,14 +3,12 @@ import { BlogProvider } from "@context/BlogContext";
 import { ExpertProvider } from "@context/ExpertContext";
 import "../styles/main.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <BlogProvider>
-      <ExpertProvider>
-        <Component {...pageProps} />
-      </ExpertProvider>
-    </BlogProvider>
-  );
-}
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <BlogProvider>
+    <ExpertProvider>
+      <Component {...pageProps} />
+    </ExpertProvider>
+  </BlogProvider>
+);
 
 export default MyApp;
