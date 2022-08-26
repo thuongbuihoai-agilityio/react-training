e2480e3d9f2b6c7bb58d2f2aba23d72d48f117dc
Bui Thi Hoai Thuong <thuong.buihoai@asnet.com.vn>
2022-08-26 16:25:49 +0700

    feat: writing unit test for app

---
+++ dealer-marketing/src/pages/__test__/App/App.test.tsx
@@ -0,0 +1,45 @@
+import { ExpertContextProps } from "@common-types/expert";
+import { EXPERT_MOCKING_LIST } from "@constants/expert";
+import { ExpertContext } from "@context/ExpertContext";
+import { BlogContextProps } from "@common-types/blog";
+import { BLOG_MOCKING_LIST } from "@constants/blog";
+import { BlogContext } from "@context/BlogContext";
+import { render } from "@testing-library/react";
+import { NextPage } from "next";
+import MyApp from "@pages/_app";
+
+const contextBlogMocking: BlogContextProps = {
+  searchValue: "",
+  setSearchValue: jest.fn(),
+  blogs: BLOG_MOCKING_LIST,
+  blogList: BLOG_MOCKING_LIST,
+  errorCode: 0,
+  setBlogs: jest.fn(),
+  setErrorCode: jest.fn(),
+  handleUpdateBlogs: jest.fn(),
+};
+
+const contextExpertMocking: ExpertContextProps = {
+  experts: EXPERT_MOCKING_LIST,
+  errorCode: 0,
+  setExperts: jest.fn(),
+  setErrorCode: jest.fn(),
+  handleUpdateExperts: jest.fn(),
+};
+
+describe("Banner component", () => {
+  const pageProps = {};
+  const mockComponent: NextPage = () => {
+    return null;
+  };
+  test("renders snapshot of app component", () => {
+    const tree = render(
+      <ExpertContext.Provider value={contextExpertMocking}>
+        <BlogContext.Provider value={contextBlogMocking}>
+          <MyApp pageProps={pageProps} Component={mockComponent} />
+        </BlogContext.Provider>
+      </ExpertContext.Provider>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+});
