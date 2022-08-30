8c15319c2d0abdddd63433fd93c64ad946a5b5d9
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2022-08-30 17:25:45 +0700

    refactor: use <Text />

--- dealer-marketing/src/pages/404.tsx
+++ dealer-marketing/src/pages/404.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Link from "next/link";
-import { Button } from "@components/common";
+import { Button, Text } from "@components/common";
 import style from "../pages/style.module.css";
+import { TextType } from "@components/common/Text";
 
 const ErrorPage: React.FC = () => {
   return (
     <div data-testid="error-page" className={style["status-not-found"]}>
       <div className={style["status-number"]}>404</div>
-      <p>This page could not be found.</p>
+      <Text size={TextType.regular} text="This page could not be found." />
       <div className={style["status-back"]}>
         <Link href="/" passHref>
           <Button icon text="Back to home" />
