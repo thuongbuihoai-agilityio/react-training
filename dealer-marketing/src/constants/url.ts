fea6d27f481e64cb3560719719ceb431d9cd142f
Bui Thi Hoai Thuong <thuong.buihoai@asnet.com.vn>
2022-09-08 09:51:49 +0700

    refactor: add blog response data

--- dealer-marketing/src/constants/url.ts
+++ dealer-marketing/src/constants/url.ts
@@ -2,8 +2,6 @@ const BASE_URL = process.env.NEXT_PUBLIC_ENDPOINTS;
 const BLOGS_URL = `${BASE_URL}/api/blogs`;
 const EXPERTS_URL = `${BASE_URL}/api/experts`;
 
-const API_URL = process.env.NEXT_PUBLIC_BASE_URL;
-const API_BLOGS = `${API_URL}/api/blogs`;
-const API_EXPERTS = `${API_URL}/api/experts`;
+const API_BLOGS = "/api/blogs";
 
-export { BLOGS_URL, EXPERTS_URL, API_BLOGS, API_EXPERTS };
+export { BLOGS_URL, EXPERTS_URL, API_BLOGS };
