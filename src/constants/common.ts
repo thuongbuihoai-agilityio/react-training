eb13b48c2097af6d1771f9dde49e6935b4f2ccf0
Thuong Bui <thuong.buihoai@asnet.com.vn>
2023-04-25 17:43:07 +0700

    feat: implement ui dropdown

---
+++ src/constants/common.ts
@@ -0,0 +1,14 @@
+import { StatusTypes } from "../types/Status";
+
+export const STATUS: StatusTypes[] = [
+  {
+    id: '1',
+    value: 'Available',
+    key: 'available'
+  },
+  {
+    id: '2',
+    value: 'Sold out',
+    key: 'sold-out'
+  }
+]
