404953304142bf99620a7636c85a1fd1c1fb9786
Thuong Bui <thuong.buihoai@asnet.com.vn>
2023-04-25 17:08:08 +0700

    feat: implement ui Input component

--- src/components/Input/Input.test.tsx
+++ src/components/Input/Input.test.tsx
@@ -1,60 +1,48 @@
-import { fireEvent, render } from "@testing-library/react";
+import { fireEvent, render } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import Input from ".";
+import Input from '.';
 
-describe("Input component", () => {
-  test("Should render Input component", () => {
-    const { getByTestId } = render(
-      <Input type="text" placeholder="Search" />,
-    );
-    const inputValue = getByTestId("input-value");
+describe('Input component', () => {
+  test('Should render Input component', () => {
+    const { getByTestId } = render(<Input type='text' placeholder='Search' />);
+    const inputValue = getByTestId('input-value');
     expect(inputValue).toBeInTheDocument();
   });
 
-  test("Should render Input component with onClick", () => {
+  test('Should render Input component with onClick', () => {
     const onClickInput = jest.fn();
     const { getByTestId } = render(
-      <Input
-        type="text"
-        onClick={onClickInput}
-        placeholder="Search"
-      />,
+      <Input type='text' onClick={onClickInput} placeholder='Search' />
     );
-    const inputValue = getByTestId("input-value");
+    const inputValue = getByTestId('input-value');
     fireEvent.click(inputValue);
     expect(onClickInput).toHaveBeenCalled();
   });
 
-  test("Should render Input component with default value of onClick", () => {
-    const { getByTestId } = render(
-      <Input type="text" placeholder="Search" />,
-    );
-    const inputValue = getByTestId("input-value");
+  test('Should render Input component with default value of onClick', () => {
+    const { getByTestId } = render(<Input type='text' placeholder='Search' />);
+    const inputValue = getByTestId('input-value');
     fireEvent.click(inputValue);
   });
 
-  test("Should render Input component with onBlur", () => {
+  test('Should render Input component with onBlur', () => {
     const onBlur = jest.fn();
     const { getByTestId } = render(
-      <Input type="text" onBlur={onBlur} placeholder="Search" />,
+      <Input type='text' onBlur={onBlur} placeholder='Search' />
     );
-    const inputValue = getByTestId("input-value");
+    const inputValue = getByTestId('input-value');
     fireEvent.blur(inputValue);
     expect(onBlur).toHaveBeenCalled();
   });
 
-  test("Should render Input component with default value of onBlur", () => {
-    const { getByTestId } = render(
-      <Input type="text" placeholder="Search" />,
-    );
-    const inputValue = getByTestId("input-value");
+  test('Should render Input component with default value of onBlur', () => {
+    const { getByTestId } = render(<Input type='text' placeholder='Search' />);
+    const inputValue = getByTestId('input-value');
     fireEvent.blur(inputValue);
   });
 
-  test("Matches snapshot", () => {
-    const { container } = render(
-      <Input type="text" placeholder="Search" />,
-    );
+  test('Matches snapshot', () => {
+    const { container } = render(<Input type='text' placeholder='Search' />);
     expect(container).toMatchSnapshot();
   });
 });
