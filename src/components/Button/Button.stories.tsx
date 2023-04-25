c2832a676c66212530028a10644a1ab0d73fe7e2
Thuong Bui <thuong.buihoai@asnet.com.vn>
2023-04-25 13:44:23 +0700

    feat: config storybook

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,21 +1,53 @@
-import { ComponentStory, ComponentMeta } from "@storybook/react";
-import { action } from "@storybook/addon-actions";
-import Button from ".";
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import Button, { ButtonType } from '.';
 
 export default {
   /* 👇 The title prop is optional.
    * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
    * to learn how to generate automatic titles
    */
-  title: "Component/Button",
-  component: Button,
+  title: 'Component/Button',
+  component: Button
 } as ComponentMeta<typeof Button>;
 
-const TemplateButton: ComponentStory<typeof Button> = (args) => (
-  <Button {...args} onClick={action("onClick action")} />
+const TemplateButton: ComponentStory<typeof Button> = args => (
+  <Button {...args} onClick={action('onClick action')} />
 );
 
 export const Primary = TemplateButton.bind({});
 Primary.args = {
-  text: "READ MORE",
+  text: 'Edit',
+  type: ButtonType.primary
+};
+
+export const Secondary = TemplateButton.bind({});
+Secondary.args = {
+  text: 'Delete',
+  type: ButtonType.secondary
+};
+
+export const Tertiary = TemplateButton.bind({});
+Tertiary.args = {
+  text: 'Cancel',
+  type: ButtonType.tertiary
+};
+
+export const Quaternary = TemplateButton.bind({});
+Quaternary.args = {
+  text: 'Delete',
+  type: ButtonType.quaternary
+};
+
+export const Quinary = TemplateButton.bind({});
+Quinary.args = {
+  text: 'Confirm',
+  type: ButtonType.quinary
+};
+
+export const ButtonDisable = TemplateButton.bind({});
+ButtonDisable.args = {
+  text: 'Disable',
+  type: ButtonType.default,
+  disable: true
 };
