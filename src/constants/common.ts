09ea4fad604b09d12d28183e5270eaf81c46ae36
Thuong Bui <thuong.buihoai@asnet.com.vn>
2023-04-26 08:33:35 +0700

    feat: create constants for dropdown

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,14 +1,19 @@
-import { StatusTypes } from "../types/Status";
+import { StatusTypes } from '../types/status';
 
 export const STATUS: StatusTypes[] = [
   {
     id: '1',
+    value: 'All',
+    key: 'all'
+  },
+  {
+    id: '2',
     value: 'Available',
     key: 'available'
   },
   {
-    id: '2',
+    id: '3',
     value: 'Sold out',
     key: 'sold-out'
   }
-]
+];
