49a5e89f946c36e99d9137e4126d2dc0d0b50040
Thuong Bui <thuong.buihoai@asnet.com.vn>
2023-04-26 10:26:53 +0700

    feat: mock data for TableHead

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -17,3 +17,57 @@ export const STATUS: StatusTypes[] = [
     key: 'sold-out'
   }
 ];
+
+export const TYPE: StatusTypes[] = [
+  {
+    id: '1',
+    value: 'All',
+    key: 'all'
+  },
+  {
+    id: '2',
+    value: 'Bravo',
+    key: 'bravo'
+  },
+  {
+    id: '3',
+    value: 'Alfa',
+    key: 'alfa'
+  },
+  {
+    id: '3',
+    value: 'Gold',
+    key: 'gold'
+  }
+];
+
+export const COLUMNS_HEADER = [
+  {
+    value: 'Product',
+    key: 'product'
+  },
+  {
+    value: 'Status',
+    key: 'status'
+  },
+  {
+    value: 'Type',
+    key: 'type'
+  },
+  {
+    value: 'Quantity',
+    key: 'quantity'
+  },
+  {
+    value: 'Brand',
+    key: 'brand'
+  },
+  {
+    value: 'Price',
+    key: 'price'
+  },
+  {
+    value: 'Action',
+    key: 'action'
+  },
+]
