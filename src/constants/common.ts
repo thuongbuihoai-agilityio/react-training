44b066d32c3a4f71bbf47dd6179e224444b0de94
Thuong Bui <thuong.buihoai@asnet.com.vn>
2023-04-26 14:20:59 +0700

    feat: update type for table

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,4 +1,6 @@
 import { StatusTypes } from '../types/status';
+import { TableType } from '../types/table';
+import { ASSETS } from './assets';
 
 export const STATUS: StatusTypes[] = [
   {
@@ -70,4 +72,41 @@ export const COLUMNS_HEADER = [
     value: 'Action',
     key: 'action'
   },
+]
+
+export const TABLE: TableType[] = [
+  {
+    id: '1',
+    productImage: {
+      url: ASSETS.IBM,
+      alt: 'This is image product IBM'
+    },
+    product: 'Louis Vuitton',
+    status: true,
+    type: 'Bravo',
+    quantity: 9177,
+    brandImage: {
+      url: ASSETS.IBM,
+      alt: 'This is image product IBM'
+    },
+    brand: 'Evan Flores',
+    price: 452.85,
+  },
+  {
+    id: '2',
+    productImage: {
+      url: ASSETS.arleneWilson,
+      alt: 'This is image Arlene Wilson'
+    },
+    product: 'Johnson & Johnson',
+    status: false,
+    type: 'Alfa',
+    quantity: 3064,
+    brandImage: {
+      url: ASSETS.arleneWilson,
+      alt: 'This is image Arlene Wilson'
+    },
+    brand: 'Arlene Wilson',
+    price: 901.33,
+  }
 ]
