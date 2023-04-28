bf5926ff3906bb1c2f6a497431fe74676922cfbc
Thuong Bui <thuong.buihoai@asnet.com.vn>
2023-04-28 17:32:27 +0700

    feat: fetch data

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -3,17 +3,26 @@ import TableHead from './TableHead';
 import TableRow from './TableRow';
 import { COLUMNS_HEADER, TABLE } from '../../constants/common';
 import styles from './Table.module.css';
+import useSWR from 'swr';
+import { PRODUCT_URL } from '../../constants/url';
+import { getData } from '../../helpers/apiHandle';
+import { TableType } from '../../types/table';
 
 interface TableProps {
   className?: string;
 }
 
 const Table: React.FC<TableProps> = ({ className = '' }) => {
+  // fetch data with useSWR
+  const { data } = useSWR(PRODUCT_URL, getData<TableType[]>);
+  console.log('data', data);
+  
+
   return (
     <table className={`${className} ${styles['table']}`}>
       <TableHead columns={COLUMNS_HEADER} />
       <tbody>
-        <TableRow data={TABLE} />
+        <TableRow data={data} />
       </tbody>
     </table>
   );
