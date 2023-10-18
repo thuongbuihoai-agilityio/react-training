f3d585430471dfce34b0fb1c63a264efc0073d62
Thuong Bui <thuong.buihoai@asnet.com.vn>
2023-10-18 16:27:27 +0700

    feat: create common component Social

---
+++ sopa-management/src/components/common/Social/__tests__/Social.test.tsx
@@ -0,0 +1,30 @@
+// Libs
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
+
+// Components
+import Social from '..';
+
+describe('Logo component', () => {
+  test('should render logo component', () => {
+    const history = createMemoryHistory();
+    const { getByTestId } = render(
+      <Router location={history.location} navigator={history}>
+        <Social />
+      </Router>
+    );
+    expect(getByTestId('social')).toBeInTheDocument();
+  });
+
+  test('matches snapshot', () => {
+    const history = createMemoryHistory();
+    const { asFragment } = render(
+      <Router location={history.location} navigator={history}>
+        <Social />
+      </Router>
+    );
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
