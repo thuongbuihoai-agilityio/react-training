9187953fadb92d4399d56afb10081ad407646c87
Thuong Bui <thuong.buihoai@asnet.com.vn>
2023-10-19 17:46:14 +0700

    feat: create apiService

---
+++ sopa-management/src/services/APIRequest.ts
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import { LIMIT_PRODUCTS } from '../constants/common';
+
+/**
+ * @param { string } url 
+ * @param { number } pageParam
+ * @returns
+ */
+const getData = async <T>(url: string, pageParam: number = 1): Promise<T> => {
+  const offset = pageParam;
+  const limit = LIMIT_PRODUCTS;
+  const response = await axios.get(`${url}?limit=${limit}&page=${offset}`);
+
+  return response.data;
+};
+
+export { getData };
