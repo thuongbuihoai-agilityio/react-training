9c6c03a98142af2b97fbaed4c048ab42dbba33e3
Thuong Bui <thuong.buihoai@asnet.com.vn>
2023-10-20 14:03:08 +0700

    feat: use useProductStore

--- sopa-management/src/hooks/useQuery.ts
+++ sopa-management/src/hooks/useQuery.ts
@@ -1,16 +1,32 @@
 import { useInfiniteQuery } from 'react-query';
+
+// Mocks axios
 import { AxiosError } from 'axios';
+
+// Constants
 import { QUERY_KEYS } from '../constants/keyQuery';
-import { Product } from '../interfaces/product';
-import { getData } from '../services/APIRequest';
 import { PRODUCT_URL } from '../constants/url';
 import { LIMIT_PRODUCTS } from '../constants/common';
+
+// Interfaces
+import { Product } from '../interfaces/product';
+
+// Services
+import { getData } from '../services/APIRequest';
+
+// Stores
+import { useProductStore } from '../stores/product';
+
+// Helpers
+import { flattenArray } from '../helpers/common';
 
 /**
  * @description custom hook to get product with show more
  */
-export const useInfiniteProducts = () =>
-  useInfiniteQuery<Product[], AxiosError>({
+export const useInfiniteProducts = () => {
+  const { setProducts } = useProductStore();
+
+  return useInfiniteQuery<Product[], AxiosError>({
     queryKey: [QUERY_KEYS.PRODUCTS],
     queryFn: ({ pageParam = 1 }) => getData(PRODUCT_URL, pageParam),
     getNextPageParam: (lastPage, pages) => {
@@ -18,5 +34,10 @@ export const useInfiniteProducts = () =>
       return lastPage?.length > 0 && lastPage?.length === LIMIT_PRODUCTS
         ? nextPage
         : undefined;
-    }
+    },
+    onSuccess: ({ pages }) => {
+      const result = flattenArray(pages);
+      setProducts(result);
+    },
   });
+};
