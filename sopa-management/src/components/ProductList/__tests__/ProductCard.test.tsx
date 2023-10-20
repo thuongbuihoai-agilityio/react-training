60699084faf90f4593a85f3ad4489595d3fb8a6a
Thuong Bui <thuong.buihoai@asnet.com.vn>
2023-10-20 14:05:39 +0700

    feat: update component productcard

--- sopa-management/src/components/ProductList/__tests__/ProductCard.test.tsx
+++ sopa-management/src/components/ProductList/__tests__/ProductCard.test.tsx
@@ -1,58 +1,23 @@
 // Libs
 import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
-import { createMemoryHistory } from 'history';
-import { Router } from 'react-router-dom';
-
-// Mocks
-import { MOCK_PRODUCT } from '../../../__mocks__/product';
 
 // Components
 import ProductCard from '../ProductCard';
 
-describe('ProductCard component', () => {
-  test('should render ProductCard component', () => {
-    const history = createMemoryHistory();
-    const { getByTestId } = render(
-      <Router location={history.location} navigator={history}>
-        <ProductCard
-          href='/'
-          src={MOCK_PRODUCT.image?.url}
-          name={MOCK_PRODUCT.name}
-          color={MOCK_PRODUCT.color}
-          price={MOCK_PRODUCT.price}
-        />
-      </Router>
-    );
-    expect(getByTestId('product-card')).toBeInTheDocument();
-  });
+// Helpers
+import { renderRouterTest } from '../../../helpers/testUtils';
 
-  test('should render ProductCard component by default', () => {
-    const history = createMemoryHistory();
-    const { getByTestId } = render(
-      <Router location={history.location} navigator={history}>
-        <ProductCard
-          href='/'
-          src={MOCK_PRODUCT.image?.url}
-        />
-      </Router>
-    );
+// Mocks
+import { MOCK_PRODUCTS } from '../../../__mocks__/product';
+
+describe('ProductList component', () => {
+  test('should render ProductList component', () => {
+    const { getByTestId } = renderRouterTest(<ProductCard href='/' data={MOCK_PRODUCTS} />);
     expect(getByTestId('product-card')).toBeInTheDocument();
   });
 
   test('matches snapshot', () => {
-    const history = createMemoryHistory();
-    const { asFragment } = render(
-      <Router location={history.location} navigator={history}>
-        <ProductCard
-          href='/'
-          src={MOCK_PRODUCT.image?.url}
-          name={MOCK_PRODUCT.name}
-          color={MOCK_PRODUCT.color}
-          price={MOCK_PRODUCT.price}
-        />
-      </Router>
-    );
+    const { asFragment } = renderRouterTest(<ProductCard href='/' data={MOCK_PRODUCTS} />);
     expect(asFragment()).toMatchSnapshot();
   });
 });
