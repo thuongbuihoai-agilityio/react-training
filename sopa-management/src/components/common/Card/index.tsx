7e2192d436fa53107968f7c79a13453549df2d00
Thuong Bui <thuong.buihoai@asnet.com.vn>
2023-10-20 14:06:40 +0700

    feat: create component card

---
+++ sopa-management/src/components/common/Card/index.tsx
@@ -0,0 +1,55 @@
+import { memo } from 'react';
+import { Link } from 'react-router-dom';
+
+// Components
+import Text,
+{
+  ThemeType
+} from '../../common/Text';
+import Price,
+{
+  PriceType
+} from '../../common/Price';
+import Image from '../Image';
+
+// Styles
+import './card.css';
+
+interface CardProps {
+  name?: string;
+  color?: string;
+  price?: number;
+  href: string;
+  src: string;
+  alt?: string;
+}
+
+const Card: React.FC<CardProps> = ({
+  name = '',
+  color = '',
+  price = 0,
+  href,
+  src,
+  alt = ''
+}) => {
+  return (
+    <div data-testid='card' className='card'>
+      <div className='card-item'>
+        <figure className='card-image'>
+          <Image href={href} url={src} alt={alt} className='card-image-item' />
+        </figure>
+        <div className='card-info'>
+          <Link to={href} className='card-name'>
+            {name}
+          </Link>
+          <div className='card-description'>
+            <Text text={color} type={ThemeType.highlightPrimary} />
+            <Price value={price} type={PriceType.primary} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default memo(Card);
