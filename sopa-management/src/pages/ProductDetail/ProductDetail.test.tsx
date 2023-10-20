11f3acd4f3ae5e174c8bbbee593dd0c45d096acf
Thuong Bui <thuong.buihoai@asnet.com.vn>
2023-10-20 17:25:43 +0700

    feat: update unit test

---
+++ sopa-management/src/pages/ProductDetail/ProductDetail.test.tsx
@@ -0,0 +1,20 @@
+// Libs
+import '@testing-library/jest-dom';
+
+// Components
+import ProductDetail from '.';
+
+// Helpers
+import { renderWithRouterAndQuery } from '../../helpers/testUtils';
+
+describe('ProductDetail component', () => {
+  test('should render ProductDetail component', () => {
+    const { getByTestId } = renderWithRouterAndQuery(<ProductDetail />);
+    expect(getByTestId('detail')).toBeInTheDocument();
+  });
+
+  test('matches snapshot', () => {
+    const { container } = renderWithRouterAndQuery(<ProductDetail />);
+    expect(container).toMatchSnapshot();
+  });
+});
