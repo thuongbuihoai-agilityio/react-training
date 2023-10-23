37bb7dac6f2538c15e31d8ac57f44491bf67b2c0
Thuong Bui <thuong.buihoai@asnet.com.vn>
2023-10-23 15:26:40 +0700

    feat: create component Modal

--- sopa-management/src/components/common/Modal/index.tsx
+++ sopa-management/src/components/common/Modal/index.tsx
@@ -1,67 +1,115 @@
-import { memo } from "react";
-import './modal.css';
-import { Trash } from "../../../../public/images/icons";
-import Image from "../Image";
-import Text, { ThemeType } from "../Text";
-import Counter from "../Counter";
-import Button, { ButtonType } from "../Button";
-import Price, { PriceType } from "../Price";
-import { Product } from "../../../interfaces/product";
-import { useCounterStore } from "../../../stores/counter";
-import { getStorage } from "../../../helpers/storage";
-import { totalPrices } from "../../../helpers/common";
+import { memo } from 'react';
 
-const Modal: React.FC = () => {
-  const { count, setCount } = useCounterStore();
-  const cartData = getStorage('cart') || [];
+// Images
+import {
+  Close,
+  Trash
+} from '../../../../public/images/icons';
+
+// Components
+import Image from '../Image';
+import Text,
+{
+  ThemeType
+} from '../Text';
+import Counter from '../Counter';
+import Button,
+{
+  ButtonType
+} from '../Button';
+import Price,
+{
+  PriceType
+} from '../Price';
+
+// Interfaces
+import { Product } from '../../../interfaces/product';
+
+// Helpers
+import { totalPrices } from '../../../helpers/common';
+
+// Stores
+import { useCartStore } from '../../../stores/cart';
+
+// Styles
+import './modal.css';
+
+interface ModalProps {
+  onToggleModal?: () => void;
+}
+const Modal: React.FC<ModalProps> = ({
+  onToggleModal = () => {},
+}) => {
+  const { carts } = useCartStore();
 
   const handleIncrement = () => {
-    setCount(count + 1);
+    // TODO: I will handle in feature/update-cart
   };
 
   const handleDecrement = () => {
-    setCount(count - 1);
+    // TODO: I will handle in feature/update-cart
   };
 
-
   return (
-    <div className="cart">
-      <div className="cart-header">
-        <Text text="Cart" className="cart-text" />
-        <Trash />
-      </div>
-      <div className="card-body">
-        {cartData?.map((item: Product) => (
-          <div className="cart-item">
-            <div className="cart-info">
-              <figure className="cart-image">
-                <Image className="cart-image-item" url={item?.image?.url} />
-              </figure>
-              <div className="cart-description">
-                <Text text={item.name} className="cart-text" />
-                <Text text={`Color: ${item.color}`} type={ThemeType.highlightPrimary} className="cart-color" />
-                <Text text={`Size: ${item.size}`} type={ThemeType.highlightPrimary} className="cart-size" />
-                <div className="card-quantity">
-                  <Counter
-                    value={item?.quantity}
-                    onDecrement={handleDecrement}
-                    onIncrement={handleIncrement}
+    <div data-testid='modal' className='overlay'>
+      <div className='cart'>
+        <div className='cart-header'>
+          <Text text='Cart' className='cart-text' />
+          <Button
+            children={<Close />}
+            type={ButtonType.btnIconPrimary}
+            onClick={onToggleModal}
+          />
+        </div>
+        <div className='card-body'>
+          {carts?.map((item: Product) => (
+            <div className='cart-item'>
+              <div className='cart-info'>
+                <figure className='cart-image'>
+                  <Image className='cart-image-item' url={item?.image?.url} />
+                </figure>
+                <div className='cart-description'>
+                  <Text text={item.name} className='cart-text' />
+                  <Text
+                    text={`Color: ${item.color}`}
+                    type={ThemeType.highlightPrimary}
+                    className='cart-color'
                   />
-                  <Button children={<Trash />} type={ButtonType.btnIconSecondary} className="cart-btn" />
+                  <Text
+                    text={`Size: ${item.size}`}
+                    type={ThemeType.highlightPrimary}
+                    className='cart-size'
+                  />
+                  <div className='card-quantity'>
+                    <Counter
+                      value={item?.quantity}
+                      onDecrement={handleDecrement}
+                      onIncrement={handleIncrement}
+                    />
+                    <Button
+                      children={<Trash />}
+                      type={ButtonType.btnIconSecondary}
+                      className='cart-btn'
+                    />
+                  </div>
                 </div>
               </div>
+              <Price
+                value={item.price}
+                type={PriceType.tertiary}
+                className='cart-price'
+              />
             </div>
-            <Price value={item.price} type={PriceType.tertiary} className="cart-price" />
-          </div>
-        ))}
-      </div>
-      <hr />
-      <div className="cart-footer">
-        <Text text="Subtotal" className="cart-text" />
-        <Price value={totalPrices(cartData)} type={PriceType.tertiary} />
+          ))}
+        </div>
+        <hr />
+        <div className='cart-footer'>
+          <Text text='Subtotal' className='cart-text' />
+          <Price value={totalPrices(carts)} type={PriceType.tertiary} />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default memo(Modal);
