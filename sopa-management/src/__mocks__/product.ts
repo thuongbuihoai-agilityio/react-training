e2ad3dcf7f60f1210347abac5d882f05d5dec1f5
Thuong Bui <thuong.buihoai@asnet.com.vn>
2023-10-23 15:07:21 +0700

    feat: update mock data

--- sopa-management/src/__mocks__/product.ts
+++ sopa-management/src/__mocks__/product.ts
@@ -12,7 +12,8 @@ const MOCK_PRODUCTS: Product[] = [
       url: '/images/products/black-shoes.png',
       alt: 'This is black shoes'
     },
-    id: '1'
+    id: '1',
+    quantity: 2
   },
   {
     name: 'Blue shoes',
@@ -25,7 +26,8 @@ const MOCK_PRODUCTS: Product[] = [
       url: '/images/products/blue-shoes.png',
       alt: 'This is blue shoes'
     },
-    id: '2'
+    id: '2',
+    quantity: 1
   }
 ];
 
@@ -33,14 +35,14 @@ const MOCK_PRODUCT: Product = {
   name: 'Black shoes',
   price: 87,
   size: 'Large',
-  description:
-    'Our goal in designing the super-soft Atoms Crew Socks was to provide a seamless match with your Atoms shoes, enhancing your comfort for everyday wear - making them the perfect socks for your everyday shoes.',
+  description: 'Our goal in designing the super-soft Atoms Crew Socks was to provide a seamless match with your Atoms shoes, enhancing your comfort for everyday wear - making them the perfect socks for your everyday shoes.',
   color: 'Black',
   image: {
     url: '/images/products/black-shoes.png',
     alt: 'This is black shoes'
   },
-  id: '1'
+  id: '1',
+  quantity: 3
 };
 
 export {
