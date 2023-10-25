07cb2d59e97b84a1eb353f2373942249036e943b
Thuong Bui <thuong.buihoai@asnet.com.vn>
2023-10-25 15:15:24 +0700

    feat: update component Input

--- sopa-management/src/components/common/Input/index.tsx
+++ sopa-management/src/components/common/Input/index.tsx
@@ -16,34 +16,66 @@ export enum InputTheme {
 }
 
 interface IconProps {
+  id?: string;
+  name?: string;
+  hidden?: boolean;
+  checked?: boolean;
   value?: string | number;
   label?: string;
   type?: string;
+  style?: string;
   theme?: string;
   placeholder?: string;
   classNameInput?: string;
   classNameLabel?: string;
+  className?: string;
 }
 
 const Input: React.FC<IconProps> = ({
+  id = '',
+  name = '',
+  hidden = false,
+  checked = false,
   value,
   label = '',
-  type = InputType.default,
+  type = '',
   theme = InputTheme.default,
+  style = InputType.default,
   placeholder = '',
   classNameInput = 'input',
-  classNameLabel = 'label'
+  classNameLabel = 'label',
+  className = ''
 }) => (
-  <div data-testId='input-value' className='input-wrapper'>
-    {label && (
-      <label className={`${classNameLabel} label-${theme}`}>{label}</label>
+  <>
+    {label ? (
+      <div data-testId='input-value' className='input-wrapper'>
+        <label className={`${classNameLabel} label-${theme}`}>{label}</label>
+        <input
+          id={id}
+          name={name}
+          hidden={hidden}
+          checked={checked}
+          value={value}
+          placeholder={placeholder}
+          aria-hidden='true'
+          className={`${classNameInput} input-${style}`}
+        />
+      </div>
+    ) : (
+      <input
+        data-testId='input'
+        id={id}
+        name={name}
+        hidden={hidden}
+        defaultChecked={checked}
+        type={type}
+        value={value}
+        placeholder={placeholder}
+        aria-hidden='true'
+        className={className}
+      />
     )}
-    <input
-      value={value}
-      placeholder={placeholder}
-      className={`${classNameInput} input-${type}`}
-    />
-  </div>
+  </>
 );
 
 export default memo(Input);
