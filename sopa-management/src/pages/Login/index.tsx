9c0bd07fcd0170755d20630ebb638fd7b9241450
Thuong Bui <thuong.buihoai@asnet.com.vn>
2023-10-30 16:51:42 +0700

    feat: revert code

--- sopa-management/src/pages/Login/index.tsx
+++ sopa-management/src/pages/Login/index.tsx
@@ -117,7 +117,7 @@ const Login: React.FC = () => {
           control={control}
           rules={VALIDATE.email}
           render={({ field }) => (
-            <div aria-hidden='true' className='form-email'>
+            <div className='form-email'>
               <Input
                 {...field}
                 label='Email'
@@ -139,7 +139,7 @@ const Login: React.FC = () => {
           control={control}
           rules={VALIDATE.password}
           render={({ field }) => (
-            <div aria-hidden='true' className='form-password'>
+            <div className='form-password'>
               <Input
                 {...field}
                 label='Password'
