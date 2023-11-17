2a429ec35cfc0a2be2b1d4e8c1eba7864e6aa08a
Thuong Bui <thuong.buihoai@asnet.com.vn>
2023-11-17 15:52:30 +0700

    feat: deploy json-server

--- sopa-management/index.js
+++ sopa-management/index.js
@@ -1,5 +1,6 @@
 // server.js
-const jsonServer = require("json-server");
+import jsonServer from "json-server";
+
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
