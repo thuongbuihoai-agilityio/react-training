3bea55315d47079bb3b5051e8390e9c6ff173a81
Thuong Bui <thuong.buihoai@asnet.com.vn>
2023-11-20 15:10:31 +0700

    feat: apply wdyr

--- sopa-management/src/wdyr.ts
+++ sopa-management/src/wdyr.ts
@@ -2,7 +2,7 @@
 import React from 'react';
 import whyDidYouRender from '@welldone-software/why-did-you-render';
 
-if (process.env.VITE_BASE_URL === 'development') {
+if (process.env.NODE_ENV === 'development') {
   whyDidYouRender(React, {
     trackAllPureComponents: true,
   });
