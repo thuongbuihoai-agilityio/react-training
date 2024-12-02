64122942d346cca237ac0912e25cb43482aa798b
Thuong Bui <thuong.buihoai@asnet.com.vn>
2024-12-02 14:57:28 +0700

    delete ButtonIcon stories

--- practices/practice-3/src/reducer/productReducer.ts
+++ practices/practice-3/src/reducer/productReducer.ts
@@ -1,9 +1,14 @@
 import { Action, DataAction, DataState } from "@common-types/data";
+<<<<<<< HEAD
+=======
+import { Product } from "@common-types/product";
+>>>>>>> fb2d47e9 (move practice-3 into practices folder)
 
 const productReducer = (state: DataState, actions: DataAction): DataState => {
   const { action, payload } = actions;
   switch (action) {
     case Action.GetProductSuccess: {
+<<<<<<< HEAD
       return { ...state, products: payload };
     }
     case Action.CreateProductsSuccess: {
@@ -20,6 +25,20 @@ const productReducer = (state: DataState, actions: DataAction): DataState => {
     case Action.UpdateProductSuccess: {
       const newProduct = [...state.products];
       return { ...state.products, products: newProduct };
+=======
+      return { ...state, products: payload as Product[] };
+    }
+    case Action.CreateProductsSuccess: {
+      return { ...state.products, products: state.products.concat(payload as Product) };
+    }
+    case Action.DeleteProductSuccess: {
+      const deleteProduct = state.products.filter(item => item.id != payload)
+      return {...state, products: deleteProduct};
+    }
+    case Action.UpdateProductSuccess: {
+      state.products.findIndex(item => item.id == payload);
+      return {...state};
+>>>>>>> fb2d47e9 (move practice-3 into practices folder)
     }
     default: {
       return { ...state };
--- practices/practice-3/src/stories/CategoryList.stories.tsx
+++ practices/practice-3/src/stories/CategoryList.stories.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react";
 import CategoryList from "@components/Categories/CategoryList/CategoryList";
 
@@ -15,3 +16,36 @@ const Default: React.FC = () => {
 }
 
 export { Default };
+=======
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import CategoryList from "@components/Categories/CategoryList/CategoryList";
+import { DataContext } from "@context/DataContext";
+import { PRODUCT_MOCKING_LIST } from "@__mocks__/constants/product";
+import { CATEGORY_MOCKING_LIST } from "@__mocks__/constants/categories";
+
+export default {
+  /* 👇 The title prop is optional.
+   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: "Component/CategoryList",
+  component: CategoryList,
+} as ComponentMeta<typeof CategoryList>;
+
+const value = {
+  products: PRODUCT_MOCKING_LIST,
+  dispatch: () => {},
+  searchValue: "",
+  setSearchValue: () => {},
+  categories: CATEGORY_MOCKING_LIST,
+  setCategories: () => {},
+};
+
+const TemplateCategoryList: ComponentStory<typeof CategoryList> = () => (
+  <DataContext.Provider value={value}>
+    <CategoryList />
+  </DataContext.Provider>
+);
+
+export const Default = TemplateCategoryList.bind({});
+>>>>>>> fb2d47e9 (move practice-3 into practices folder)
