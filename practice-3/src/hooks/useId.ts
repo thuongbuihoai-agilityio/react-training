36212070bfcf77d280d1847c78975c90439bce82
Thuong Bui <thuong.buihoai@asnet.com.vn>
2024-12-02 14:41:42 +0700

    resolve conflict

--- practice-3/src/hooks/useId.ts
+++
@@ -1,8 +0,0 @@
-import { useState } from "react";
-
-export default function useId() {
-  const [id, setId] = useState("");
-  const date = new Date();
-  setId(date.valueOf().toString())
-  return id;
-}
