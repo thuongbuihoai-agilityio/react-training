7bb7a83398d51b3ef5fbf7cada81dc8ba4888bcc
Thuong Bui <thuong.buihoai@asnet.com.vn>
2024-12-02 14:49:16 +0700

    delete product file

--- practices/practice-3/src/context/AppContext.tsx
+++ practices/practice-3/src/context/AppContext.tsx
@@ -1,16 +1,9 @@
-import ProductProvider from "./ProductContext";
 import SearchProvider from "./SearchContext";
 
 const AppProvider: React.FC<{ children: JSX.Element[] | JSX.Element }> = ({
   children,
 }) => {
-  return (
-    <ProductProvider>
-      <SearchProvider>
-        {children}
-      </SearchProvider>
-    </ProductProvider>
-  );
+  return <SearchProvider>{children}</SearchProvider>;
 };
 
 export default AppProvider;
