80c5f79847ac73a17adfeef6fa4614dec75003b0
Thuong Bui <thuong.buihoai@asnet.com.vn>
2024-12-02 14:52:10 +0700

    delete scrollButton.css

--- practices/practice-3/src/stories/Button.stories.tsx
+++ practices/practice-3/src/stories/Button.stories.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Button from "@/components/common/Button/Button";
+import Button from "@/components/common/Button/Button/Button";
 
 export default {
   /* 👇 The title prop is optional.
---
+++ practices/practice-3/src/stories/ScrollButton.stories.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ScrollButton from "@/components/common/Button/ScrollButton/ScrollButton";
+
+export default {
+  /* 👇 The title prop is optional.
+   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: "Component/ScrollButton",
+  component: ScrollButton,
+};
+
+const Default: React.FC = () => {
+  return (
+    <ScrollButton
+      className="btn__scroll"
+      text={<i className="fa fa-arrow-alt-circle-up"></i>}
+    />
+  );
+};
+
+export { Default };
