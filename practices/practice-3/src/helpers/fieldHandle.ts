2c15f3f86fb72578a1fd89742cbb17b3a5da076d
Thuong Bui <thuong.buihoai@asnet.com.vn>
2024-12-02 14:50:23 +0700

    delete helper file

---
+++ practices/practice-3/src/helpers/fieldHandle.ts
@@ -0,0 +1,14 @@
+// handle validate field value
+const setFieldsValue = (fieldsValue: any, value: string, fieldName: string) => {
+  const fieldData = fieldsValue[fieldName];
+  return {
+    ...fieldsValue,
+    [fieldName]: {
+      ...fieldData,
+      value: value,
+      error: ""
+    }
+  };
+}
+
+export { setFieldsValue };
